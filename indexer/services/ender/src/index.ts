99c311507de42e98a9cbf6dddb5e1030c679bda2
dydxwill <119354122+dydxwill@users.noreply.github.com>
2023-08-16 13:14:14 -0400

    add skeleton for asset creation handler/basic validation (#6)

--- indexer/services/ender/src/index.ts
+++ indexer/services/ender/src/index.ts
@@ -1,6 +1,6 @@
 import { logger, startBugsnag, wrapBackgroundTask } from '@dydxprotocol-indexer/base';
 import { stopConsumer, startConsumer } from '@dydxprotocol-indexer/kafka';
-import { assetRefresher, perpetualMarketRefresher } from '@dydxprotocol-indexer/postgres';
+import { assetRefresher, perpetualMarketRefresher, marketRefresher } from '@dydxprotocol-indexer/postgres';
 
 import { initializeAllCaches } from './caches/block-cache';
 import config from './config';
@@ -22,9 +22,11 @@ async function startKafka(): Promise<void> {
   await Promise.all([
     perpetualMarketRefresher.updatePerpetualMarkets(),
     assetRefresher.updateAssets(),
+    marketRefresher.updateMarkets(),
   ]);
   wrapBackgroundTask(perpetualMarketRefresher.start(), true, 'startUpdatePerpetualMarkets');
   wrapBackgroundTask(assetRefresher.start(), true, 'startUpdateAssets');
+  wrapBackgroundTask(marketRefresher.start(), true, 'startUpdateMarkets');
   await initializeAllCaches();
 
   await connect();
