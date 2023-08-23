1fd9f622a4a76bdf1d123d303de00e87a8660843
Brendan Chou <3680392+BrendanChou@users.noreply.github.com>
2023-08-23 15:47:57 -0400

    update bridge log and sepolia contract (#48)
    * update bridge log and sepolia contract
    
    ---------
    
    Co-authored-by: Tian Qin <tian@dydx.exchange>

--- protocol/lib/eth/util.go
+++ protocol/lib/eth/util.go
@@ -15,6 +15,11 @@ import (
 
 var bridgeEventAbi *ethabi.ABI
 
+const (
+	MIN_ADDRESS_BYTES = 20
+	MAX_ADDRESS_BYTES = 32
+)
+
 // getBridgeEventAbi returns the ABI (application binary interface) for the Bridge contract.
 func getBridgeEventAbi() *ethabi.ABI {
 	// Initialize the singleton if it does not exist.
@@ -28,7 +33,24 @@ func getBridgeEventAbi() *ethabi.ABI {
 	return bridgeEventAbi
 }
 
-// BridgeLogToEvent converts an Ethereum log from Bridge contract to a BridgeEvent.
+// padOrTruncateAddress right-pads an address with zeros if it's shorter than `MIN_ADDRESS_BYTES` or
+// takes the first `MAX_ADDRESS_BYTES` if it's longer than that.
+func padOrTruncateAddress(address []byte) []byte {
+	if len(address) > MAX_ADDRESS_BYTES {
+		return address[:MAX_ADDRESS_BYTES]
+	} else if len(address) < MIN_ADDRESS_BYTES {
+		return append(address, make([]byte, MIN_ADDRESS_BYTES-len(address))...)
+	}
+	return address
+}
+
+/*
+BridgeLogToEvent converts an Ethereum log from Bridge contract to a BridgeEvent.
+Note: The format of a dYdX address is [prefix][separator][address][checksum], where `prefix` is `dydx`,
+`separator` is `1`, `address` is the actual address portion, and `checksum` occupies last 6 characters.
+An address in Ethereum logs is in hexadecimal format and in Cosmos bech32 format. For example, a
+20-byte address in hexadecimal format is 20*8=160 bits, which is 160/5=32 bech32 characters.
+*/
 func BridgeLogToEvent(
 	log ethcoretypes.Log,
 	denom string,
@@ -42,12 +64,16 @@ func BridgeLogToEvent(
 		panic(err)
 	}
 	amount := bridgeEventData[0].(*big.Int)
-	address := bridgeEventData[1].([32]byte)
+	address := padOrTruncateAddress(bridgeEventData[2].([]byte))
+
+	// Unused daemon fields.
+	// bridgeEventData[1] is the Ethereum address that sent the tokens
+	// bridgeEventData[3] is the user-supplied memo
 
 	return bridgetypes.BridgeEvent{
 		Id:             id,
 		Coin:           sdk.NewCoin(denom, sdk.NewIntFromBigInt(amount)),
-		Address:        sdk.MustBech32ifyAddressBytes(config.Bech32PrefixAccAddr, address[:]),
+		Address:        sdk.MustBech32ifyAddressBytes(config.Bech32PrefixAccAddr, address),
 		EthBlockHeight: log.BlockNumber,
 	}
 }
