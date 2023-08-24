ab5c83f1b542bd53979c7ae3bd86e6763fa43c8c
jayy04 <103467857+jayy04@users.noreply.github.com>
2023-08-23 20:07:09 -0400

    [CLOB-68] start with a random subaccount instead of using a fixed order (#38)
    * [CLOB-68] start with a random subaccount instead of a fixed order
    
    * comments

--- protocol/x/clob/keeper/liquidations_test.go
+++ protocol/x/clob/keeper/liquidations_test.go
@@ -4,6 +4,7 @@ import (
 	"math"
 	"math/big"
 	"testing"
+	"time"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
@@ -1055,6 +1056,8 @@ func TestPlacePerpetualLiquidation_PreexistingLiquidation(t *testing.T) {
 				ks.ClobKeeper.InitializeLiquidationsConfig(ctx, tc.liquidationConfig),
 			)
 
+			ks.ClobKeeper.SetBlockTimeForLastCommittedBlock(ctx.WithBlockTime(time.Unix(5, 0)))
+
 			// Place all existing orders on the orderbook.
 			for _, matchableOrder := range tc.placedMatchableOrders {
 				// If the order is a liquidation order, place the liquidation.
@@ -1879,6 +1882,8 @@ func TestPlacePerpetualLiquidation_Deleveraging(t *testing.T) {
 				t,
 				ks.ClobKeeper.InitializeLiquidationsConfig(ctx, tc.liquidationConfig),
 			)
+
+			ks.ClobKeeper.SetBlockTimeForLastCommittedBlock(ctx.WithBlockTime(time.Unix(5, 0)))
 
 			// Place all existing orders on the orderbook.
 			for _, matchableOrder := range tc.placedMatchableOrders {
