397c603ded11653f8d17a9563a42b99848b033e2
Teddy Ding <teddy@dydx.exchange>
2023-08-24 18:07:54 -0400

    nit

--- protocol/x/perpetuals/keeper/msg_server_create_perpetual.go
+++ protocol/x/perpetuals/keeper/msg_server_create_perpetual.go
@@ -11,5 +11,6 @@ func (k msgServer) CreatePerpetual(
 	goCtx context.Context,
 	msg *types.MsgCreatePerpetual,
 ) (*types.MsgCreatePerpetualResponse, error) {
+	// TODO(CORE-502): Implement message handler.
 	return &types.MsgCreatePerpetualResponse{}, errors.New("Not implemented")
 }
