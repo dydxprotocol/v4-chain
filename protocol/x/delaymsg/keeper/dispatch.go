1b77f48d80e51be4167fcf2e85e6427029803faf
Crystal Lemire <crystal@dydx.exchange>
2023-08-24 16:08:13 -0700

    All-or-nothing execution of delayed messages per block with cleanup.

--- protocol/x/delaymsg/keeper/dispatch.go
+++ protocol/x/delaymsg/keeper/dispatch.go
@@ -15,28 +15,42 @@ func (k Keeper) DispatchMessagesForBlock(ctx sdk.Context) {
 		return
 	}
 
+	// Create a cache context to avoid writing to the store during message execution.
+	// Only persist the cache if all message executions are successful.
+	cacheCtx, writeCache := ctx.CacheContext()
+	failure := false
+
 	// Execute all delayed messages scheduled for this block and delete them from the store.
 	for _, id := range blockMessageIds.Ids {
-		delayedMsg, found := k.GetMessage(ctx, id)
+		delayedMsg, found := k.GetMessage(cacheCtx, id)
 		if !found {
-			k.Logger(ctx).Error("delayed message %v not found", id)
-			continue
+			k.Logger(cacheCtx).Error("delayed message %v not found", id)
+			failure = true
+			break
 		}
 
 		var msg sdk.Msg
 		err := k.DecodeMessage(delayedMsg.Msg, &msg)
 
 		if err != nil {
-			k.Logger(ctx).Error("failed to decode delayed message with id %v: %v", id, err)
-			continue
+			k.Logger(cacheCtx).Error("failed to decode delayed message with id %v: %v", id, err)
+			failure = true
+			break
 		}
 
 		handler := k.router.Handler(msg)
-		_, err = handler(ctx, msg)
+		_, err = handler(cacheCtx, msg)
 
 		if err != nil {
-			k.Logger(ctx).Error("failed to execute delayed message with id %v: %v", id, err)
+			k.Logger(cacheCtx).Error("failed to execute delayed message with id %v: %v", id, err)
+			failure = true
+			break
 		}
+	}
+
+	// If all message executions are successful, persist the cache.
+	if !failure {
+		writeCache()
 	}
 
 	for _, id := range blockMessageIds.Ids {
