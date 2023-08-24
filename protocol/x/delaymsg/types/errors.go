44f904c1e151f7518bb745ce77fb6018be6faea7
Crystal Lemire <crystal@dydx.exchange>
2023-08-24 10:33:39 -0700

    Fix kruft from x/gov copy pasta.

--- protocol/x/delaymsg/types/errors.go
+++ protocol/x/delaymsg/types/errors.go
@@ -6,7 +6,7 @@ import sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 var (
 	ErrInvalidInput    = sdkerrors.Register(ModuleName, 1, "Invalid input")
 	ErrMsgIsNil        = sdkerrors.Register(ModuleName, 2, "Delayed msg is nil")
-	ErrMsgIsUnroutable = sdkerrors.Register(ModuleName, 3, "proposal message not recognized by router")
+	ErrMsgIsUnroutable = sdkerrors.Register(ModuleName, 3, "Message not recognized by router")
 
 	ErrInvalidGenesisState = sdkerrors.Register(ModuleName, 10, "Invalid genesis state")
 )
