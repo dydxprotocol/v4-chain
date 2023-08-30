e2ea4e4ebebbe708f42d77c890214941fc2830ae
Crystal Lemire <crystal.lemire@gmail.com>
2023-08-30 16:53:46 -0700

    Add signers check. Remove unused test constants. (#155)

--- protocol/testutil/constants/delaymsg.go
+++ protocol/testutil/constants/delaymsg.go
@@ -30,10 +30,5 @@ var (
 	}
 	InvalidMsg = &testdata.TestMsg{Signers: []string{"invalid - no module handles this message"}}
 
-	// Msg1Bytes, Msg2Bytes and Msg3Bytes are left as long lines for ease of byte-by-byte comparison.
-	Msg1Bytes = []byte("\n&/dydxprotocol.bridge.MsgCompleteBridge\x126\n+dydx1mkkvp26dngu6n8rmalaxyp3gwkjuzztq5zx6tr\x12\x07\x08\x01\x12\x03\x12\x010") // nolint:lll
-	Msg2Bytes = []byte("\n&/dydxprotocol.bridge.MsgCompleteBridge\x126\n+dydx1mkkvp26dngu6n8rmalaxyp3gwkjuzztq5zx6tr\x12\x07\x08\x02\x12\x03\x12\x010") // nolint:lll
-	Msg3Bytes = []byte("\n&/dydxprotocol.bridge.MsgCompleteBridge\x126\n+dydx1mkkvp26dngu6n8rmalaxyp3gwkjuzztq5zx6tr\x12\x07\x08\x03\x12\x03\x12\x010") // nolint:lll
-
 	AllMsgs = []sdk.Msg{TestMsg1, TestMsg2, TestMsg3}
 )
