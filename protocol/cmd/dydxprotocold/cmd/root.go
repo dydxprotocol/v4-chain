93bda1ca8e2a7b26874c6f0654a3051d10a1ee13
vincentwschau <99756290+vincentwschau@users.noreply.github.com>
2023-09-05 18:26:10 -0400

    Generate and use swagger docs correctly. (#169)

--- protocol/cmd/dydxprotocold/cmd/root.go
+++ protocol/cmd/dydxprotocold/cmd/root.go
@@ -43,6 +43,10 @@ import (
 	"github.com/dydxprotocol/v4-chain/protocol/lib/metrics"
 	"github.com/spf13/cast"
 	"github.com/spf13/cobra"
+
+	// Unnamed import of statik for swagger UI support.
+	// Used in cosmos-sdk when registering the route for swagger docs.
+	_ "github.com/dydxprotocol/v4-chain/protocol/client/docs/statik"
 )
 
 const (
