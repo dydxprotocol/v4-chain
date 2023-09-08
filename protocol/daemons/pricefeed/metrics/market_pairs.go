70f04c46c4cdef71c4fd5bcb56ceb082f618b57c
Crystal Lemire <crystal@dydx.exchange>
2023-09-08 16:21:15 -0700

    Add documentation.

--- protocol/daemons/pricefeed/metrics/market_pairs.go
+++ protocol/daemons/pricefeed/metrics/market_pairs.go
@@ -6,12 +6,16 @@ import (
 )
 
 var (
+	// marketToPair is a map of marketId to marketPair strings used for labelling metrics.
+	// This map is populated whenever markets are created or updated and access to this map is
+	// synchronized by the below mutex.
 	marketToPair = map[types.MarketId]string{}
-	lock         sync.RWMutex
+	// lock syncronizes access to the marketToPair map.
+	lock sync.RWMutex
 )
 
 // AddMarketPairForTelemetry adds a market pair to an in-memory map of marketId to marketPair strings used
-// for labelling metrics.
+// for labelling metrics. This method is synchronized.
 func AddMarketPairForTelemetry(marketId types.MarketId, marketPair string) {
 	lock.Lock()
 	defer lock.Unlock()
@@ -19,7 +23,7 @@ func AddMarketPairForTelemetry(marketId types.MarketId, marketPair string) {
 }
 
 // GetMarketPairForTelemetry returns the market pair string for a given marketId. If the marketId is not
-// found in the map, returns the INVALID string.
+// found in the map, returns the INVALID string. This method is synchronized.
 func GetMarketPairForTelemetry(marketId types.MarketId) string {
 	lock.RLock()
 	defer lock.RUnlock()
