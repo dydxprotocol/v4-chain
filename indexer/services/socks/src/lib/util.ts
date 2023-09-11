8da528cfa4be4482fe46e4c05d4e8cd5e99538c3
Vincent Chau <99756290+vincentwschau@users.noreply.github.com>
2023-09-11 17:42:35 -0400

    Remove unneeded file.

--- indexer/services/socks/src/lib/util.ts
+++
@@ -1,5 +0,0 @@
-const RESTRICTED_COUNTRY_CODES: Set<string> = new Set(config.RESTRICTED_COUNTRIES.split(','));
-
-export function isRestrictedCountry(ipCountry: string): boolean {
-  return RESTRICTED_COUNTRY_CODES.has(ipCountry);
-}
