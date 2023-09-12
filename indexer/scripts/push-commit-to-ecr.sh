eaca098345be1ff85b407124789f5a7beb323670
Lukasz Cwik <lcwik@apache.org>
2023-09-12 10:40:48 -0700

    Rename testnet2 back to testnet for public testnet

--- indexer/scripts/push-commit-to-ecr.sh
+++ indexer/scripts/push-commit-to-ecr.sh
@@ -24,8 +24,7 @@ case $env in
     "dev5") account=917958511744;;
     "staging") account=677285201534;;
     "public-testnet") account=013339450148;; # public testnet
-    "testnet1") account=013339450148;; # public testnet
-    "testnet2") account=013339450148;; # public testnet
+    "testnet3") account=013339450148;; # public testnet
     *) account=329916310755;;
 esac
 
