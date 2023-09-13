5e2787cd7f3b4ac72602fb388bdca5ba1f99d383
Crystal Lemire <crystal@dydx.exchange>
2023-09-13 15:53:22 -0700

    Refactor self-nits.

--- protocol/app/stoppable/stoppable_test.go
+++ protocol/app/stoppable/stoppable_test.go
@@ -18,25 +18,25 @@ func (m *TestStoppable) Stop() {
 }
 
 func TestStopServices(t *testing.T) {
-	mockStoppable := &TestStoppable{}
-	mockStoppable2 := &TestStoppable{}
-	mockStoppableSeparateTest := &TestStoppable{}
-	stoppable.RegisterServiceForTestCleanup("test", mockStoppable)
-	stoppable.RegisterServiceForTestCleanup("test", mockStoppable2)
-	stoppable.RegisterServiceForTestCleanup("test2", mockStoppableSeparateTest)
+	testStoppable := &TestStoppable{}
+	testStoppable2 := &TestStoppable{}
+	testStoppableSeparateTest := &TestStoppable{}
+	stoppable.RegisterServiceForTestCleanup("test", testStoppable)
+	stoppable.RegisterServiceForTestCleanup("test", testStoppable2)
+	stoppable.RegisterServiceForTestCleanup("test2", testStoppableSeparateTest)
 
 	// Stop test services, verify.
 	stoppable.StopServices(t, "test")
 
 	// Verify test services stopped, test2 services unaffected.
-	require.True(t, mockStoppable.stopCalled)
-	require.True(t, mockStoppable2.stopCalled)
-	require.False(t, mockStoppableSeparateTest.stopCalled)
+	require.True(t, testStoppable.stopCalled)
+	require.True(t, testStoppable2.stopCalled)
+	require.False(t, testStoppableSeparateTest.stopCalled)
 
 	// Stop test services again. This should not cause any panics.
 	stoppable.StopServices(t, "test")
 
 	// Stop test2 services, verify.
 	stoppable.StopServices(t, "test2")
-	require.True(t, mockStoppableSeparateTest.stopCalled)
+	require.True(t, testStoppableSeparateTest.stopCalled)
 }
