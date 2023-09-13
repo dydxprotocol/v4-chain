8c917e04cb8e7d2d9954aa9855dcdd6fdf55f1ec
Brendan Chou <3680392+BrendanChou@users.noreply.github.com>
2023-09-13 17:26:36 -0400

    remove ShallowCopy

--- protocol/lib/maps/util.go
+++ protocol/lib/maps/util.go
@@ -18,15 +18,3 @@ func MergeAllMapsMustHaveDistinctKeys[K comparable, V any](maps ...map[K]V) map[
 	}
 	return combinedMap
 }
-
-// ShallowCopy returns a shallow copy of originalMap.
-func ShallowCopy[K comparable, V any](originalMap map[K]V) map[K]V {
-	if originalMap == nil {
-		return nil
-	}
-	newMap := make(map[K]V)
-	for k, v := range originalMap {
-		newMap[k] = v
-	}
-	return newMap
-}
