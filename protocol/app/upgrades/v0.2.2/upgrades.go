5391d79440388e549019ca5ec7b027e239219180
Jay Yu <103467857+jayy04@users.noreply.github.com>
2023-09-13 19:17:54 -0400

    update numbers

--- protocol/app/upgrades/v0.2.2/upgrades.go
+++ protocol/app/upgrades/v0.2.2/upgrades.go
@@ -22,71 +22,104 @@ func CreateUpgradeHandler(
 		for _, clobPair := range clobPairs {
 			switch clobPair.Id {
 			case 0:
-				clobPair.SubticksPerTick = 1
+				clobPair.SubticksPerTick = 1e4
+				clobPair.QuantumConversionExponent = -8
 			case 1:
-				clobPair.SubticksPerTick = 1
+				clobPair.SubticksPerTick = 1e5
+				clobPair.QuantumConversionExponent = -9
 			case 2:
-				clobPair.SubticksPerTick = 1
+				clobPair.SubticksPerTick = 1e6
+				clobPair.QuantumConversionExponent = -9
 			case 3:
-				clobPair.SubticksPerTick = 1
+				clobPair.SubticksPerTick = 1e7
+				clobPair.QuantumConversionExponent = -10
 			case 4:
-				clobPair.SubticksPerTick = 1
+				clobPair.SubticksPerTick = 1e7
+				clobPair.QuantumConversionExponent = -10
 			case 5:
-				clobPair.SubticksPerTick = 1
+				clobPair.SubticksPerTick = 1e5
+				clobPair.QuantumConversionExponent = -8
 			case 6:
-				clobPair.SubticksPerTick = 1
+				clobPair.SubticksPerTick = 1e7
+				clobPair.QuantumConversionExponent = -10
 			case 7:
-				clobPair.SubticksPerTick = 1
+				clobPair.SubticksPerTick = 1e5
+				clobPair.QuantumConversionExponent = -8
 			case 8:
-				clobPair.SubticksPerTick = 1
+				clobPair.SubticksPerTick = 1e6
+				clobPair.QuantumConversionExponent = -9
 			case 9:
-				clobPair.SubticksPerTick = 1
+				clobPair.SubticksPerTick = 1e5
+				clobPair.QuantumConversionExponent = -8
 			case 10:
-				clobPair.SubticksPerTick = 1
+				clobPair.SubticksPerTick = 1e8
+				clobPair.QuantumConversionExponent = -11
 			case 11:
-				clobPair.SubticksPerTick = 1
+				clobPair.SubticksPerTick = 1e6
+				clobPair.QuantumConversionExponent = -9
 			case 12:
-				clobPair.SubticksPerTick = 1
+				clobPair.SubticksPerTick = 1e6
+				clobPair.QuantumConversionExponent = -9
 			case 13:
-				clobPair.SubticksPerTick = 1
+				clobPair.SubticksPerTick = 1e6
+				clobPair.QuantumConversionExponent = -9
 			case 14:
-				clobPair.SubticksPerTick = 1
+				clobPair.SubticksPerTick = 1e4
+				clobPair.QuantumConversionExponent = -7
 			case 15:
-				clobPair.SubticksPerTick = 1
+				clobPair.SubticksPerTick = 1e8
+				clobPair.QuantumConversionExponent = -11
 			case 16:
-				clobPair.SubticksPerTick = 1
+				clobPair.SubticksPerTick = 1e6
+				clobPair.QuantumConversionExponent = -9
 			case 17:
-				clobPair.SubticksPerTick = 1
+				clobPair.SubticksPerTick = 1e3
+				clobPair.QuantumConversionExponent = -6
 			case 18:
-				clobPair.SubticksPerTick = 1
+				clobPair.SubticksPerTick = 1e7
+				clobPair.QuantumConversionExponent = -10
 			case 19:
-				clobPair.SubticksPerTick = 1
+				clobPair.SubticksPerTick = 1e5
+				clobPair.QuantumConversionExponent = -8
 			case 20:
-				clobPair.SubticksPerTick = 1
+				clobPair.SubticksPerTick = 1e5
+				clobPair.QuantumConversionExponent = -8
 			case 21:
-				clobPair.SubticksPerTick = 1
+				clobPair.SubticksPerTick = 1e6
+				clobPair.QuantumConversionExponent = -9
 			case 22:
-				clobPair.SubticksPerTick = 1
+				clobPair.SubticksPerTick = 1e6
+				clobPair.QuantumConversionExponent = -9
 			case 23:
-				clobPair.SubticksPerTick = 1
+				clobPair.SubticksPerTick = 1e6
+				clobPair.QuantumConversionExponent = -9
 			case 24:
-				clobPair.SubticksPerTick = 1
+				clobPair.SubticksPerTick = 1e6
+				clobPair.QuantumConversionExponent = -9
 			case 25:
-				clobPair.SubticksPerTick = 1
+				clobPair.SubticksPerTick = 1e7
+				clobPair.QuantumConversionExponent = -10
 			case 26:
-				clobPair.SubticksPerTick = 1
+				clobPair.SubticksPerTick = 1e6
+				clobPair.QuantumConversionExponent = -9
 			case 27:
-				clobPair.SubticksPerTick = 1
+				clobPair.SubticksPerTick = 1e6
+				clobPair.QuantumConversionExponent = -9
 			case 28:
-				clobPair.SubticksPerTick = 1
+				clobPair.SubticksPerTick = 1e8
+				clobPair.QuantumConversionExponent = -11
 			case 29:
-				clobPair.SubticksPerTick = 1
+				clobPair.SubticksPerTick = 1e7
+				clobPair.QuantumConversionExponent = -10
 			case 30:
-				clobPair.SubticksPerTick = 1
+				clobPair.SubticksPerTick = 1e8
+				clobPair.QuantumConversionExponent = -11
 			case 31:
-				clobPair.SubticksPerTick = 1
+				clobPair.SubticksPerTick = 1e7
+				clobPair.QuantumConversionExponent = -10
 			case 32:
-				clobPair.SubticksPerTick = 1
+				clobPair.SubticksPerTick = 1e7
+				clobPair.QuantumConversionExponent = -10
 			}
 			clobKeeper.UnsafeSetClobPair(
 				ctx,
