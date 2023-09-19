d7f197e439ddedde9278f4660905d3635ecf72d7
Vincent Chau <99756290+vincentwschau@users.noreply.github.com>
2023-09-18 20:31:50 -0400

    Fix lint.

--- indexer/packages/postgres/src/db/migrations/migration_files/20230918143243_update_subaccount_table_add_updated_at_index.ts
+++ indexer/packages/postgres/src/db/migrations/migration_files/20230918143243_update_subaccount_table_add_updated_at_index.ts
@@ -1,4 +1,4 @@
-import * as Knex from "knex";
+import * as Knex from 'knex';
 
 // Use raw SQL to ensure index creation / deletion does not lock the table
 export async function up(knex: Knex): Promise<void> {
@@ -6,7 +6,6 @@ export async function up(knex: Knex): Promise<void> {
     CREATE INDEX CONCURRENTLY IF NOT EXISTS "subaccounts_updatedat_index" on "subaccounts" ("updatedAt");
   `);
 }
-
 
 export async function down(knex: Knex): Promise<void> {
   await knex.raw(`
