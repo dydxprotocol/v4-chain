d7345879ff6e2b639897b49ecad1c333dcfd696c
Tian Qin <tian@dydx.exchange>
2023-09-20 16:50:08 -0400

    add basic validation to bridge governance messages

--- protocol/x/bridge/types/params.go
+++ protocol/x/bridge/types/params.go
@@ -1,11 +1,16 @@
 package types
 
 import (
+	errorsmod "cosmossdk.io/errors"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/dydxprotocol/v4-chain/protocol/lib"
 )
 
 func (m *EventParams) Validate() error {
-	return nil
+	if m.EthAddress == "" {
+		return errorsmod.Wrap(ErrInvalidEthAddress, "Ethereum contract address cannot be empty")
+	}
+	return sdk.ValidateDenom(m.Denom)
 }
 
 func (m *ProposeParams) Validate() error {
