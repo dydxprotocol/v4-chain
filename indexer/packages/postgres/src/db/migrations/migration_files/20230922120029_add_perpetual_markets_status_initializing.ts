1d22801847f7ac1b0c298c863ba65fa5bbb6d8da
Christopher-Li <Christopher-Li@users.noreply.github.com>
2023-09-22 14:04:08 -0700

    [IND-382]: Add INITIALIZING to valid perpetual_markets.status (#353)

---
+++ indexer/packages/postgres/src/db/migrations/migration_files/20230922120029_add_perpetual_markets_status_initializing.ts
@@ -0,0 +1,19 @@
+import * as Knex from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.raw(`
+    ALTER TABLE "perpetual_markets"
+    DROP CONSTRAINT "perpetual_markets_status_check",
+    ADD CONSTRAINT "perpetual_markets_status_check" 
+    CHECK (status IN ('ACTIVE', 'PAUSED', 'CANCEL_ONLY', 'POST_ONLY', 'INITIALIZING'))
+  `);
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.raw(`
+    ALTER TABLE "perpetual_markets"
+    DROP CONSTRAINT "perpetual_markets_status_check",
+    ADD CONSTRAINT "perpetual_markets_status_check" 
+    CHECK (status IN ('ACTIVE', 'PAUSED', 'CANCEL_ONLY', 'POST_ONLY'))
+  `);
+}
