858f1cb0d0b1ba47dd58156f4cea011c2c1aa873
Teddy Ding <teddy@dydx.exchange>
2023-09-22 14:01:33 -0400

    nit

--- protocol/x/prices/types/constants.go
+++ protocol/x/prices/types/constants.go
@@ -3,6 +3,6 @@ package types
 import "time"
 
 const (
-	MarketIsRecentDuration    = 20 * time.Second
-	PriceInitializationBlocks = 10
+	MarketIsRecentDuration          = 20 * time.Second
+	PriceDaemonInitializationBlocks = 20
 )
