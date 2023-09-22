c84c83f48e08e0e89d471084c270f93aa3d4693c
Crystal Lemire <crystal.lemire@gmail.com>
2023-09-22 14:58:24 -0700

    [DEC-2084] - `lib/error` audit refactors (#309)

--- protocol/lib/error/log_contextualizer.go
+++ protocol/lib/error/log_contextualizer.go
@@ -2,6 +2,10 @@ package error
 
 import "github.com/cometbft/cometbft/libs/log"
 
+// LogContextualizer describes an object that can add context - that is, descriptive key-value pairs, to a logger.
+// This interface is implemented by ErrorWithLogContext, which wraps some errors that are returned from the protocol.
+// This can be used, for example, to add context to log statements from the process proposal handler that helps with
+// debugging erroneous unexpected and/or non-deterministic behaviors.
 type LogContextualizer interface {
 	AddLoggingContext(logger log.Logger) log.Logger
 	Unwrap() error
