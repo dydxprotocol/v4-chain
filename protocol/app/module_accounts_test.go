c8e83b5090bdba17893b8c24569f07ba59eb567d
Lukasz Cwik <lcwik@apache.org>
2023-09-26 13:47:46 -0700

    [DEC-2081] Audit app/module_accounts.go
    Added the single test that was missing.

--- protocol/app/module_accounts_test.go
+++ protocol/app/module_accounts_test.go
@@ -71,3 +71,23 @@ func TestMaccPerms(t *testing.T) {
 	}
 	require.Equal(t, expectedMaccPerms, maccPerms, "default macc perms list does not match expected")
 }
+
+func TestModuleAccountAddrs(t *testing.T) {
+	expectedModuleAccAddresses := map[string]bool{
+		"dydx17xpfvakm2amg962yls6f84z3kell8c5leqdyt2": true,
+		"dydx1zlefkpe3g0vvm9a4h0jf9000lmqutlh9jwjnsv": true,
+		"dydx1jv65s3grqf6v6jl3dp4t6c9t9rk99cd8wx2cfg": true,
+		"dydx1fl48vsnmsdzcv85q5d2q4z5ajdha8yu3uz8teq": true,
+		"dydx1tygms3xhhs3yv487phx3dw4a95jn7t7lgzm605": true,
+		"dydx10d07y265gmmuvt4z0w9aw880jnsr700jnmapky": true,
+		"dydx1yl6hdjhmkf37639730gffanpzndzdpmh8xcdh5": true,
+		"dydx1v88c3xv9xyv3eetdx0tvcmq7ung3dywp5upwc6": true,
+		"dydx1c7ptc87hkd54e3r7zjy92q29xkq7t79w64slrq": true,
+		"dydx16wrau2x4tsg033xfrrdpae6kxfn9kyuerr5jjp": true,
+		"dydx1ltyc6y4skclzafvpznpt2qjwmfwgsndp458rmp": true,
+		"dydx15ztc7xy42tn2ukkc0qjthkucw9ac63pgp70urn": true,
+		"dydx1wxje320an3karyc6mjw4zghs300dmrjkwn7xtk": true,
+	}
+
+	require.Equal(t, expectedModuleAccAddresses, app.ModuleAccountAddrs())
+}
