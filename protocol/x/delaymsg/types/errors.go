b049bb53fdb07935f8ff41515bcafdbfa3d50a0b
Crystal Lemire <crystal.lemire@gmail.com>
2023-10-03 13:49:24 -0700

    [DEC-2065] ValidateBasic in msgServer.DelayMessage (#411)

--- protocol/x/delaymsg/types/errors.go
+++ protocol/x/delaymsg/types/errors.go
@@ -4,10 +4,11 @@ import errorsmod "cosmossdk.io/errors"
 
 // x/delaymsg module sentinel errors
 var (
-	ErrInvalidInput    = errorsmod.Register(ModuleName, 1, "Invalid input")
-	ErrMsgIsNil        = errorsmod.Register(ModuleName, 2, "Delayed msg is nil")
-	ErrMsgIsUnroutable = errorsmod.Register(ModuleName, 3, "Message not recognized by router")
-	ErrInvalidSigner   = errorsmod.Register(ModuleName, 4, "Invalid signer")
+	ErrInvalidInput     = errorsmod.Register(ModuleName, 1, "Invalid input")
+	ErrMsgIsNil         = errorsmod.Register(ModuleName, 2, "Delayed msg is nil")
+	ErrMsgIsUnroutable  = errorsmod.Register(ModuleName, 3, "Message not recognized by router")
+	ErrInvalidSigner    = errorsmod.Register(ModuleName, 4, "Invalid signer")
+	ErrInvalidAuthority = errorsmod.Register(ModuleName, 5, "Invalid authority")
 
 	ErrInvalidGenesisState = errorsmod.Register(ModuleName, 10, "Invalid genesis state")
 )
