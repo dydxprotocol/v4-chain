3063828c3f6059def2245d5f2ab34d0b1a88d82f
Brendan Chou <3680392+BrendanChou@users.noreply.github.com>
2023-10-07 11:44:30 -0400

    fix test name

--- protocol/x/clob/types/order_id_test.go
+++ protocol/x/clob/types/order_id_test.go
@@ -16,7 +16,7 @@ import (
 // `OrderFlags` is greater than one byte (proto varints are encoded with 7 bits per byte).
 const numOrderIdFlagsTestCases = 129
 
-func TestMustMarshal(t *testing.T) {
+func TestToStateKey(t *testing.T) {
 	// Success
 	b, _ := constants.OrderId_Alice_Num0_ClientId0_Clob0.Marshal()
 	require.Equal(t, b, constants.OrderId_Alice_Num0_ClientId0_Clob0.ToStateKey())
