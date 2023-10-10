387613431fa1a5e522fa9311ca5ff069e13c7c76
Lukasz Cwik <lcwik@apache.org>
2023-10-10 13:06:31 -0700

    Fix indexer generated files.

--- indexer/packages/v4-protos/src/codegen/google/bundle.ts
+++ indexer/packages/v4-protos/src/codegen/google/bundle.ts
@@ -1,10 +1,9 @@
 import * as _92 from "./api/annotations";
 import * as _93 from "./api/http";
-import * as _94 from "./protobuf/any";
-import * as _95 from "./protobuf/descriptor";
-import * as _96 from "./protobuf/duration";
-import * as _97 from "./protobuf/timestamp";
-import * as _98 from "./protobuf/wrappers";
+import * as _94 from "./protobuf/descriptor";
+import * as _95 from "./protobuf/duration";
+import * as _96 from "./protobuf/timestamp";
+import * as _97 from "./protobuf/any";
 export namespace google {
   export const api = { ..._92,
     ..._93
@@ -12,7 +11,6 @@ export namespace google {
   export const protobuf = { ..._94,
     ..._95,
     ..._96,
-    ..._97,
-    ..._98
+    ..._97
   };
 }
