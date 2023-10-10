2c7ad1e37970c5db61b162187e9238933c268547
Jonathan Fung <jonathan@dydx.exchange>
2023-10-10 13:08:05 -0400

    remove pb gen files

--- protocol/daemons/server/metrics.go
+++ protocol/daemons/server/metrics.go
@@ -6,10 +6,6 @@ import (
 	"github.com/dydxprotocol/v4-chain/protocol/daemons/server/types"
 )
 
-// MetricsServer defines the fields required for emitting periodic metrics on app versfion..
-type MetricsServer struct {
-}
-
 // ExpectMetricsDaemon registers the periodic metrics daemon with the server. This is required
 // in order to ensure that the daemon service is called at least once during every
 // maximumAcceptableUpdateDelay duration. It will cause the protocol to panic if the daemon does not
