365aa5744ac25fe0de2a4484e10d36bc499bad60
Roy Li <roy@dydx.exchange>
2023-10-18 10:55:59 -0400

    Add some error handling

--- docs/release_note_generator/main.py
+++ docs/release_note_generator/main.py
@@ -1,8 +1,9 @@
 # Usage:
 # GITHUB_TOKEN=<token> python main.py --old <old commit> --new <new commit> --path <path>
 #
-# The above command will create release notes for changes between the <old> commit and the <new> commit.
-# Only commits that change files in <path> will be included. A github token is required to avoid rate limits.
+# The above command will emit markdown release notes down to stdout for changes between the <old commit>
+# and the <new commit>. Only commits that change files in <path> will be included. A github token is
+# required to avoid rate limits.
 
 import argparse
 import os
@@ -16,11 +17,13 @@ LIST_COMMIT_PULLS_ENDPOINT = "https://api.github.com/repos/dydxprotocol/v4-chain
 
 def get_commit(session, commit_sha):
     r = session.get(GET_COMMIT_ENDPOINT % commit_sha)
+    r.raise_for_status()
     return r.json()
 
 # Return a string that is a markdown list entry. It should summarize the changes in this commit.
 def commit_to_entry(session, commit_json):
     r = session.get(LIST_COMMIT_PULLS_ENDPOINT % commit_json["sha"])
+    r.raise_for_status()
     pulls = r.json()
 
     if len(pulls) > 1:
@@ -47,6 +50,7 @@ def get_release_notes(session, new, old, path):
         "since": old_json["commit"]["author"]["date"],
         "path": path,
     })
+    r.raise_for_status()
 
     # old commit will be the last commit iff it changes files in path. If it's there, remove it.
     commits = r.json()
@@ -65,6 +69,9 @@ if __name__ == "__main__":
     parser.add_argument("--new", help="Newest commit, inclusive")
     parser.add_argument("--path", help="Path to filter commits by")
     args = parser.parse_args()
+
+    if not os.getenv(GITHUB_TOKEN_ENV_VAR):
+        raise ValueError("Environment variable %s is not set." % GITHUB_TOKEN_ENV_VAR)
 
     session = requests.Session()
     session.headers.update({'Authorization': 'Bearer %s' % os.getenv(GITHUB_TOKEN_ENV_VAR)})
