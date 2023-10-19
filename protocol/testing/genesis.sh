04ac6662bc9e5666f7ece0835020cd76026fae20
Taehoon Lee <19664986+ttl33@users.noreply.github.com>
2023-10-19 17:48:18 -0400

    apply to dev/staging genesis

--- protocol/testing/genesis.sh
+++ protocol/testing/genesis.sh
@@ -83,6 +83,8 @@ function edit_genesis() {
 	dasel put -t string -f "$GENESIS" '.app_state.gov.params.max_deposit_period' -v '300s'
 	# reduced voting period
 	dasel put -t string -f "$GENESIS" '.app_state.gov.params.voting_period' -v '300s'
+	# set initial deposit ratio to prevent spamming
+	dasel put -t string -f "$GENESIS" '.app_state.gov.params.min_initial_deposit_ratio' -v '0.20000' # 20%
 
 	# Update staking module.
 	dasel put -t string -f "$GENESIS" '.app_state.staking.params.unbonding_time' -v '1814400s' # 21 days
