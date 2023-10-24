31da9388daeeb49ed0bd30112cb5a276d5887cbd
Will Liu <will@dydx.exchange>
2023-10-24 13:41:46 -0400

    fix

--- indexer/services/scripts/src/helpers/block-helpers.ts
+++ indexer/services/scripts/src/helpers/block-helpers.ts
@@ -38,66 +38,77 @@ export function annotateIndexerTendermintEvent(
     case (DydxIndexerSubtypes.ORDER_FILL.toString()): {
       return {
         ...event,
+        dataBytes: new Uint8Array(),
         data: JSON.stringify(OrderFillEventV1.decode(eventDataBinary)),
       };
     }
     case (DydxIndexerSubtypes.SUBACCOUNT_UPDATE.toString()): {
       return {
         ...event,
+        dataBytes: new Uint8Array(),
         data: JSON.stringify(SubaccountUpdateEventV1.decode(eventDataBinary)),
       };
     }
     case (DydxIndexerSubtypes.TRANSFER.toString()): {
       return {
         ...event,
+        dataBytes: new Uint8Array(),
         data: JSON.stringify(TransferEventV1.decode(eventDataBinary)),
       };
     }
     case (DydxIndexerSubtypes.MARKET.toString()): {
       return {
         ...event,
+        dataBytes: new Uint8Array(),
         data: JSON.stringify(MarketEventV1.decode(eventDataBinary)),
       };
     }
     case (DydxIndexerSubtypes.STATEFUL_ORDER.toString()): {
       return {
         ...event,
+        dataBytes: new Uint8Array(),
         data: JSON.stringify(StatefulOrderEventV1.decode(eventDataBinary)),
       };
     }
     case (DydxIndexerSubtypes.FUNDING.toString()): {
       return {
         ...event,
+        dataBytes: new Uint8Array(),
         data: JSON.stringify(FundingEventV1.decode(eventDataBinary)),
       };
     }
     case (DydxIndexerSubtypes.ASSET.toString()): {
       return {
         ...event,
+        dataBytes: new Uint8Array(),
         data: JSON.stringify(AssetCreateEventV1.decode(eventDataBinary)),
       };
     }
     case (DydxIndexerSubtypes.PERPETUAL_MARKET.toString()): {
       return {
         ...event,
+        dataBytes: new Uint8Array(),
         data: JSON.stringify(PerpetualMarketCreateEventV1.decode(eventDataBinary)),
       };
     }
     case (DydxIndexerSubtypes.LIQUIDITY_TIER.toString()): {
       return {
         ...event,
+        dataBytes: new Uint8Array(),
         data: JSON.stringify(LiquidityTierUpsertEventV1.decode(eventDataBinary)),
       };
     }
     case (DydxIndexerSubtypes.UPDATE_PERPETUAL.toString()): {
       return {
         ...event,
+        dataBytes: new Uint8Array(),
         data: JSON.stringify(UpdatePerpetualEventV1.decode(eventDataBinary)),
       };
     }
     case (DydxIndexerSubtypes.UPDATE_CLOB_PAIR.toString()): {
       return {
         ...event,
+        dataBytes: new Uint8Array(),
         data: JSON.stringify(UpdateClobPairEventV1.decode(eventDataBinary)),
       };
     }
