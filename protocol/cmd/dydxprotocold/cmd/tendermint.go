7b82834497ffe72c7f348ee9f7ec1e5cea55d730
Jay Yu <103467857+jayy04@users.noreply.github.com>
2023-10-29 20:51:37 -0400

    Add debug tendermint command

--- protocol/cmd/dydxprotocold/cmd/tendermint.go
+++ protocol/cmd/dydxprotocold/cmd/tendermint.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"os"
 
+	debug "github.com/cometbft/cometbft/cmd/cometbft/commands/debug"
 	"github.com/dydxprotocol/v4-chain/protocol/command/genprivkey"
 	"github.com/spf13/cobra"
 )
@@ -18,4 +19,5 @@ func AddTendermintSubcommands(rootCmd *cobra.Command) {
 	// Add "gen-priv-key" command to Tendermint subcommand.
 	// TODO(DEC-1079): Remove this command after updating to Cosmos `0.46.X` in favor of `init --recover`.
 	tmCmd.AddCommand(genprivkey.Command())
+	tmCmd.AddCommand(debug.DebugCmd)
 }
