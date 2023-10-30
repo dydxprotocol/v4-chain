826162592a4f939edf833e339122c32ab1d4a54f
roy-dydx <133032749+roy-dydx@users.noreply.github.com>
2023-10-30 14:47:07 -0400

    Make iaviewer usage more human friendly (#719)
    * Make iaviewer usage more human friendly
    
    * Add comment
    
    * lint

--- protocol/scripts/iaviewer/iaviewer.go
+++ protocol/scripts/iaviewer/iaviewer.go
@@ -8,12 +8,12 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"strconv"
+	"regexp"
 	"strings"
 
 	dbm "github.com/cometbft/cometbft-db"
-
 	"github.com/cosmos/iavl"
+	"github.com/spf13/cobra"
 )
 
 // TODO: make this configurable?
@@ -21,46 +21,164 @@ const (
 	DefaultCacheSize int = 10000
 )
 
+var (
+	prefixRe *regexp.Regexp = regexp.MustCompile("(s/k:[[:alpha:]]+/).*")
+)
+
+var rootCmd = &cobra.Command{
+	Use:          "iaviewer",
+	SilenceUsage: true,
+}
+
 func main() {
-	args := os.Args[1:]
-	if len(args) < 3 || (args[0] != "data" && args[0] != "shape" && args[0] != "versions") {
-		fmt.Fprintln(os.Stderr, "Usage: iaviewer <data|shape|versions> <leveldb dir> <prefix> [version number]")
-		fmt.Fprintln(os.Stderr, "<prefix> is the prefix of db, and the iavl tree of different modules in cosmos-sdk uses ")
-		fmt.Fprintln(os.Stderr, "different <prefix> to identify, just like \"s/k:gov/\" represents the prefix of gov module")
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "iaviewer got error: '%s'", err)
 		os.Exit(1)
 	}
+}
 
-	version := 0
-	if len(args) == 4 {
-		var err error
-		version, err = strconv.Atoi(args[3])
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Invalid version number: %s\n", err)
-			os.Exit(1)
-		}
+func init() {
+	rootCmd.AddCommand(DataAllCmd())
+	rootCmd.AddCommand(DataCmd())
+	rootCmd.AddCommand(ShapeCmd())
+	rootCmd.AddCommand(VersionsCmd())
+}
+
+func AddPathFlag(cmd *cobra.Command) {
+	cmd.Flags().String("path", "", "path to leveldb dir")
+	if err := cmd.MarkFlagRequired("path"); err != nil {
+		panic(err)
+	}
+}
+
+func AddPrefixFlag(cmd *cobra.Command) {
+	cmd.Flags().String("prefix", "", "prefix of the db e.g. \"s/k:gov/\"")
+	if err := cmd.MarkFlagRequired("prefix"); err != nil {
+		panic(err)
+	}
+}
+
+func AddVersionFlag(cmd *cobra.Command) {
+	cmd.Flags().Uint64("version", 0, "version of the tree (default: latest version)")
+}
+
+func DataAllCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "data-all",
+		Short: "Get data of all iavl trees",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			path, _ := cmd.Flags().GetString("path")
+			version, _ := cmd.Flags().GetUint64("version")
+
+			db, err := OpenDB(path)
+			if err != nil {
+				return err
+			}
+			prefixes := GetAllPrefixes(db)
+			for _, prefix := range prefixes {
+				tree, err := ReadTree(db, version, []byte(prefix))
+				if err != nil {
+					return err
+				}
+				err = PrintTree(tree, prefix)
+				if err != nil {
+					return err
+				}
+			}
+			return nil
+		},
 	}
 
-	tree, err := ReadTree(args[1], version, []byte(args[2]))
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error reading data: %s\n", err)
-		os.Exit(1)
+	AddPathFlag(cmd)
+	AddVersionFlag(cmd)
+	return cmd
+}
+
+func DataCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "data",
+		Short: "Get data of a specific iavl tree",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			path, _ := cmd.Flags().GetString("path")
+			prefix, _ := cmd.Flags().GetString("prefix")
+			version, _ := cmd.Flags().GetUint64("version")
+
+			db, err := OpenDB(path)
+			if err != nil {
+				return err
+			}
+			tree, err := ReadTree(db, version, []byte(prefix))
+			if err != nil {
+				return err
+			}
+			err = PrintTree(tree, prefix)
+			if err != nil {
+				return err
+			}
+			return nil
+		},
 	}
 
-	switch args[0] {
-	case "data":
-		PrintKeys(tree)
-		hash, err := tree.Hash()
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error hashing tree: %s\n", err)
-			os.Exit(1)
-		}
-		fmt.Printf("Hash: %X\n", hash)
-		fmt.Printf("Size: %X\n", tree.Size())
-	case "shape":
-		PrintShape(tree)
-	case "versions":
-		PrintVersions(tree)
+	AddPathFlag(cmd)
+	AddPrefixFlag(cmd)
+	AddVersionFlag(cmd)
+	return cmd
+}
+
+func ShapeCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "shape",
+		Short: "Get shape of a specific iavl tree",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			path, _ := cmd.Flags().GetString("path")
+			prefix, _ := cmd.Flags().GetString("prefix")
+			version, _ := cmd.Flags().GetUint64("version")
+
+			db, err := OpenDB(path)
+			if err != nil {
+				return err
+			}
+			tree, err := ReadTree(db, version, []byte(prefix))
+			if err != nil {
+				return err
+			}
+			PrintShape(tree)
+			return nil
+		},
 	}
+
+	AddPathFlag(cmd)
+	AddPrefixFlag(cmd)
+	AddVersionFlag(cmd)
+	return cmd
+}
+
+func VersionsCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "versions",
+		Short: "List versions of specific iavl tree",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			path, _ := cmd.Flags().GetString("path")
+			prefix, _ := cmd.Flags().GetString("prefix")
+			version, _ := cmd.Flags().GetUint64("version")
+
+			db, err := OpenDB(path)
+			if err != nil {
+				return err
+			}
+			tree, err := ReadTree(db, version, []byte(prefix))
+			if err != nil {
+				return err
+			}
+			PrintVersions(tree)
+			return nil
+		},
+	}
+
+	AddPathFlag(cmd)
+	AddPrefixFlag(cmd)
+	AddVersionFlag(cmd)
+	return cmd
 }
 
 func OpenDB(dir string) (dbm.DB, error) {
@@ -91,6 +209,31 @@ func OpenDB(dir string) (dbm.DB, error) {
 	return db, nil
 }
 
+// GetAllPrefixes fetches all prefixes for iavl trees in the DB. There's no great way to do this,
+// so we just iterate through all keys and look for prefixes that match what we expect from modules.
+func GetAllPrefixes(db dbm.DB) []string {
+	it, err := db.Iterator(nil, nil)
+	if err != nil {
+		panic(err)
+	}
+	defer it.Close()
+	// Luckily for us, this iterator goes in alphabetical order
+	var prefixes []string
+	for ; it.Valid(); it.Next() {
+		k := it.Key()
+		matches := prefixRe.FindStringSubmatch(string(k))
+		if matches != nil {
+			if len(prefixes) == 0 || matches[1] != prefixes[len(prefixes)-1] {
+				prefixes = append(prefixes, matches[1])
+			}
+		}
+	}
+	if err := it.Error(); err != nil {
+		panic(err)
+	}
+	return prefixes
+}
+
 func PrintDBStats(db dbm.DB) {
 	count := 0
 	prefix := map[string]int{}
@@ -114,14 +257,10 @@ func PrintDBStats(db dbm.DB) {
 	}
 }
 
-// ReadTree loads an iavl tree from the directory
+// ReadTree loads an iavl tree from a db
 // If version is 0, load latest, otherwise, load named version
 // The prefix represents which iavl tree you want to read. The iaviwer will always set a prefix.
-func ReadTree(dir string, version int, prefix []byte) (*iavl.MutableTree, error) {
-	db, err := OpenDB(dir)
-	if err != nil {
-		return nil, err
-	}
+func ReadTree(db dbm.DB, version uint64, prefix []byte) (*iavl.MutableTree, error) {
 	if len(prefix) != 0 {
 		db = dbm.NewPrefixDB(db, prefix)
 	}
@@ -131,18 +270,34 @@ func ReadTree(dir string, version int, prefix []byte) (*iavl.MutableTree, error)
 		return nil, err
 	}
 	ver, err := tree.LoadVersion(int64(version))
-	fmt.Printf("Got version: %d\n", ver)
+	fmt.Printf("Tree %s version: %d\n", prefix, ver)
 	return tree, err
 }
 
-func PrintKeys(tree *iavl.MutableTree) {
-	fmt.Println("Printing all keys with hashed values (to detect diff)")
+func PrintTree(tree *iavl.MutableTree, prefix string) error {
+	fmt.Printf("Tree %s data:\n", prefix)
 	tree.Iterate(func(key []byte, value []byte) bool { //nolint:errcheck
 		printKey := parseWeaveKey(key)
 		digest := sha256.Sum256(value)
 		fmt.Printf("  %s\n    %X\n", printKey, digest)
+		if treeUnmarshallerRegistry, ok := unmarshallerRegistry[prefix]; ok {
+			for keyPrefix, unmarshaller := range treeUnmarshallerRegistry {
+				if strings.HasPrefix(string(key), keyPrefix) {
+					str := unmarshaller(value)
+					fmt.Printf("    %s\n", str)
+					break
+				}
+			}
+		}
 		return false
 	})
+	hash, err := tree.Hash()
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Tree %s Hash: %X\n", prefix, hash)
+	fmt.Printf("Tree %s Size: %X\n", prefix, tree.Size())
+	return nil
 }
 
 // parseWeaveKey assumes a separating : where all in front should be ascii,
