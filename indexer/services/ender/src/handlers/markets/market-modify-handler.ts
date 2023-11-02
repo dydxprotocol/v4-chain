674b625283bb8972e98e190ee74504c4921f51eb
Lukasz Cwik <lcwik@apache.org>
2023-11-02 11:40:59 -0700

    [IND-468] Push ender market modify logic to use a single SQL function

--- indexer/services/ender/src/handlers/markets/market-modify-handler.ts
+++ indexer/services/ender/src/handlers/markets/market-modify-handler.ts
@@ -1,9 +1,11 @@
 import { logger } from '@dydxprotocol-indexer/base';
 import {
-  MarketFromDatabase, MarketUpdateObject, MarketTable, marketRefresher,
+  MarketFromDatabase, MarketUpdateObject, MarketTable, marketRefresher, storeHelpers, MarketModel,
 } from '@dydxprotocol-indexer/postgres';
 import { MarketEventV1 } from '@dydxprotocol-indexer/v4-protos';
+import * as pg from 'pg';
 
+import config from '../../config';
 import { ConsolidatedKafkaEvent, MarketModifyEventMessage } from '../../lib/types';
 import { Handler } from '../handler';
 
@@ -17,6 +19,18 @@ export class MarketModifyHandler extends Handler<MarketEventV1> {
 
   public async internalHandle(): Promise<ConsolidatedKafkaEvent[]> {
     logger.info({
+      at: 'MarketCreateHandler#handle',
+      message: 'Received MarketEvent with MarketCreate.',
+      event: this.event,
+    });
+    if (config.USE_MARKET_MODIFY_HANDLER_SQL_FUNCTION) {
+      return this.handleViaSqlFunction();
+    }
+    return this.handleViaKnexQueries();
+  }
+
+  private async handleViaKnexQueries(): Promise<ConsolidatedKafkaEvent[]> {
+    logger.info({
       at: 'MarketModifyHandler#handle',
       message: 'Received MarketEvent with MarketModify.',
       event: this.event,
@@ -29,6 +43,42 @@ export class MarketModifyHandler extends Handler<MarketEventV1> {
       this.updateMarketFromEvent(castedMarketModifyMessage),
       this.generateTimingStatsOptions('update_market'),
     );
+    return [];
+  }
+
+  private async handleViaSqlFunction(): Promise<ConsolidatedKafkaEvent[]> {
+    const eventDataBinary: Uint8Array = this.indexerTendermintEvent.dataBytes;
+    const result: pg.QueryResult = await storeHelpers.rawQuery(
+      `SELECT dydx_market_modify_handler(
+        '${JSON.stringify(MarketEventV1.decode(eventDataBinary))}' 
+      ) AS result;`,
+      { txId: this.txId },
+    ).catch((error: Error) => {
+      logger.error({
+        at: 'MarketModifyHandler#handleViaSqlFunction',
+        message: 'Failed to handle MarketEventV1',
+        error,
+      });
+
+      const castedMarketModifyMessage:
+      MarketModifyEventMessage = this.event as MarketModifyEventMessage;
+
+      if (error.message.includes('Market in MarketModify doesn\'t exist')) {
+        this.logAndThrowParseMessageError(
+          'Market in MarketModify doesn\'t exist',
+          { castedMarketModifyMessage },
+        );
+      }
+
+      this.logAndThrowParseMessageError(
+        'Failed to update market in markets table',
+        { castedMarketModifyMessage },
+      );
+    });
+
+    const market: MarketFromDatabase = MarketModel.fromJson(
+      result.rows[0].result.market) as MarketFromDatabase;
+    marketRefresher.updateMarket(market);
     return [];
   }
 
