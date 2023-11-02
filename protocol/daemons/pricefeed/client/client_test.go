c325aec4afc402b587e1232dfb5677badf226a43
Crystal Lemire <crystal@dydx.exchange>
2023-11-02 16:28:35 -0700

    Refactor test util method since it will be used for all daemons.

--- protocol/daemons/pricefeed/client/client_test.go
+++ protocol/daemons/pricefeed/client/client_test.go
@@ -12,6 +12,7 @@ import (
 	pricefeed_types "github.com/dydxprotocol/v4-chain/protocol/daemons/server/types/pricefeed"
 	daemontypes "github.com/dydxprotocol/v4-chain/protocol/daemons/types"
 	"github.com/dydxprotocol/v4-chain/protocol/testutil/appoptions"
+	daemontestutils "github.com/dydxprotocol/v4-chain/protocol/testutil/daemons"
 	grpc_util "github.com/dydxprotocol/v4-chain/protocol/testutil/grpc"
 	pricetypes "github.com/dydxprotocol/v4-chain/protocol/x/prices/types"
 	"google.golang.org/grpc"
@@ -731,34 +732,6 @@ func TestPriceUpdater_Mixed(t *testing.T) {
 	}
 }
 
-// singleTickTickerAndStop creates a ticker that ticks once before the stop channel is signaled.
-func singleTickTickerAndStop() (*time.Ticker, chan bool) {
-	// Create a ticker with a duration long enough that we do not expect to see a tick within the timeframe
-	// of a normal unit test.
-	ticker := time.NewTicker(10 * time.Minute)
-	// Override the ticker's channel with a new channel we can insert into directly, and add a single tick.
-	newChan := make(chan time.Time, 1)
-	newChan <- time.Now()
-	ticker.C = newChan
-
-	stop := make(chan bool, 1)
-
-	// Start a go-routine that will signal the stop channel once the single tick is consumed.
-	go func() {
-		for {
-			// Once the single tick is consumed, stop the ticker and signal the stop channel.
-			if len(ticker.C) == 0 {
-				stop <- true
-				ticker.Stop()
-				return
-			}
-			time.Sleep(10 * time.Millisecond)
-		}
-	}()
-
-	return ticker, stop
-}
-
 func TestHealthCheck_Mixed(t *testing.T) {
 	tests := map[string]struct {
 		updateMarketPricesError error
@@ -790,7 +763,7 @@ func TestHealthCheck_Mixed(t *testing.T) {
 			mockPriceFeedClient.On("UpdateMarketPrices", grpc_util.Ctx, mock.Anything).
 				Return(nil, tc.updateMarketPricesError).Once()
 
-			ticker, stop := singleTickTickerAndStop()
+			ticker, stop := daemontestutils.SingleTickTickerAndStop()
 			client := newClient()
 
 			// Act.
