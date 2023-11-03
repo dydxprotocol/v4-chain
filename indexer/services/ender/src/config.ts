4bd3c34a0bf385e91bf5fb509115e19dcf9bd44a
Lukasz Cwik <126621805+lcwik@users.noreply.github.com>
2023-11-03 10:59:26 -0700

    [IND-470] Update asset create handler to use SQL function. (#748)

--- indexer/services/ender/src/config.ts
+++ indexer/services/ender/src/config.ts
@@ -23,7 +23,7 @@ export const configSchema = {
   SEND_WEBSOCKET_MESSAGES: parseBoolean({
     default: true,
   }),
-  USE_ORDER_HANDLER_SQL_FUNCTION: parseBoolean({
+  USE_ASSET_CREATE_HANDLER_SQL_FUNCTION: parseBoolean({
     default: true,
   }),
   USE_LIQUIDATION_HANDLER_SQL_FUNCTION: parseBoolean({
@@ -36,6 +36,9 @@ export const configSchema = {
     default: true,
   }),
   USE_MARKET_PRICE_UPDATE_HANDLER_SQL_FUNCTION: parseBoolean({
+    default: true,
+  }),
+  USE_ORDER_HANDLER_SQL_FUNCTION: parseBoolean({
     default: true,
   }),
   USE_SUBACCOUNT_UPDATE_SQL_FUNCTION: parseBoolean({
