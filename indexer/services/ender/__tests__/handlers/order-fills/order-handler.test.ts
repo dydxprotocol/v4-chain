10a2875c398c8f60818e559731222830419bc6e4
Christopher Li <Christopherfooli@gmail.com>
2023-11-03 16:14:38 -0400

    [IND-450]: Prevent orders transitioning from canceled to best effort canceled

--- indexer/services/ender/__tests__/handlers/order-fills/order-handler.test.ts
+++ indexer/services/ender/__tests__/handlers/order-fills/order-handler.test.ts
@@ -708,7 +708,7 @@ describe('OrderHandler', () => {
         totalFilled: totalMakerOrderFilled,
         price,
         status: isOrderCanceled
-          ? OrderStatus.BEST_EFFORT_CANCELED
+          ? OrderStatus.CANCELED
           : OrderStatus.OPEN, // orderSize > totalFilled so status is open
         clobPairId: defaultClobPairId,
         side: protocolTranslations.protocolOrderSideToOrderSide(makerOrderProto.side),
@@ -1472,30 +1472,52 @@ describe('OrderHandler', () => {
       'via knex',
       false,
       IndexerOrder_TimeInForce.TIME_IN_FORCE_UNSPECIFIED,
+      true,
     ],
     [
       'limit',
       'via SQL function',
       true,
       IndexerOrder_TimeInForce.TIME_IN_FORCE_UNSPECIFIED,
+      true,
     ],
     [
-      'post-only',
+      'post-only best effort canceled',
       'via knex',
       false,
       IndexerOrder_TimeInForce.TIME_IN_FORCE_POST_ONLY,
+      true,
     ],
     [
-      'post-only',
+      'post-only best effort canceled',
       'via SQL function',
       true,
       IndexerOrder_TimeInForce.TIME_IN_FORCE_POST_ONLY,
+      true,
+    ],
+    [
+      'post-only canceled',
+      'via knex',
+      false,
+      IndexerOrder_TimeInForce.TIME_IN_FORCE_POST_ONLY,
+      false,
+      OrderStatus.CANCELED,
+    ],
+    [
+      'post-only canceled',
+      'via SQL function',
+      true,
+      IndexerOrder_TimeInForce.TIME_IN_FORCE_POST_ONLY,
+      false,
+      OrderStatus.CANCELED,
     ],
   ])('correctly sets status for short term %s orders (%s)', async (
     _orderType: string,
     _name: string,
     useSqlFunction: boolean,
     timeInForce: IndexerOrder_TimeInForce,
+    isBestEffortCanceled: boolean, // either best effort canceled or canceled
+    status: OrderStatus = OrderStatus.BEST_EFFORT_CANCELED,
   ) => {
     config.USE_ORDER_HANDLER_SQL_FUNCTION = useSqlFunction;
     const transactionIndex: number = 0;
@@ -1538,7 +1560,11 @@ describe('OrderHandler', () => {
     });
 
     const makerOrderId: string = OrderTable.orderIdToUuid(makerOrderProto.orderId!);
-    await CanceledOrdersCache.addCanceledOrderId(makerOrderId, Date.now(), redisClient);
+    if (isBestEffortCanceled === true) {
+      await CanceledOrdersCache.addBestEffortCanceledOrderId(makerOrderId, Date.now(), redisClient);
+    } else {
+      await CanceledOrdersCache.addCanceledOrderId(makerOrderId, Date.now(), redisClient);
+    }
 
     const fillAmount: number = 10;
     const orderFillEvent: OrderFillEventV1 = createOrderFillEvent(
@@ -1587,7 +1613,7 @@ describe('OrderHandler', () => {
     expect(takerOrder).toBeDefined();
 
     // maker order is partially filled, and in CanceledOrdersCache
-    expect(makerOrder!.status).toEqual(OrderStatus.BEST_EFFORT_CANCELED);
+    expect(makerOrder!.status).toEqual(status);
     // taker order is partially filled, and not in CanceledOrdersCache
     expect(takerOrder!.status).toEqual(OrderStatus.OPEN);
   });
