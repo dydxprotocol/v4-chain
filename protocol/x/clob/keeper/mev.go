154a13df5994fbafbf7c78a4783ed7711b4705e1
Zygimantas <5236121+Zygimantass@users.noreply.github.com>
2023-12-01 16:44:22 +0100

    feat(mev): enable sending BpMevMatches and ProposalReceiveTime to MEV indexer

--- protocol/x/clob/keeper/mev.go
+++ protocol/x/clob/keeper/mev.go
@@ -327,6 +327,8 @@ func (k Keeper) RecordMevMetrics(
 				MevNodeToNode: types.MevNodeToNodeMetrics{
 					ValidatorMevMatches: validatorMevMatches,
 					ClobMidPrices:       mevClobMidPrices,
+					BpMevMatches:        blockProposerMevMatches,
+					ProposalReceiveTime: uint64(time.Now().Second()),
 				},
 			},
 		)
