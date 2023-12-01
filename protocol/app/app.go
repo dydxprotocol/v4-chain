1ef57449bd6c0890ae028dffdbf8dd93b7db34e1
Zygimantas <5236121+Zygimantass@users.noreply.github.com>
2023-12-01 16:28:11 +0100

    feat(mev): enable multiple telemetry hosts

--- protocol/app/app.go
+++ protocol/app/app.go
@@ -858,6 +858,7 @@ func New(
 		rate_limit.NewPanicRateLimiter[*clobmoduletypes.MsgPlaceOrder](),
 		rate_limit.NewPanicRateLimiter[*clobmoduletypes.MsgCancelOrder](),
 	)
+
 	clobModule := clobmodule.NewAppModule(
 		appCodec,
 		app.ClobKeeper,
