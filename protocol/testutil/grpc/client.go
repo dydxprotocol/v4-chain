76df86e40ee3e94aa8bf219abaa40a965da4b3f6
jayy04 <103467857+jayy04@users.noreply.github.com>
2023-12-08 17:29:38 -0500

    [CLOB-1037] grpc to get all perpetuals and liquidity tiers (#859)

--- protocol/testutil/grpc/client.go
+++ protocol/testutil/grpc/client.go
@@ -6,6 +6,7 @@ import (
 	pricefeedtypes "github.com/dydxprotocol/v4-chain/protocol/daemons/pricefeed/api"
 	blocktimetypes "github.com/dydxprotocol/v4-chain/protocol/x/blocktime/types"
 	clobtypes "github.com/dydxprotocol/v4-chain/protocol/x/clob/types"
+	perptypes "github.com/dydxprotocol/v4-chain/protocol/x/perpetuals/types"
 	pricetypes "github.com/dydxprotocol/v4-chain/protocol/x/prices/types"
 	satypes "github.com/dydxprotocol/v4-chain/protocol/x/subaccounts/types"
 )
@@ -15,8 +16,9 @@ type QueryClient interface {
 	blocktimetypes.QueryClient
 	satypes.QueryClient
 	clobtypes.QueryClient
+	perptypes.QueryClient
+	pricetypes.QueryClient
 	bridgetypes.BridgeServiceClient
 	liquidationtypes.LiquidationServiceClient
 	pricefeedtypes.PriceFeedServiceClient
-	pricetypes.QueryClient
 }
