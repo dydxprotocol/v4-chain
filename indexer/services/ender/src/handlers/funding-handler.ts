e696ea6cc1ac274a55b23d3b180f7026db13d1e1
dydxwill <119354122+dydxwill@users.noreply.github.com>
2023-12-08 14:20:22 -0500

    add more Indexer stats around funding indices (#861)

--- indexer/services/ender/src/handlers/funding-handler.ts
+++ indexer/services/ender/src/handlers/funding-handler.ts
@@ -1,12 +1,15 @@
-import { logger } from '@dydxprotocol-indexer/base';
+import { logger, stats } from '@dydxprotocol-indexer/base';
 import {
   FundingIndexUpdatesTable,
   PerpetualMarketFromDatabase,
   TendermintEventTable,
   protocolTranslations,
   PerpetualMarketModel,
+  FundingIndexUpdatesFromDatabase,
+  FundingIndexUpdatesModel,
 } from '@dydxprotocol-indexer/postgres';
 import { NextFundingCache } from '@dydxprotocol-indexer/redis';
+import { bytesToBigInt } from '@dydxprotocol-indexer/v4-proto-parser';
 import {
   FundingEventV1_Type,
   FundingUpdateV1,
@@ -15,6 +18,7 @@ import Big from 'big.js';
 import _ from 'lodash';
 import * as pg from 'pg';
 
+import config from '../config';
 import { redisClient } from '../helpers/redis/redis-controller';
 import { indexerTendermintEventToTransactionIndex } from '../lib/helper';
 import { ConsolidatedKafkaEvent, FundingEventMessage } from '../lib/types';
@@ -56,6 +60,17 @@ export class FundingHandler extends Handler<FundingEventMessage> {
         PerpetualMarketModel.fromJson(perpetualMarket as object) as PerpetualMarketFromDatabase,
       );
     }
+    const fundingIndices:
+    Map<string, FundingIndexUpdatesFromDatabase> = new
+    Map<string, FundingIndexUpdatesFromDatabase>();
+    for (const [key, fundingIndex] of Object.entries(resultRow.funding_index_updates)) {
+      fundingIndices.set(
+        key,
+        FundingIndexUpdatesModel.fromJson(
+          fundingIndex as object,
+        ) as FundingIndexUpdatesFromDatabase,
+      );
+    }
 
     const promises: Promise<number>[] = new Array<Promise<number>>(this.event.updates.length);
 
@@ -67,6 +82,7 @@ export class FundingHandler extends Handler<FundingEventMessage> {
           message: resultRow.errors[i],
           update,
         });
+        stats.increment(`${config.SERVICE_NAME}.handle_funding_event.failure`, 1);
         continue;
       }
 
@@ -78,6 +94,7 @@ export class FundingHandler extends Handler<FundingEventMessage> {
           message: 'Received FundingUpdate with unknown perpetualId.',
           update,
         });
+        stats.increment(`${config.SERVICE_NAME}.handle_funding_event.failure`, 1);
         continue;
       }
 
@@ -92,6 +109,29 @@ export class FundingHandler extends Handler<FundingEventMessage> {
         case FundingEventV1_Type.TYPE_FUNDING_RATE_AND_INDEX:
           // clear the cache for the predicted next funding rate
           promises[i] = NextFundingCache.clearFundingSamples(perpetualMarket.ticker, redisClient);
+          stats.gauge(
+            `${config.SERVICE_NAME}.funding_index_update_event`,
+            Number(
+              protocolTranslations.fundingIndexToHumanFixedString(
+                bytesToBigInt(update.fundingIndex).toString(),
+                perpetualMarket,
+              ),
+            ),
+            {
+              ticker: perpetualMarket.ticker,
+            },
+          );
+          // eslint-disable-next-line no-case-declarations
+          const fundingIndexUpdate: FundingIndexUpdatesFromDatabase = fundingIndices.get(
+            update.perpetualId.toString(),
+          )!;
+          stats.gauge(
+            `${config.SERVICE_NAME}.funding_index_update`,
+            Number(fundingIndexUpdate.fundingIndex),
+            {
+              ticker: perpetualMarket.ticker,
+            },
+          );
           break;
         default:
           logger.error({
@@ -99,6 +139,7 @@ export class FundingHandler extends Handler<FundingEventMessage> {
             message: 'Received unknown FundingEvent type.',
             event: this.event,
           });
+          stats.increment(`${config.SERVICE_NAME}.handle_funding_event.failure`, 1);
       }
     }
 
