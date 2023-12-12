3a2ff3a19789030c3cc7a043f9a0992db8330445
Jay Yu <103467857+jayy04@users.noreply.github.com>
2023-12-12 13:37:36 -0500

    switch from map to slice

--- indexer/packages/v4-protos/src/codegen/dydxprotocol/daemons/liquidation/liquidation.ts
+++ indexer/packages/v4-protos/src/codegen/dydxprotocol/daemons/liquidation/liquidation.ts
@@ -2,14 +2,6 @@ import { SubaccountId, SubaccountIdSDKType } from "../../subaccounts/subaccount"
 import { SubaccountOpenPositionInfo, SubaccountOpenPositionInfoSDKType } from "../../clob/liquidations";
 import * as _m0 from "protobufjs/minimal";
 import { DeepPartial } from "../../../helpers";
-export interface LiquidateSubaccountsRequest_SubaccountOpenPositionInfoEntry {
-  key: number;
-  value?: SubaccountOpenPositionInfo;
-}
-export interface LiquidateSubaccountsRequest_SubaccountOpenPositionInfoEntrySDKType {
-  key: number;
-  value?: SubaccountOpenPositionInfoSDKType;
-}
 /**
  * LiquidateSubaccountsRequest is a request message that contains a list of
  * subaccount ids that potentially need to be liquidated. The list of subaccount
@@ -26,9 +18,7 @@ export interface LiquidateSubaccountsRequest {
   /** The list of subaccount ids with negative total net collateral. */
 
   negativeTncSubaccountIds: SubaccountId[];
-  subaccountOpenPositionInfo?: {
-    [key: number]: SubaccountOpenPositionInfo;
-  };
+  subaccountOpenPositionInfo: SubaccountOpenPositionInfo[];
 }
 /**
  * LiquidateSubaccountsRequest is a request message that contains a list of
@@ -46,9 +36,7 @@ export interface LiquidateSubaccountsRequestSDKType {
   /** The list of subaccount ids with negative total net collateral. */
 
   negative_tnc_subaccount_ids: SubaccountIdSDKType[];
-  subaccount_open_position_info?: {
-    [key: number]: SubaccountOpenPositionInfoSDKType;
-  };
+  subaccount_open_position_info: SubaccountOpenPositionInfoSDKType[];
 }
 /**
  * LiquidateSubaccountsResponse is a response message for
@@ -63,67 +51,12 @@ export interface LiquidateSubaccountsResponse {}
 
 export interface LiquidateSubaccountsResponseSDKType {}
 
-function createBaseLiquidateSubaccountsRequest_SubaccountOpenPositionInfoEntry(): LiquidateSubaccountsRequest_SubaccountOpenPositionInfoEntry {
-  return {
-    key: 0,
-    value: undefined
-  };
-}
-
-export const LiquidateSubaccountsRequest_SubaccountOpenPositionInfoEntry = {
-  encode(message: LiquidateSubaccountsRequest_SubaccountOpenPositionInfoEntry, writer: _m0.Writer = _m0.Writer.create()): _m0.Writer {
-    if (message.key !== 0) {
-      writer.uint32(8).uint32(message.key);
-    }
-
-    if (message.value !== undefined) {
-      dydxprotocol.clob.SubaccountOpenPositionInfo.encode(message.value, writer.uint32(18).fork()).ldelim();
-    }
-
-    return writer;
-  },
-
-  decode(input: _m0.Reader | Uint8Array, length?: number): LiquidateSubaccountsRequest_SubaccountOpenPositionInfoEntry {
-    const reader = input instanceof _m0.Reader ? input : new _m0.Reader(input);
-    let end = length === undefined ? reader.len : reader.pos + length;
-    const message = createBaseLiquidateSubaccountsRequest_SubaccountOpenPositionInfoEntry();
-
-    while (reader.pos < end) {
-      const tag = reader.uint32();
-
-      switch (tag >>> 3) {
-        case 1:
-          message.key = reader.uint32();
-          break;
-
-        case 2:
-          message.value = dydxprotocol.clob.SubaccountOpenPositionInfo.decode(reader, reader.uint32());
-          break;
-
-        default:
-          reader.skipType(tag & 7);
-          break;
-      }
-    }
-
-    return message;
-  },
-
-  fromPartial(object: DeepPartial<LiquidateSubaccountsRequest_SubaccountOpenPositionInfoEntry>): LiquidateSubaccountsRequest_SubaccountOpenPositionInfoEntry {
-    const message = createBaseLiquidateSubaccountsRequest_SubaccountOpenPositionInfoEntry();
-    message.key = object.key ?? 0;
-    message.value = object.value !== undefined && object.value !== null ? dydxprotocol.clob.SubaccountOpenPositionInfo.fromPartial(object.value) : undefined;
-    return message;
-  }
-
-};
-
 function createBaseLiquidateSubaccountsRequest(): LiquidateSubaccountsRequest {
   return {
     blockHeight: 0,
     liquidatableSubaccountIds: [],
     negativeTncSubaccountIds: [],
-    subaccountOpenPositionInfo: {}
+    subaccountOpenPositionInfo: []
   };
 }
 
@@ -141,12 +74,10 @@ export const LiquidateSubaccountsRequest = {
       SubaccountId.encode(v!, writer.uint32(26).fork()).ldelim();
     }
 
-    Object.entries(message.subaccountOpenPositionInfo).forEach(([key, value]) => {
-      LiquidateSubaccountsRequest_SubaccountOpenPositionInfoEntry.encode({
-        key: (key as any),
-        value
-      }, writer.uint32(34).fork()).ldelim();
-    });
+    for (const v of message.subaccountOpenPositionInfo) {
+      SubaccountOpenPositionInfo.encode(v!, writer.uint32(34).fork()).ldelim();
+    }
+
     return writer;
   },
 
@@ -172,12 +103,7 @@ export const LiquidateSubaccountsRequest = {
           break;
 
         case 4:
-          const entry4 = LiquidateSubaccountsRequest_SubaccountOpenPositionInfoEntry.decode(reader, reader.uint32());
-
-          if (entry4.value !== undefined) {
-            message.subaccountOpenPositionInfo[entry4.key] = entry4.value;
-          }
-
+          message.subaccountOpenPositionInfo.push(SubaccountOpenPositionInfo.decode(reader, reader.uint32()));
           break;
 
         default:
@@ -194,15 +120,7 @@ export const LiquidateSubaccountsRequest = {
     message.blockHeight = object.blockHeight ?? 0;
     message.liquidatableSubaccountIds = object.liquidatableSubaccountIds?.map(e => SubaccountId.fromPartial(e)) || [];
     message.negativeTncSubaccountIds = object.negativeTncSubaccountIds?.map(e => SubaccountId.fromPartial(e)) || [];
-    message.subaccountOpenPositionInfo = Object.entries(object.subaccountOpenPositionInfo ?? {}).reduce<{
-      [key: number]: SubaccountOpenPositionInfo;
-    }>((acc, [key, value]) => {
-      if (value !== undefined) {
-        acc[Number(key)] = SubaccountOpenPositionInfo.fromPartial(value);
-      }
-
-      return acc;
-    }, {});
+    message.subaccountOpenPositionInfo = object.subaccountOpenPositionInfo?.map(e => SubaccountOpenPositionInfo.fromPartial(e)) || [];
     return message;
   }
 
