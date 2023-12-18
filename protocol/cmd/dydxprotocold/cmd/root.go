f23cd6586f1de0fb99e6981d34c12cc483333fb1
Lukasz Cwik <lcwik@apache.org>
2023-12-18 11:13:37 -0800

    Swap to AppGenesisFromFile as per Cosmos upgrade guide https://docs.cosmos.network/v0.50/build/migrations/upgrading#xgenutil

--- protocol/cmd/dydxprotocold/cmd/root.go
+++ protocol/cmd/dydxprotocold/cmd/root.go
@@ -13,7 +13,6 @@ import (
 	"cosmossdk.io/store/snapshots"
 	snapshottypes "cosmossdk.io/store/snapshots/types"
 	tmcli "github.com/cometbft/cometbft/libs/cli"
-	tmtypes "github.com/cometbft/cometbft/types"
 	dbm "github.com/cosmos/cosmos-db"
 	"github.com/cosmos/cosmos-sdk/baseapp"
 	"github.com/cosmos/cosmos-sdk/client"
@@ -289,7 +288,7 @@ func (a appCreator) newApp(
 	chainID := cast.ToString(appOpts.Get(flags.FlagChainID))
 	if chainID == "" {
 		// fallback to genesis chain-id
-		appGenesis, err := tmtypes.GenesisDocFromFile(filepath.Join(homeDir, "config", "genesis.json"))
+		appGenesis, err := genutiltypes.AppGenesisFromFile(filepath.Join(homeDir, "config", "genesis.json"))
 		if err != nil {
 			panic(err)
 		}
