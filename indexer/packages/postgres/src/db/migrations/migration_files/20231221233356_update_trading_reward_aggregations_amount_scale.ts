1840add1bbb5c5ac037d77c9f4635cd3e377112e
Christopher Li <Christopherfooli@gmail.com>
2023-12-22 00:03:43 -0500

    lint

--- indexer/packages/postgres/src/db/migrations/migration_files/20231221233356_update_trading_reward_aggregations_amount_scale.ts
+++ indexer/packages/postgres/src/db/migrations/migration_files/20231221233356_update_trading_reward_aggregations_amount_scale.ts
@@ -4,7 +4,8 @@ export async function up(knex: Knex): Promise<void> {
   return knex
     .schema
     .alterTable('trading_reward_aggregations', (table) => {
-      // 27 is the max precision and 18 is scale, which means 9 digits before the decimal point and 18 after
+      // 27 is the max precision and 18 is scale, which means 9 digits before the decimal point
+      // and 18 after
       table.decimal('amount', 27, 18).notNullable().alter();
     });
 }
