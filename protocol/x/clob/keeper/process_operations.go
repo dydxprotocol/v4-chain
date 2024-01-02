3b4d12bb625070c2396103dae25d3100d9436fed
Lucas <lucas@dydx.exchange>
2024-01-02 09:29:30 -0800

    Fix lint

--- protocol/x/clob/keeper/process_operations.go
+++ protocol/x/clob/keeper/process_operations.go
@@ -676,8 +676,9 @@ func (k Keeper) PersistMatchDeleveragingToState(
 	deltaBaseQuantumsIsNegative := position.GetIsLong()
 
 	// TODO: This logic assumes that the subaccount is negative equity. Should account for final settlement markets.
-	// If there are zero-deleveraging fills, this is a sentinel value to indicate a subaccount could not be liquidated or deleveraged
-	// and still has negative equity. Mark the current block number in state to indicate a negative TNC subaccount was seen.
+	// If there are zero-deleveraging fills, this is a sentinel value to indicate a subaccount could not be liquidated
+	// or deleveraged and still has negative equity. Mark the current block number in state to indicate a negative TNC
+	// subaccount was seen.
 	if len(matchDeleveraging.GetFills()) == 0 {
 		k.subaccountsKeeper.SetNegativeTncSubaccountSeenAtBlock(ctx, lib.MustConvertIntegerToUint32(ctx.BlockHeight()))
 		return nil
