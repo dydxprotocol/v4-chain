33b82453c209c9b197009922e227065adac282ec
Christopher-Li <Christopher-Li@users.noreply.github.com>
2024-01-02 18:18:00 -0500

    Fix Trading reward event processing (#913) (#916)

--- indexer/services/scripts/src/helpers/types.ts
+++ indexer/services/scripts/src/helpers/types.ts
@@ -20,4 +20,5 @@ export enum DydxIndexerSubtypes {
   LIQUIDITY_TIER = 'liquidity_tier',
   UPDATE_PERPETUAL = 'update_perpetual',
   UPDATE_CLOB_PAIR = 'update_clob_pair',
+  TRADING_REWARD = 'trading_reward',
 }
