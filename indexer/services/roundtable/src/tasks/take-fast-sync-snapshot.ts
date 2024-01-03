697bc6ab058612a26243a9975f94e06476dc4cc4
Will Liu <will@dydx.exchange>
2024-01-03 12:39:54 -0500

    fix

--- indexer/services/roundtable/src/tasks/take-fast-sync-snapshot.ts
+++ indexer/services/roundtable/src/tasks/take-fast-sync-snapshot.ts
@@ -1,14 +1,11 @@
 import { InfoObject, logger, stats } from '@dydxprotocol-indexer/base';
 import RDS from 'aws-sdk/clients/rds';
-import S3 from 'aws-sdk/clients/s3';
 import { DateTime } from 'luxon';
 
 import config from '../config';
 import {
   checkIfExportJobToS3IsOngoing,
-  checkIfS3ObjectExists,
   FAST_SYNC_SNAPSHOT_S3_BUCKET_NAME,
-  getMostRecentDBSnapshotIdentifier,
   startExportTask,
 } from '../helpers/aws';
 
@@ -16,41 +13,14 @@ const statStart: string = `${config.SERVICE_NAME}.fast_sync_export_db_snapshot`;
 
 export default async function runTask(): Promise<void> {
   const at: string = 'fast-sync-export-db-snapshot#runTask';
+  logger.info({ at, message: 'Starting task.' });
 
   const rds: RDS = new RDS();
 
-  // get most recent rds snapshot
-  const startDescribe: number = Date.now();
-  const dateString: string = DateTime.utc().toFormat('yyyy-MM-dd');
-  const mostRecentSnapshot: string = await getMostRecentDBSnapshotIdentifier(rds);
-  stats.timing(`${statStart}.describe_rds_snapshots`, Date.now() - startDescribe);
+  const dateString: string = DateTime.utc().toFormat('yyyy-MM-dd-HH-mm');
+  const rdsExportIdentifier: string = `${config.RDS_INSTANCE_NAME}-fast-sync-${dateString}`;
 
-  // dev example: rds:dev-indexer-apne1-db-2023-06-25-18-34
-  const s3Date: string = mostRecentSnapshot.split(config.RDS_INSTANCE_NAME)[1].slice(1);
-  const s3: S3 = new S3();
-
-  // check if s3 object exists
-  const startS3Check: number = Date.now();
-  const s3ObjectExists: boolean = await checkIfS3ObjectExists(
-    s3,
-    s3Date,
-    FAST_SYNC_SNAPSHOT_S3_BUCKET_NAME,
-  );
-  stats.timing(`${statStart}.checkS3Object`, Date.now() - startS3Check);
-
-  const rdsExportIdentifier: string = `${config.RDS_INSTANCE_NAME}-fast-sync-${s3Date}`;
-
-  // If the s3 object exists, return
-  if (s3ObjectExists) {
-    logger.info({
-      at,
-      dateString,
-      message: 'S3 object exists.',
-    });
-    return;
-  }
-
-  // if we haven't created the object, check if it is being created
+  // check if it is being created
   const rdsExportCheck: number = Date.now();
   const exportJobOngoing: boolean = await checkIfExportJobToS3IsOngoing(rds, rdsExportIdentifier);
   stats.timing(`${statStart}.checkRdsExport`, Date.now() - rdsExportCheck);
@@ -63,7 +33,8 @@ export default async function runTask(): Promise<void> {
     });
     return;
   }
-  // start Export Job if S3 Object does not exist
+
+  // start Export Job if not already started
   const startExport: number = Date.now();
   try {
     const exportData: RDS.ExportTask = await startExportTask(
