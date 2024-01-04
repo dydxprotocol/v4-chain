d6cdfa2d08bade93f27053f63227b4036ef90bb7
Christopher Li <Christopherfooli@gmail.com>
2024-01-04 16:50:10 -0500

    fix

--- indexer/packages/postgres/__tests__/stores/wallet-table.test.ts
+++ indexer/packages/postgres/__tests__/stores/wallet-table.test.ts
@@ -1,6 +1,6 @@
 import { WalletFromDatabase } from '../../src/types';
 import { clearData, migrate, teardown } from '../../src/helpers/db-helpers';
-import { defaultAddress, defaultWallet } from '../helpers/constants';
+import { defaultAddress, defaultWallet2 } from '../helpers/constants';
 import * as WalletTable from '../../src/stores/wallet-table';
 
 describe('Wallet store', () => {
@@ -17,27 +17,27 @@ describe('Wallet store', () => {
   });
 
   it('Successfully creates a Wallet', async () => {
-    await WalletTable.create(defaultWallet);
+    await WalletTable.create(defaultWallet2);
   });
 
   it('Successfully upserts a Wallet multiple times', async () => {
-    await WalletTable.upsert(defaultWallet);
+    await WalletTable.upsert(defaultWallet2);
     let wallet: WalletFromDatabase | undefined = await WalletTable.findById(
       defaultAddress,
     );
 
-    expect(wallet).toEqual(expect.objectContaining(defaultWallet));
-    await WalletTable.upsert(defaultWallet);
+    expect(wallet).toEqual(expect.objectContaining(defaultWallet2));
+    await WalletTable.upsert(defaultWallet2);
     wallet = await WalletTable.findById(
       defaultAddress,
     );
 
-    expect(wallet).toEqual(expect.objectContaining(defaultWallet));
+    expect(wallet).toEqual(expect.objectContaining(defaultWallet2));
   });
 
   it('Successfully finds all Wallets', async () => {
     await Promise.all([
-      WalletTable.create(defaultWallet),
+      WalletTable.create(defaultWallet2),
       WalletTable.create({
         address: 'fake_address',
         totalTradingRewards: '0',
@@ -51,19 +51,19 @@ describe('Wallet store', () => {
     );
 
     expect(wallets.length).toEqual(2);
-    expect(wallets[0]).toEqual(expect.objectContaining(defaultWallet));
+    expect(wallets[0]).toEqual(expect.objectContaining(defaultWallet2));
     expect(wallets[1]).toEqual(expect.objectContaining({
       address: 'fake_address',
     }));
   });
 
   it('Successfully finds a Wallet', async () => {
-    await WalletTable.create(defaultWallet);
+    await WalletTable.create(defaultWallet2);
 
     const wallet: WalletFromDatabase | undefined = await WalletTable.findById(
       defaultAddress,
     );
 
-    expect(wallet).toEqual(expect.objectContaining(defaultWallet));
+    expect(wallet).toEqual(expect.objectContaining(defaultWallet2));
   });
 });
