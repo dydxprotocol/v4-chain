eea0999d871c67339dd25bb98246d0145f9578e9
jayy04 <103467857+jayy04@users.noreply.github.com>
2024-01-05 11:34:10 -0500

    [CLOB-1056] add validations for enums in clob protos (#909)
    * [CLOB-1056] add validations for enums in clob protos
    
    * e2e test to verify that existing orders won't break things
    
    * comment

--- protocol/x/clob/types/message_place_order.go
+++ protocol/x/clob/types/message_place_order.go
@@ -41,8 +41,17 @@ func (msg *MsgPlaceOrder) ValidateBasic() (err error) {
 		return err
 	}
 
+	// Verify that enum type values are valid.
 	if _, exists := Order_Side_name[int32(msg.Order.Side)]; !exists {
 		return errorsmod.Wrapf(ErrInvalidOrderSide, "invalid order side (%s)", msg.Order.Side)
+	}
+
+	if _, exists := Order_TimeInForce_name[int32(msg.Order.TimeInForce)]; !exists {
+		return errorsmod.Wrapf(ErrInvalidTimeInForce, "invalid time in force (%s)", msg.Order.TimeInForce)
+	}
+
+	if _, exists := Order_ConditionType_name[int32(msg.Order.ConditionType)]; !exists {
+		return errorsmod.Wrapf(ErrInvalidConditionType, "invalid condition type (%s)", msg.Order.ConditionType)
 	}
 
 	if msg.Order.Side == Order_SIDE_UNSPECIFIED {
