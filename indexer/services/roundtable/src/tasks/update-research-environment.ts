d4088ddf4301c493a0d98e4539cb479ae4fa595d
Christopher Li <Christopherfooli@gmail.com>
2024-01-05 11:13:03 -0500

    [IND-553]: Add support for trading reward tables in Athena

--- indexer/services/roundtable/src/tasks/update-research-environment.ts
+++ indexer/services/roundtable/src/tasks/update-research-environment.ts
@@ -33,6 +33,8 @@ import * as athenaPerpetualPositions from '../lib/athena-ddl-tables/perpetual_po
 import * as athenaPnlTicks from '../lib/athena-ddl-tables/pnl_ticks';
 import * as athenaSubaccounts from '../lib/athena-ddl-tables/subaccounts';
 import * as athenaTendermintEvents from '../lib/athena-ddl-tables/tendermint_events';
+import * as athenaTradingRewardAggregations from '../lib/athena-ddl-tables/trading_reward_aggregations';
+import * as athenaTradingRewards from '../lib/athena-ddl-tables/trading_rewards';
 import * as athenaTransfers from '../lib/athena-ddl-tables/transfers';
 import * as athenaWallets from '../lib/athena-ddl-tables/wallets';
 
@@ -51,6 +53,8 @@ export const tablesToAddToAthena: { [table: string]: AthenaTableDDLQueries } = {
   pnl_ticks: athenaPnlTicks,
   subaccounts: athenaSubaccounts,
   tendermint_events: athenaTendermintEvents,
+  trading_rewards: athenaTradingRewards,
+  trading_reward_aggregation: athenaTradingRewardAggregations,
   transfers: athenaTransfers,
   liquidity_tiers: athenaLiquidityTiers,
   wallets: athenaWallets,
