e3da9452587049885f3dd9ee4f060a487569cbd7
Jay Yu <103467857+jayy04@users.noreply.github.com>
2024-01-08 15:32:23 -0500

    [CLOB-1054] update state R/W methods

--- protocol/x/clob/keeper/match_state.go
+++ protocol/x/clob/keeper/match_state.go
@@ -7,12 +7,30 @@ import (
 	"github.com/dydxprotocol/v4-chain/protocol/x/clob/types"
 )
 
-// GetLastTradePriceForPerpetual gets the last trade price for a perpetual.
-func (k Keeper) GetLastTradePriceForPerpetual(
+// GetTradePricesForPerpetual gets the last trade price for a perpetual.
+func (k Keeper) GetTradePricesForPerpetual(
+	ctx sdk.Context,
+	perpetualId uint32,
+) (
+	minTradePriceSubticks types.Subticks,
+	maxTradePriceSubticks types.Subticks,
+	found bool,
+) {
+	minTradePriceSubticks, found = k.getMinTradePriceForPerpetual(ctx, perpetualId)
+	if !found {
+		return 0, 0, false
+	}
+
+	maxTradePriceSubticks, _ = k.getMaxTradePriceForPerpetual(ctx, perpetualId)
+	return minTradePriceSubticks, maxTradePriceSubticks, true
+}
+
+// getMinTradePriceForPerpetual gets the min trade price for a perpetual.
+func (k Keeper) getMinTradePriceForPerpetual(
 	ctx sdk.Context,
 	perpetualId uint32,
 ) (subticks types.Subticks, found bool) {
-	store := k.GetLastTradePriceStore(ctx)
+	store := k.GetMinTradePriceStore(ctx)
 
 	b := store.Get(lib.Uint32ToKey(perpetualId))
 	if b == nil {
@@ -24,17 +42,43 @@ func (k Keeper) GetLastTradePriceForPerpetual(
 	return types.Subticks(result.Value), true
 }
 
-// SetLastTradePriceForPerpetual sets the last trade price for a perpetual.
-func (k Keeper) SetLastTradePriceForPerpetual(
+// getMaxTradePriceForPerpetual gets the max trade price for a perpetual.
+func (k Keeper) getMaxTradePriceForPerpetual(
+	ctx sdk.Context,
+	perpetualId uint32,
+) (subticks types.Subticks, found bool) {
+	store := k.GetMaxTradePriceStore(ctx)
+
+	b := store.Get(lib.Uint32ToKey(perpetualId))
+	if b == nil {
+		return 0, false
+	}
+
+	result := gogotypes.UInt64Value{Value: 0}
+	k.cdc.MustUnmarshal(b, &result)
+	return types.Subticks(result.Value), true
+}
+
+// SetTradePricesForPerpetual sets the trade prices for a perpetual.
+func (k Keeper) SetTradePricesForPerpetual(
 	ctx sdk.Context,
 	perpetualId uint32,
 	subticks types.Subticks,
 ) {
-	store := k.GetLastTradePriceStore(ctx)
-
+	minTradePriceSubticks, maxTradePriceSubticks, found := k.GetTradePricesForPerpetual(ctx, perpetualId)
+	key := lib.Uint32ToKey(perpetualId)
 	value := gogotypes.UInt64Value{Value: subticks.ToUint64()}
-	store.Set(
-		lib.Uint32ToKey(perpetualId),
-		k.cdc.MustMarshal(&value),
-	)
+	b := k.cdc.MustMarshal(&value)
+
+	// Update the min price if not previously saved or if the new price is lower.
+	if !found || subticks < minTradePriceSubticks {
+		store := k.GetMinTradePriceStore(ctx)
+		store.Set(key, b)
+	}
+
+	// Update the max price if not previously saved or if the new price is higher.
+	if !found || subticks > maxTradePriceSubticks {
+		store := k.GetMaxTradePriceStore(ctx)
+		store.Set(key, b)
+	}
 }
