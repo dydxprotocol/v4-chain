5afad732fc75dcc10e8ece3183f99f2de6370f18
Jay Yu <103467857+jayy04@users.noreply.github.com>
2024-01-17 13:11:30 -0500

    initial contract setup

---
+++ protocol/contracts/hackathon/src/msg.rs
@@ -0,0 +1,26 @@
+use cosmwasm_schema::{cw_serde, QueryResponses};
+
+#[cw_serde]
+pub struct InstantiateMsg {
+    pub count: i32,
+}
+
+#[cw_serde]
+pub enum ExecuteMsg {
+    Increment {},
+    Reset { count: i32 },
+}
+
+#[cw_serde]
+#[derive(QueryResponses)]
+pub enum QueryMsg {
+    // GetCount returns the current count as a json-encoded number
+    #[returns(GetCountResponse)]
+    GetCount {},
+}
+
+// We define a custom struct for each query response
+#[cw_serde]
+pub struct GetCountResponse {
+    pub count: i32,
+}
