c378e6c58e49cf9d36acf2ff66a5b9d4cadbede2
dydxwill <119354122+dydxwill@users.noreply.github.com>
2024-01-18 15:52:55 -0500

    add rust lib for subaccount query (#985)

--- protocol/dydx-cosmwasm/src/query.rs
+++ protocol/dydx-cosmwasm/src/query.rs
@@ -3,6 +3,7 @@ use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
 use crate::proto_structs::MarketPrice;
+use crate::proto_structs::Subaccount;
 use crate::route::DydxRoute;
 
 /// SeiQueryWrapper is an override of QueryRequest::Custom to access Sei-specific modules
@@ -23,9 +24,18 @@ pub enum DydxQuery {
     MarketPrice {
         id: u32,
     },
+    Subaccount {
+        owner: String,
+        number: u32,
+    }
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct MarketPriceResponse {
     pub market_price: MarketPrice,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct SubaccountResponse {
+    pub subaccount: Subaccount,
 }
