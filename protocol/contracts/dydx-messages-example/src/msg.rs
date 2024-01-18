8e3f5b290d9f39392e40a6d2898704941c7ffdf3
vincentwschau <99756290+vincentwschau@users.noreply.github.com>
2024-01-18 10:20:10 -0600

    Example escrow contract with sending messages and custom message decorator. (#977)

--- protocol/contracts/dydx-messages-example/src/msg.rs
+++ protocol/contracts/dydx-messages-example/src/msg.rs
@@ -1,26 +1,39 @@
 use cosmwasm_schema::{cw_serde, QueryResponses};
+use cosmwasm_std::{Addr, Coin};
+use cw_utils::Expiration;
 
 #[cw_serde]
 pub struct InstantiateMsg {
-    pub count: i32,
+    pub arbiter: String,
+    pub recipient: String,
+    /// When end height set and block height exceeds this value, the escrow is expired.
+    /// Once an escrow is expired, it can be returned to the original funder (via "refund").
+    ///
+    /// When end time (in seconds since epoch 00:00:00 UTC on 1 January 1970) is set and
+    /// block time exceeds this value, the escrow is expired.
+    /// Once an escrow is expired, it can be returned to the original funder (via "refund").
+    pub expiration: Option<Expiration>,
 }
 
 #[cw_serde]
 pub enum ExecuteMsg {
-    Increment {},
-    Reset { count: i32 },
+    Approve {
+        // release some coins - if quantity is None, release all coins in balance
+        quantity: Option<u64>,
+        //quantity: Option<Vec<Coin>>,
+    },
+    Refund {},
 }
 
 #[cw_serde]
 #[derive(QueryResponses)]
 pub enum QueryMsg {
-    // GetCount returns the current count as a json-encoded number
-    #[returns(GetCountResponse)]
-    GetCount {},
+    /// Returns a human-readable representation of the arbiter.
+    #[returns(ArbiterResponse)]
+    Arbiter {},
 }
 
-// We define a custom struct for each query response
 #[cw_serde]
-pub struct GetCountResponse {
-    pub count: i32,
+pub struct ArbiterResponse {
+    pub arbiter: Addr,
 }
--- protocol/contracts/dydx-messages-example/src/state.rs
+++ protocol/contracts/dydx-messages-example/src/state.rs
@@ -1,13 +1,15 @@
-use schemars::JsonSchema;
-use serde::{Deserialize, Serialize};
-
+use cosmwasm_schema::cw_serde;
 use cosmwasm_std::Addr;
 use cw_storage_plus::Item;
+use cw_utils::Expiration;
 
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
-pub struct State {
-    pub count: i32,
-    pub owner: Addr,
+#[cw_serde]
+pub struct Config {
+    pub arbiter: Addr,
+    pub recipient: Addr,
+    pub source: Addr,
+    pub expiration: Option<Expiration>,
 }
 
-pub const STATE: Item<State> = Item::new("state");
+pub const CONFIG_KEY: &str = "config";
+pub const CONFIG: Item<Config> = Item::new(CONFIG_KEY);
