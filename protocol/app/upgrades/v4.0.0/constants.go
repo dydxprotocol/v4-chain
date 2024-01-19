6a20dc698e9b1b20794c04dd0098029f112405b8
Roy Li <roy@dydx.exchange>
2024-01-18 21:46:11 -0500

    Remove initialization of existing module store in v4.0.0 upgrade handler

--- protocol/app/upgrades/v4.0.0/constants.go
+++ protocol/app/upgrades/v4.0.0/constants.go
@@ -4,7 +4,6 @@ import (
 	store "cosmossdk.io/store/types"
 	circuittypes "cosmossdk.io/x/circuit/types"
 	icacontrollertypes "github.com/cosmos/ibc-go/v8/modules/apps/27-interchain-accounts/controller/types"
-	icahosttypes "github.com/cosmos/ibc-go/v8/modules/apps/27-interchain-accounts/host/types"
 	"github.com/dydxprotocol/v4-chain/protocol/app/upgrades"
 )
 
@@ -22,7 +21,6 @@ var Upgrade = upgrades.Upgrade{
 
 			// Add new ICA stores that are needed by ICA host types as of v8.
 			icacontrollertypes.StoreKey,
-			icahosttypes.StoreKey,
 		},
 	},
 }
