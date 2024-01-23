ee6cdffd2dfa9db4af0cb82ad97090c44472a915
Lukasz Cwik <lcwik@apache.org>
2024-01-23 12:48:20 -0800

    [CORE-840] WIP: Drop getLegacyMsgSignersFn
    WIP: make proto-gen and update cosmos version

--- protocol/app/module/interface_registry.go
+++ protocol/app/module/interface_registry.go
@@ -2,7 +2,6 @@ package module
 
 import (
 	"cosmossdk.io/x/tx/signing"
-	"fmt"
 	addresscodec "github.com/cosmos/cosmos-sdk/codec/address"
 	"github.com/cosmos/cosmos-sdk/codec/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -12,42 +11,6 @@ import (
 )
 
 var InterfaceRegistry types.InterfaceRegistry
-
-func getLegacyMsgSignerFn(path []string) func(msg proto.Message) ([][]byte, error) {
-	if len(path) == 0 {
-		panic("path is expected to contain at least one value.")
-	}
-
-	return func(msg proto.Message) ([][]byte, error) {
-		m := msg.ProtoReflect()
-		for _, p := range path[:len(path)-1] {
-			fieldDesc := m.Descriptor().Fields().ByName(protoreflect.Name(p))
-			if fieldDesc.Kind() != protoreflect.MessageKind {
-				return nil, fmt.Errorf("Expected for field %s to be Message type in path %+v for msg %+v.", p, path, msg)
-			}
-			v := m.Get(fieldDesc)
-			if !v.IsValid() {
-				return nil, fmt.Errorf("Expected for field %s to be populated in path %+v for msg %+v.", p, path, msg)
-			}
-			m = v.Message()
-		}
-
-		fieldDesc := m.Descriptor().Fields().ByName(protoreflect.Name(path[len(path)-1]))
-		if fieldDesc.Kind() != protoreflect.StringKind {
-			return nil, fmt.Errorf(
-				"Expected for final field %s to be String type in path %+v for msg %+v.",
-				path[len(path)-1],
-				path,
-				msg,
-			)
-		}
-		signer, err := sdk.AccAddressFromBech32(m.Get(fieldDesc).String())
-		if err != nil {
-			return nil, err
-		}
-		return [][]byte{signer}, nil
-	}
-}
 
 // TODO(CORE-846): Consider having app injected messages return an error instead of empty signers list.
 func noSigners(_ proto.Message) ([][]byte, error) {
@@ -76,19 +39,6 @@ func NewInterfaceRegistry(addrPrefix string, valAddrPrefix string) (types.Interf
 			// https://github.com/cosmos/cosmos-sdk/issues/18722 is fixed, replace this with the cosmos.msg.v1.signing
 			// annotation on the protos.
 			CustomGetSigners: map[protoreflect.FullName]signing.GetSignersFunc{
-				"dydxprotocol.clob.MsgCancelOrder": getLegacyMsgSignerFn(
-					[]string{"order_id", "subaccount_id", "owner"},
-				),
-				"dydxprotocol.clob.MsgPlaceOrder": getLegacyMsgSignerFn(
-					[]string{"order", "order_id", "subaccount_id", "owner"},
-				),
-				"dydxprotocol.sending.MsgCreateTransfer": getLegacyMsgSignerFn(
-					[]string{"transfer", "sender", "owner"},
-				),
-				"dydxprotocol.sending.MsgWithdrawFromSubaccount": getLegacyMsgSignerFn(
-					[]string{"sender", "owner"},
-				),
-
 				// App injected messages have no signers.
 				"dydxprotocol.bridge.MsgAcknowledgeBridges":  noSigners,
 				"dydxprotocol.clob.MsgProposedOperations":    noSigners,
