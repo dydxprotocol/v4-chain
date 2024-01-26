e06aea3d36c2c7be9922bfc9923c677b7f6678f4
Teddy Ding <teddy@dydx.exchange>
2024-01-26 16:53:24 -0700

    [CORE-824] Add upgrade handler for ratelimit keeper (#1019)

--- protocol/app/upgrades/v4.0.0/upgrade.go
+++ protocol/app/upgrades/v4.0.0/upgrade.go
@@ -7,17 +7,26 @@ import (
 	upgradetypes "cosmossdk.io/x/upgrade/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
+
+	ratelimitkeeper "github.com/dydxprotocol/v4-chain/protocol/x/ratelimit/keeper"
+	ratelimittypes "github.com/dydxprotocol/v4-chain/protocol/x/ratelimit/types"
 )
 
 func CreateUpgradeHandler(
 	mm *module.Manager,
 	configurator module.Configurator,
+	rateLimitKeepr ratelimitkeeper.Keeper,
 ) upgradetypes.UpgradeHandler {
 	return func(ctx context.Context, plan upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
 		sdkCtx := sdk.UnwrapSDKContext(ctx)
 		sdkCtx.Logger().Info(fmt.Sprintf("Running %s Upgrade...", UpgradeName))
 
-		// TODO(CORE-824): Initialize ratelimit module params to desired state.
+		if err := rateLimitKeepr.SetLimitParams(
+			sdkCtx,
+			ratelimittypes.DefaultUsdcRateLimitParams(),
+		); err != nil {
+			panic(fmt.Sprintf("failed to set default x/ratelimit params: %s", err))
+		}
 
 		return mm.RunMigrations(ctx, configurator, vm)
 	}
