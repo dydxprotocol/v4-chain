326401845e03253baccefcdcf8afc2426a146bd0
Teddy Ding <teddy@dydx.exchange>
2024-01-26 13:15:15 -0700

    fix tests

--- protocol/lib/ante/unsupported_msgs.go
+++ protocol/lib/ante/unsupported_msgs.go
@@ -2,6 +2,7 @@ package ante
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	gov "github.com/cosmos/cosmos-sdk/x/gov/types/v1"
 	govbeta "github.com/cosmos/cosmos-sdk/x/gov/types/v1beta1"
 	icacontrollertypes "github.com/cosmos/ibc-go/v8/modules/apps/27-interchain-accounts/controller/types"
 )
@@ -16,7 +17,8 @@ func IsUnsupportedMsg(msg sdk.Msg) bool {
 		*icacontrollertypes.MsgRegisterInterchainAccount,
 		// ------- CosmosSDK default modules
 		// gov
-		*govbeta.MsgSubmitProposal:
+		*govbeta.MsgSubmitProposal,
+		*gov.MsgCancelProposal:
 		return true
 	}
 	return false
