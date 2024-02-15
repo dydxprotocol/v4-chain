4292ecedcfef7e28625c67b86ddd66f4159fa3dc
Nikhil Vasan <nikhil@skip.money>
2024-02-15 09:32:32 -0800

    fix Makefile + godoc ExtendVoteHandler

--- protocol/app/vote_extensions/extend_vote_handler.go
+++ protocol/app/vote_extensions/extend_vote_handler.go
@@ -11,12 +11,21 @@ import (
 	prices "github.com/dydxprotocol/v4-chain/protocol/x/prices/types"
 )
 
+// ExtendVoteHandler is a wrapper around the Slinky ExtendVoteHandler. This wrapper is responsible for applying the newest 
+// MarketPriceUpdates in a block + calculating the latest historical smoothed prices, so that the prices to be submitted in a vote
+// extension are determined on the latest available information.
 type ExtendVoteHandler struct {
 	SlinkyExtendVoteHandler sdk.ExtendVoteHandler
 	PricesTxDecoder         process.UpdateMarketPriceTxDecoder
 	PricesKeeper            PricesKeeper
 }
 
+// ExtendVoteHandler returns a sdk.ExtendVoteHandler, responsible for the following:
+//  1. Decoding the x/prices MsgUpdateMarketPrices in the current block - fail on errors
+//  2. Applying any changes to the x/prices smoothed prices so that valid prices are proposed
+//  3. Validating the proposed MsgUpdateMarketPrices in accordance with the ProcessProposal check
+//  4. Updating the market prices in the PricesKeeper so that the GetValidMarketPriceUpdates function returns the latest available market prices
+//  5. Calling the Slinky ExtendVoteHandler to handle the rest of ExtendVote
 func (e *ExtendVoteHandler) ExtendVoteHandler() sdk.ExtendVoteHandler {
 	return func(ctx sdk.Context, req *cometabci.RequestExtendVote) (resp *cometabci.ResponseExtendVote, err error) {
 		// Decode the x/prices txn in the current block
@@ -24,22 +33,32 @@ func (e *ExtendVoteHandler) ExtendVoteHandler() sdk.ExtendVoteHandler {
 		if err != nil {
 			return nil, fmt.Errorf("DecodeMarketPricesTx failure %w", err)
 		}
+
 		// Apply the changes to the PricesKeeper so valid prices are proposed
 		if err = e.PricesKeeper.UpdateSmoothedPrices(ctx, lib.Uint64LinearInterpolate); err != nil {
 			return nil, fmt.Errorf("UpdateSmoothedPrices failure %w", err)
 		}
+
+		// ensure that the proposed MsgUpdateMarketPrices is valid in accordance w/ stateful information
+		// this check is equivalent to the check in ProcessProposal (indexPriceCache has not been updated)
 		err = updatePricesTx.Validate()
 		if err != nil {
 			return nil, fmt.Errorf("updatePricesTx.Validate failure %w", err)
 		}
+
+		// Update the market prices in the PricesKeeper, so that the GetValidMarketPriceUpdates
+		// function returns the latest available market prices.
 		updateMarketPricesMsg, ok := updatePricesTx.GetMsg().(*prices.MsgUpdateMarketPrices)
 		if !ok {
 			return nil, fmt.Errorf("expected %s, got %T", "MsgUpdateMarketPrices", updateMarketPricesMsg)
 		}
+
+		// Update the market prices in the PricesKeeper
 		err = e.PricesKeeper.UpdateMarketPrices(ctx, updateMarketPricesMsg.MarketPriceUpdates)
 		if err != nil {
 			return nil, fmt.Errorf("failed to update market prices in extend vote handler pre-slinky invocation %w", err)
 		}
+
 		// Call the Slinky ExtendVoteHandler
 		return e.SlinkyExtendVoteHandler(ctx, req)
 	}
