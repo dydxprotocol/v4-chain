188bdc8aab696ca35bc012dd1f6425bac52147b8
Will Liu <will@dydx.exchange>
2024-02-23 19:27:22 -0500

    cast to jsonb directly

--- indexer/services/ender/src/scripts/handlers/dydx_funding_handler.sql
+++ indexer/services/ender/src/scripts/handlers/dydx_funding_handler.sql
@@ -38,7 +38,7 @@ BEGIN
         perpetual_market_id = (funding_update->'perpetualId')::bigint;
         SELECT * INTO perpetual_market_record FROM perpetual_markets WHERE "id" = perpetual_market_id;
         IF NOT FOUND THEN
-            errors_response = array_append(errors_response, to_jsonb('Received FundingUpdate with unknown perpetualId.'::text));
+            errors_response = array_append(errors_response, 'Received FundingUpdate with unknown perpetualId.'::jsonb);
             CONTINUE;
         END IF;
 
@@ -82,7 +82,7 @@ BEGIN
                 funding_update_response = jsonb_set(funding_update_response, ARRAY[(funding_index_updates_record."perpetualId")::text], dydx_to_jsonb(funding_index_updates_record));
 
             ELSE
-                errors_response = array_append(errors_response, to_jsonb('Received unknown FundingEvent type.'::text));
+                errors_response = array_append(errors_response, 'Received unknown FundingEvent type.'::jsonb);
                 CONTINUE;
         END CASE;
 
