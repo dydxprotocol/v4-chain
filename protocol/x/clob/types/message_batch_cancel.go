8f77dbe0003580dc21841be4bcc0ed3e5fed2b63
Jonathan Fung <jonathan@dydx.exchange>
2024-02-26 17:15:19 -0800

    validatebasic for new MsgBatchCancel

---
+++ protocol/x/clob/types/message_batch_cancel.go
@@ -0,0 +1,54 @@
+package types
+
+import (
+	errorsmod "cosmossdk.io/errors"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	types "github.com/dydxprotocol/v4-chain/protocol/x/subaccounts/types"
+)
+
+var _ sdk.Msg = &MsgBatchCancel{}
+
+// NewMsgCancelOrderShortTerm constructs a MsgBatchCancel from an `OrderId` and a `GoodTilBlock`.
+func NewMsgBatchCancel(subaccountId types.SubaccountId, cancelBatch []OrderBatch, goodTilBlock uint32) *MsgBatchCancel {
+	return &MsgBatchCancel{
+		SubaccountId:     subaccountId,
+		ShortTermCancels: cancelBatch,
+		GoodTilBlock:     goodTilBlock,
+	}
+}
+
+// ValidateBasic performs stateless validation for
+func (msg *MsgBatchCancel) ValidateBasic() (err error) {
+	subaccountId := msg.GetSubaccountId()
+	if err := subaccountId.Validate(); err != nil {
+		return err
+	}
+
+	cancelBatches := msg.GetShortTermCancels()
+	totalNumberCancels := 0
+	for _, cancelBatch := range cancelBatches {
+		totalNumberCancels += len(cancelBatch.GetClientIds())
+		seenClientIds := map[uint32]struct{}{}
+		for _, clientId := range cancelBatch.GetClientIds() {
+			if _, seen := seenClientIds[clientId]; seen {
+				return errorsmod.Wrapf(
+					ErrInvalidBatchCancel,
+					"Batch cancel cannot have duplicate cancels. Duplicate clob pair id: %+v, client id: %+v",
+					cancelBatch.GetClobPairId(),
+					clientId,
+				)
+			}
+			seenClientIds[clientId] = struct{}{}
+		}
+	}
+	if uint32(totalNumberCancels) > MaxMsgBatchCancelBatchSize {
+		return errorsmod.Wrapf(
+			ErrInvalidBatchCancel,
+			"Batch cancel cannot have over %+v orders. Order count: %+v",
+			MaxMsgBatchCancelBatchSize,
+			totalNumberCancels,
+		)
+	}
+	return nil
+}
