da21726598c2c929ce03ed3665745144a1c2b23a
dydxwill <119354122+dydxwill@users.noreply.github.com>
2024-03-01 14:15:29 -0500

    [OTE-141] Implement skeleton for post /compliance/geoblock endpoint (#1111)

--- indexer/services/comlink/src/controllers/api/v4/compliance-v2-controller.ts
+++ indexer/services/comlink/src/controllers/api/v4/compliance-v2-controller.ts
@@ -1,4 +1,5 @@
-import { stats, TooManyRequestsError } from '@dydxprotocol-indexer/base';
+import { Secp256k1, sha256, ExtendedSecp256k1Signature } from '@cosmjs/crypto';
+import { logger, stats, TooManyRequestsError } from '@dydxprotocol-indexer/base';
 import {
   ComplianceReason,
   ComplianceStatus,
@@ -15,7 +16,7 @@ import {
 import { getReqRateLimiter } from '../../../caches/rate-limiters';
 import config from '../../../config';
 import { complianceProvider } from '../../../helpers/compliance/compliance-clients';
-import { DYDX_ADDRESS_PREFIX } from '../../../lib/constants';
+import { DYDX_ADDRESS_PREFIX, GEOBLOCK_REQUEST_TTL_SECONDS } from '../../../lib/constants';
 import { create4xxResponse, handleControllerError } from '../../../lib/helpers';
 import { rateLimiterMiddleware } from '../../../lib/rate-limit';
 import { getIpAddr } from '../../../lib/utils';
@@ -27,6 +28,11 @@ import { ComplianceControllerHelper } from './compliance-controller';
 
 const router: express.Router = express.Router();
 const controllerName: string = 'compliance-v2-controller';
+
+export enum ComplianceAction {
+  ONBOARD = 'ONBOARD',
+  CONNECT = 'CONNECT',
+}
 
 @Route('compliance')
 class ComplianceV2Controller extends Controller {
@@ -130,7 +136,7 @@ router.get(
         );
       }
       return handleControllerError(
-        'ComplianceV2Controller GET /',
+        'ComplianceV2Controller GET /screen/:address',
         'Compliance error',
         error,
         req,
@@ -139,6 +145,91 @@ router.get(
     } finally {
       stats.timing(
         `${config.SERVICE_NAME}.${controllerName}.compliance_screen.timing`,
+        Date.now() - start,
+      );
+    }
+  },
+);
+
+router.post(
+  '/geoblock',
+  handleValidationErrors,
+  ExportResponseCodeStats({ controllerName }),
+  async (req: express.Request, res: express.Response) => {
+    const start: number = Date.now();
+
+    const {
+      address,
+      message,
+      currentStatus,
+      action,
+      signedMessage,
+      pubkey,
+      timestamp,
+    }: {
+      address: string,
+      message: string,
+      currentStatus?: string,
+      action: ComplianceAction,
+      signedMessage: Uint8Array,
+      pubkey: Uint8Array,
+      timestamp: number,  // UNIX timestamp in seconds
+    } = req.body;
+
+    try {
+      if (!address.startsWith(DYDX_ADDRESS_PREFIX)) {
+        return create4xxResponse(
+          res,
+          `Address ${address} is not a valid dYdX V4 address`,
+        );
+      }
+
+      // Verify the timestamp is within GEOBLOCK_REQUEST_TTL_SECONDS seconds of the current time
+      const now = DateTime.now().toSeconds();
+      if (Math.abs(now - timestamp) > GEOBLOCK_REQUEST_TTL_SECONDS) {
+        return create4xxResponse(
+          res,
+          `Timestamp is not within the valid range of ${GEOBLOCK_REQUEST_TTL_SECONDS} seconds`,
+        );
+      }
+
+      // Prepare the message for verification
+      const messageToSign: string = `${message}${action}${currentStatus || ''}${timestamp}`;
+      const messageHash: Uint8Array = sha256(Buffer.from(messageToSign));
+      const signature: ExtendedSecp256k1Signature = ExtendedSecp256k1Signature
+        .fromFixedLength(signedMessage);
+
+      // Verify the signature
+      const isValidSignature: boolean = await
+      Secp256k1.verifySignature(signature, messageHash, pubkey);
+      if (!isValidSignature) {
+        return create4xxResponse(
+          res,
+          'Signature verification failed',
+        );
+      }
+
+      // TODO(OTE-141): Implement logic.
+      const response = {
+        status: ComplianceStatus.COMPLIANT,
+      };
+
+      return res.send(response);
+    } catch (error) {
+      logger.error({
+        at: 'ComplianceV2Controller POST /geoblock',
+        message,
+        error,
+        params: JSON.stringify(req.params),
+        query: JSON.stringify(req.query),
+      });
+      return create4xxResponse(
+        res,
+        error.message,
+      );
+    } finally {
+      stats.timing(
+        `${config.SERVICE_NAME}.${controllerName}.geo_block.timing`,
         Date.now() - start,
       );
     }
