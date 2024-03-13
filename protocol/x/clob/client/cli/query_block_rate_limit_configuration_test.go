bee83f593c35ec242b8ebd8f22ca5956271f624f
Jonathan Fung <jonathan@dydx.exchange>
2024-03-13 00:12:18 -0700

    fix cli rate limit test

--- protocol/x/clob/client/cli/query_block_rate_limit_configuration_test.go
+++ protocol/x/clob/client/cli/query_block_rate_limit_configuration_test.go
@@ -4,12 +4,13 @@ package cli_test
 
 import (
 	"fmt"
+	"testing"
+
 	tmcli "github.com/cometbft/cometbft/libs/cli"
 	clitestutil "github.com/cosmos/cosmos-sdk/testutil/cli"
 	"github.com/dydxprotocol/v4-chain/protocol/x/clob/client/cli"
 	"github.com/dydxprotocol/v4-chain/protocol/x/clob/types"
 	"github.com/stretchr/testify/require"
-	"testing"
 )
 
 var (
@@ -17,6 +18,7 @@ var (
 		MaxShortTermOrdersPerNBlocks:             []types.MaxPerNBlocksRateLimit{},
 		MaxStatefulOrdersPerNBlocks:              []types.MaxPerNBlocksRateLimit{},
 		MaxShortTermOrderCancellationsPerNBlocks: []types.MaxPerNBlocksRateLimit{},
+		MaxShortTermOrdersAndCancelsPerNBlocks:   []types.MaxPerNBlocksRateLimit{},
 	}
 )
 
