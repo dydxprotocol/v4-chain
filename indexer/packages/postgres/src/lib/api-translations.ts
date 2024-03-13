7953365320a692023e87ecb0408250a7709addb7
Shrenuj Bansal <shrenuj@dydx.exchange>
2024-03-13 12:14:14 -0400

    Add util to convert between parent and child subaccount numbers
    Signed-off-by: Shrenuj Bansal <shrenuj@dydx.exchange>

--- indexer/packages/postgres/src/lib/api-translations.ts
+++ indexer/packages/postgres/src/lib/api-translations.ts
@@ -1,4 +1,4 @@
-import { TIME_IN_FORCE_TO_API_TIME_IN_FORCE } from '../constants';
+import { TIME_IN_FORCE_TO_API_TIME_IN_FORCE, CHILD_SUBACCOUNT_MULTIPLIER, MAX_PARENT_SUBACCOUNTS } from '../constants';
 import { APITimeInForce, TimeInForce } from '../types';
 
 /**
@@ -19,4 +19,31 @@ export function isOrderTIFPostOnly(timeInForce: TimeInForce): boolean {
  */
 export function orderTIFToAPITIF(timeInForce: TimeInForce): APITimeInForce {
   return TIME_IN_FORCE_TO_API_TIME_IN_FORCE[timeInForce];
+}
+
+/**
+ * Gets a list of all possible child subaccount numbers for a parent subaccount number
+ * Child subaccounts = [128*0+parentSubaccount, 128*1+parentSubaccount ... 128*999+parentSubaccount]
+ * @param parentSubaccount
+ * @returns
+ */
+export function getChildSubaccountNums(parentSubaccountNum: number): number[] {
+  if (parentSubaccountNum >= MAX_PARENT_SUBACCOUNTS) {
+    throw new Error(`Parent subaccount number must be less than ${MAX_PARENT_SUBACCOUNTS}`);
+  }
+  return Array.from({ length: CHILD_SUBACCOUNT_MULTIPLIER },
+    (_, i) => MAX_PARENT_SUBACCOUNTS * i + parentSubaccountNum);
+}
+
+/**
+ * Gets the parent subaccount number from a child subaccount number
+ * Parent subaccount = childSubaccount / 128
+ * @param childSubaccountNum
+ * @returns
+ */
+export function getParentSubaccountNum(childSubaccountNum: number): number {
+  if (childSubaccountNum > MAX_PARENT_SUBACCOUNTS * CHILD_SUBACCOUNT_MULTIPLIER) {
+    throw new Error(`Child subaccount number must be less than ${MAX_PARENT_SUBACCOUNTS * CHILD_SUBACCOUNT_MULTIPLIER}`);
+  }
+  return Math.floor(childSubaccountNum % MAX_PARENT_SUBACCOUNTS);
 }
