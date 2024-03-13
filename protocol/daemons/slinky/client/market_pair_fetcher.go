3c052332b6aa5da30d43f6412cd648087af53c45
Eric Warehime <eric.warehime@gmail.com>
2024-03-13 08:25:10 -0700

    Squashed commit of the following: (#1109)
    commit 4061b7330cb8c046c83e8596eb97e9f0666f5524
    Author: Eric <eric.warehime@gmail.com>
    Date:   Tue Mar 12 14:23:48 2024 -0700
    
        Fix test failure
    
    commit d59fdc8e2e3f939fc75a67b6d8047c8c445d7b6c
    Author: Eric <eric.warehime@gmail.com>
    Date:   Mon Mar 11 13:03:53 2024 -0700
    
        Update sample pregenesis
    
    commit 18028fd095bb2dada4b89033f1afb3204122562c
    Merge: b23d4612 340801a3
    Author: Eric <eric.warehime@gmail.com>
    Date:   Mon Mar 11 12:57:44 2024 -0700
    
        Merge remote-tracking branch 'upstream' into eric/sidecar-integration
    
    commit b23d4612c2cd1a842301851649c44f896979b353
    Author: Eric <eric.warehime@gmail.com>
    Date:   Mon Mar 11 12:57:40 2024 -0700
    
        Fix config paths in Dockerfile
    
    commit 9c202cdd05b622c9d7a4ce27b061030a4732edb6
    Author: Eric <eric.warehime@gmail.com>
    Date:   Fri Mar 8 10:01:21 2024 -0800
    
        Remove min exchanges check
    
    commit 63b4a0cc7485763c03843ad2fbf7722d43a14a13
    Author: Eric <eric.warehime@gmail.com>
    Date:   Wed Mar 6 18:57:45 2024 -0800
    
        Write multiple index prices for slinky to cache
    
    commit 11df103608a948fffc8a909828935ffb684db0e9
    Author: Eric <eric.warehime@gmail.com>
    Date:   Wed Mar 6 18:38:48 2024 -0800
    
        Push prices directly to indexPriceCache
    
    commit 51d448d3dee104d91b42ecec5cc687a3b7f279b5
    Merge: 54a8f959 a5118b77
    Author: Eric <eric.warehime@gmail.com>
    Date:   Tue Mar 5 22:51:20 2024 -0800
    
        Merge remote-tracking branch 'upstream' into eric/sidecar-integration
    
    commit 54a8f95938e31a973092b325908e95daa9ba7c47
    Author: Eric <eric.warehime@gmail.com>
    Date:   Tue Mar 5 22:47:54 2024 -0800
    
        Remove dupe flag
    
    commit b4bed6a7341a44ec622b9e20f63d257f8443b29f
    Author: Eric <eric.warehime@gmail.com>
    Date:   Tue Mar 5 22:46:06 2024 -0800
    
        Move flags and provide cli overrides
    
    commit b685cfca3180dbb4380d91721ab6453dfd9430b9
    Author: Eric <eric.warehime@gmail.com>
    Date:   Tue Mar 5 22:06:02 2024 -0800
    
        Change log to error level
    
    commit 10f86ab5a14d74be0bc709f958c2e8aa8910b2c2
    Author: Eric <eric.warehime@gmail.com>
    Date:   Tue Mar 5 16:51:34 2024 -0800
    
        Add slinky configs to repo
    
    commit 88675f5198c9cea74428260f03142cc32568c462
    Author: Eric <eric.warehime@gmail.com>
    Date:   Tue Feb 27 12:46:33 2024 -0800
    
        Register daemon with health monitor
    
    commit 8438d542e9b8e4e1cab451e34232ba2c29346acb
    Author: Eric <eric.warehime@gmail.com>
    Date:   Tue Feb 27 12:32:36 2024 -0800
    
        PR updates
    
    commit 3046d6940ac8f6d4acb29ca8b8bb74d91783d9cd
    Author: Eric <eric.warehime@gmail.com>
    Date:   Mon Feb 26 15:05:52 2024 -0800
    
        Make unflaky
    
    commit 7799d29bcfa761b1c3712535de0f4eac772cb249
    Author: Eric <eric.warehime@gmail.com>
    Date:   Mon Feb 26 15:01:18 2024 -0800
    
        Lints, more tests
    
    commit e0a8c378900974f1696cf59f5e96ddc0fd85ea91
    Author: Eric <eric.warehime@gmail.com>
    Date:   Mon Feb 26 14:35:11 2024 -0800
    
        Add tests
    
    commit d8e5e9061d376f21c250c821abe5db10629d6196
    Author: Eric <eric.warehime@gmail.com>
    Date:   Sat Feb 24 21:01:35 2024 -0800
    
        Add interfaces, generate mocks
    
    commit 3a8ebec048aca5dbb62ad143133618411fe942e9
    Author: Eric <eric.warehime@gmail.com>
    Date:   Sat Feb 24 10:18:33 2024 -0800
    
        Lint
    
    commit 5cdd35f437190da48399ff8c81e260429b56f877
    Author: Eric <eric.warehime@gmail.com>
    Date:   Sat Feb 24 10:09:56 2024 -0800
    
        Initial commit of sidecar integration

---
+++ protocol/daemons/slinky/client/market_pair_fetcher.go
@@ -0,0 +1,108 @@
+package client
+
+import (
+	"context"
+	"fmt"
+	"sync"
+
+	"cosmossdk.io/log"
+	"google.golang.org/grpc"
+
+	oracletypes "github.com/skip-mev/slinky/x/oracle/types"
+
+	appflags "github.com/dydxprotocol/v4-chain/protocol/app/flags"
+	daemontypes "github.com/dydxprotocol/v4-chain/protocol/daemons/types"
+	"github.com/dydxprotocol/v4-chain/protocol/lib/slinky"
+	pricetypes "github.com/dydxprotocol/v4-chain/protocol/x/prices/types"
+)
+
+// MarketPairFetcher is a lightweight process run in a goroutine by the slinky client.
+// Its purpose is to periodically query the prices module for MarketParams and create
+// an easily indexed mapping between Slinky's CurrencyPair type and the corresponding ID
+// in the x/prices module.
+type MarketPairFetcher interface {
+	Start(context.Context, appflags.Flags, daemontypes.GrpcClient) error
+	Stop()
+	GetIDForPair(oracletypes.CurrencyPair) (uint32, error)
+	FetchIdMappings(context.Context) error
+}
+
+// MarketPairFetcherImpl implements the MarketPairFetcher interface.
+type MarketPairFetcherImpl struct {
+	Logger            log.Logger
+	QueryConn         *grpc.ClientConn
+	PricesQueryClient pricetypes.QueryClient
+
+	// compatMappings stores a mapping between CurrencyPair and the corresponding market(param|price) ID
+	compatMappings map[oracletypes.CurrencyPair]uint32
+	compatMu       sync.RWMutex
+}
+
+func NewMarketPairFetcher(logger log.Logger) MarketPairFetcher {
+	return &MarketPairFetcherImpl{
+		Logger:         logger,
+		compatMappings: make(map[oracletypes.CurrencyPair]uint32),
+	}
+}
+
+// Start opens the grpc connections for the fetcher.
+func (m *MarketPairFetcherImpl) Start(
+	ctx context.Context,
+	appFlags appflags.Flags,
+	grpcClient daemontypes.GrpcClient) error {
+	// Create the query client connection
+	queryConn, err := grpcClient.NewTcpConnection(ctx, appFlags.GrpcAddress)
+	if err != nil {
+		m.Logger.Error(
+			"Failed to establish gRPC connection",
+			"gRPC address", appFlags.GrpcAddress,
+			"error", err,
+		)
+		return err
+	}
+	m.PricesQueryClient = pricetypes.NewQueryClient(queryConn)
+	return nil
+}
+
+// Stop closes all existing connections.
+func (m *MarketPairFetcherImpl) Stop() {
+	if m.QueryConn != nil {
+		_ = m.QueryConn.Close()
+	}
+}
+
+// GetIDForPair returns the ID corresponding to the currency pair in the x/prices module.
+// If the currency pair is not found it will return an error.
+func (m *MarketPairFetcherImpl) GetIDForPair(cp oracletypes.CurrencyPair) (uint32, error) {
+	var id uint32
+	m.compatMu.RLock()
+	defer m.compatMu.RUnlock()
+	id, found := m.compatMappings[cp]
+	if !found {
+		return id, fmt.Errorf("pair %s not found in compatMappings", cp.String())
+	}
+	return id, nil
+}
+
+// FetchIdMappings is run periodically to refresh the cache of known mappings between
+// CurrencyPair and MarketParam ID.
+func (m *MarketPairFetcherImpl) FetchIdMappings(ctx context.Context) error {
+	// fetch all market params
+	resp, err := m.PricesQueryClient.AllMarketParams(ctx, &pricetypes.QueryAllMarketParamsRequest{})
+	if err != nil {
+		return err
+	}
+	var compatMappings = make(map[oracletypes.CurrencyPair]uint32, len(resp.MarketParams))
+	for _, mp := range resp.MarketParams {
+		cp, err := slinky.MarketPairToCurrencyPair(mp.Pair)
+		if err != nil {
+			return err
+		}
+		m.Logger.Debug("Mapped market to pair", "market id", mp.Id, "currency pair", cp.String())
+		compatMappings[cp] = mp.Id
+	}
+	m.compatMu.Lock()
+	defer m.compatMu.Unlock()
+	m.compatMappings = compatMappings
+	return nil
+}
---
+++ protocol/daemons/slinky/client/market_pair_fetcher_test.go
@@ -0,0 +1,91 @@
+package client_test
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"cosmossdk.io/log"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+
+	slinkytypes "github.com/skip-mev/slinky/x/oracle/types"
+
+	"github.com/dydxprotocol/v4-chain/protocol/daemons/slinky/client"
+	"github.com/dydxprotocol/v4-chain/protocol/mocks"
+	"github.com/dydxprotocol/v4-chain/protocol/x/prices/types"
+)
+
+func TestMarketPairFetcher(t *testing.T) {
+	logger := log.NewTestLogger(t)
+	queryClient := mocks.NewQueryClient(t)
+	fetcher := client.MarketPairFetcherImpl{
+		Logger:            logger,
+		PricesQueryClient: queryClient,
+	}
+	asset0 := "FOO"
+	asset1 := "BAR"
+	pair0 := types.MarketParam{Id: 0, Pair: fmt.Sprintf("%s-%s", asset0, asset1)}
+	pair1 := types.MarketParam{Id: 1, Pair: fmt.Sprintf("%s-%s", asset1, asset0)}
+	invalidPair := types.MarketParam{Id: 2, Pair: "foobar"}
+
+	t.Run("caches and returns valid pairs", func(t *testing.T) {
+		queryClient.
+			On("AllMarketParams", mock.Anything, mock.Anything).
+			Return(
+				&types.QueryAllMarketParamsResponse{
+					MarketParams: []types.MarketParam{
+						pair0,
+						pair1,
+					}},
+				nil,
+			).Once()
+		err := fetcher.FetchIdMappings(context.Background())
+		require.NoError(t, err)
+		id, err := fetcher.GetIDForPair(slinkytypes.CurrencyPair{Base: asset0, Quote: asset1})
+		require.NoError(t, err)
+		require.Equal(t, pair0.Id, id)
+		id, err = fetcher.GetIDForPair(slinkytypes.CurrencyPair{Base: asset1, Quote: asset0})
+		require.NoError(t, err)
+		require.Equal(t, pair1.Id, id)
+	})
+
+	t.Run("errors on fetch non-cached pair", func(t *testing.T) {
+		queryClient.
+			On("AllMarketParams", mock.Anything, mock.Anything).
+			Return(
+				&types.QueryAllMarketParamsResponse{
+					MarketParams: []types.MarketParam{}},
+				nil,
+			).Once()
+		err := fetcher.FetchIdMappings(context.Background())
+		require.NoError(t, err)
+		_, err = fetcher.GetIDForPair(slinkytypes.CurrencyPair{Base: asset0, Quote: asset1})
+		require.Error(t, err, fmt.Errorf("pair %s/%s not found in compatMappings", asset0, asset1))
+	})
+
+	t.Run("fails on fetching invalid pairs", func(t *testing.T) {
+		queryClient.
+			On("AllMarketParams", mock.Anything, mock.Anything).
+			Return(
+				&types.QueryAllMarketParamsResponse{
+					MarketParams: []types.MarketParam{
+						invalidPair,
+					}},
+				nil,
+			).Once()
+		err := fetcher.FetchIdMappings(context.Background())
+		require.Error(t, err, "incorrectly formatted CurrencyPair: foobar")
+	})
+
+	t.Run("fails on prices query error", func(t *testing.T) {
+		queryClient.
+			On("AllMarketParams", mock.Anything, mock.Anything).
+			Return(
+				&types.QueryAllMarketParamsResponse{},
+				fmt.Errorf("test error"),
+			).Once()
+		err := fetcher.FetchIdMappings(context.Background())
+		require.Error(t, err, "test error")
+	})
+}
