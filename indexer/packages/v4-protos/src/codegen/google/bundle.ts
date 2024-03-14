20ffef4b58aa7ca319865de65541c7f860729d74
Tian <tian@dydx.exchange>
2024-03-14 11:36:00 -0400

    [tra-87] add x/vault protos (#1169)
    * add x/vault protos

--- indexer/packages/v4-protos/src/codegen/google/bundle.ts
+++ indexer/packages/v4-protos/src/codegen/google/bundle.ts
@@ -1,16 +1,16 @@
-import * as _102 from "./api/annotations";
-import * as _103 from "./api/http";
-import * as _104 from "./protobuf/descriptor";
-import * as _105 from "./protobuf/duration";
-import * as _106 from "./protobuf/timestamp";
-import * as _107 from "./protobuf/any";
+import * as _103 from "./api/annotations";
+import * as _104 from "./api/http";
+import * as _105 from "./protobuf/descriptor";
+import * as _106 from "./protobuf/duration";
+import * as _107 from "./protobuf/timestamp";
+import * as _108 from "./protobuf/any";
 export namespace google {
-  export const api = { ..._102,
-    ..._103
+  export const api = { ..._103,
+    ..._104
   };
-  export const protobuf = { ..._104,
-    ..._105,
+  export const protobuf = { ..._105,
     ..._106,
-    ..._107
+    ..._107,
+    ..._108
   };
 }
---
+++ protocol/x/vault/keeper/msg_server_deposit_to_vault.go
@@ -0,0 +1,15 @@
+package keeper
+
+import (
+	"context"
+
+	"github.com/dydxprotocol/v4-chain/protocol/x/vault/types"
+)
+
+// DepositToVault deposits from a subaccount to a vault.
+func (k msgServer) DepositToVault(
+	goCtx context.Context,
+	msg *types.MsgDepositToVault,
+) (*types.MsgDepositToVaultResponse, error) {
+	return &types.MsgDepositToVaultResponse{}, nil
+}
