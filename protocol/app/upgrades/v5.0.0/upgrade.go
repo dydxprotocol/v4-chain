5167d863ae2a51212161217b8ab3be33b8aa6c18
Jonathan Fung <jonathan@dydx.exchange>
2024-03-18 13:55:00 -0400

    upgrade handler for batch cancel configs

--- protocol/app/upgrades/v5.0.0/upgrade.go
+++ protocol/app/upgrades/v5.0.0/upgrade.go
@@ -6,6 +6,7 @@ import (
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
+	clobtypes "github.com/dydxprotocol/v4-chain/protocol/x/clob/types"
 	perptypes "github.com/dydxprotocol/v4-chain/protocol/x/perpetuals/types"
 
 	upgradetypes "cosmossdk.io/x/upgrade/types"
@@ -30,10 +31,41 @@ func perpetualsUpgrade(
 	}
 }
 
+func blockRateLimitConfigUpdate(
+	ctx sdk.Context,
+	clobKeeper clobtypes.ClobKeeper,
+) {
+	// Based off of https://docs.dydx.exchange/trading/rate_limits
+	blockRateLimitConfig := clobtypes.BlockRateLimitConfiguration{
+		// Kept the same
+		MaxStatefulOrdersPerNBlocks: []clobtypes.MaxPerNBlocksRateLimit{
+			{
+				NumBlocks: 2,
+				Limit:     1,
+			},
+			{
+				NumBlocks: 20,
+				Limit:     100,
+			},
+		},
+		MaxShortTermOrdersAndCancelsPerNBlocks: []clobtypes.MaxPerNBlocksRateLimit{
+			{
+				NumBlocks: 5,
+				Limit:     2000,
+			},
+		},
+	}
+
+	if err := clobKeeper.InitializeBlockRateLimit(ctx, blockRateLimitConfig); err != nil {
+		panic(fmt.Sprintf("failed to update the block rate limit configuration: %s", err))
+	}
+}
+
 func CreateUpgradeHandler(
 	mm *module.Manager,
 	configurator module.Configurator,
 	perpetualsKeeper perptypes.PerpetualsKeeper,
+	clobKeeper clobtypes.ClobKeeper,
 ) upgradetypes.UpgradeHandler {
 	return func(ctx context.Context, plan upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
 		sdkCtx := lib.UnwrapSDKContext(ctx, "app/upgrades")
@@ -41,6 +73,9 @@ func CreateUpgradeHandler(
 
 		// Set all perpetuals to cross market type
 		perpetualsUpgrade(sdkCtx, perpetualsKeeper)
+
+		// Set block rate limit configuration
+		blockRateLimitConfigUpdate(sdkCtx, clobKeeper)
 
 		// TODO(TRA-93): Initialize `x/vault` module.
 
