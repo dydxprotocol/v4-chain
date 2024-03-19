2d235da8939b79e9cf7005dc4c1334937b0a23f6
Eric <eric.warehime@gmail.com>
2024-03-18 18:28:05 -0700

    Remove unncessary test vars

--- protocol/daemons/slinky/client/price_fetcher_test.go
+++ protocol/daemons/slinky/client/price_fetcher_test.go
@@ -3,52 +3,28 @@ package client_test
 import (
 	"context"
 	"fmt"
-	"sync"
 	"testing"
 	"time"
 
 	"cosmossdk.io/log"
 	"github.com/skip-mev/slinky/service/servers/oracle/types"
 	"github.com/stretchr/testify/mock"
-	"github.com/stretchr/testify/suite"
+	"github.com/stretchr/testify/require"
 
-	appflags "github.com/dydxprotocol/v4-chain/protocol/app/flags"
-	daemonflags "github.com/dydxprotocol/v4-chain/protocol/daemons/flags"
 	pricefeed_types "github.com/dydxprotocol/v4-chain/protocol/daemons/pricefeed/types"
 	pricefeedserver_types "github.com/dydxprotocol/v4-chain/protocol/daemons/server/types/pricefeed"
 	"github.com/dydxprotocol/v4-chain/protocol/daemons/slinky/client"
 	"github.com/dydxprotocol/v4-chain/protocol/mocks"
-	"github.com/dydxprotocol/v4-chain/protocol/testutil/appoptions"
 )
 
-func TestPriceFetcherTestSuite(t *testing.T) {
-	suite.Run(t, &PriceFetcherTestSuite{})
-}
-
-type PriceFetcherTestSuite struct {
-	suite.Suite
-	daemonFlags daemonflags.DaemonFlags
-	appFlags    appflags.Flags
-	wg          sync.WaitGroup
-}
-
-func (p *PriceFetcherTestSuite) SetupTest() {
-	// Setup daemon and grpc servers.
-	p.daemonFlags = daemonflags.GetDefaultDaemonFlags()
-	p.appFlags = appflags.GetFlagValuesFromOptions(appoptions.GetDefaultTestAppOptions("", nil))
-}
-
-func (p *PriceFetcherTestSuite) TearDownTest() {
-}
-
-func (p *PriceFetcherTestSuite) TestPriceFetcher() {
-	logger := log.NewTestLogger(p.T())
-	mpf := mocks.NewMarketPairFetcher(p.T())
-	slinky := mocks.NewOracleClient(p.T())
+func TestPriceFetcher(t *testing.T) {
+	logger := log.NewTestLogger(t)
+	mpf := mocks.NewMarketPairFetcher(t)
+	slinky := mocks.NewOracleClient(t)
 	slinky.On("Stop").Return(nil)
 	var fetcher client.PriceFetcher
 
-	p.Run("fetches prices on valid inputs", func() {
+	t.Run("fetches prices on valid inputs", func(t *testing.T) {
 		slinky.On("Start", mock.Anything).Return(nil).Once()
 		slinky.On("Prices", mock.Anything, mock.Anything).
 			Return(&types.QueryPricesResponse{
@@ -65,12 +41,12 @@ func (p *PriceFetcherTestSuite) TestPriceFetcher() {
 			slinky,
 			logger,
 		)
-		p.Require().NoError(fetcher.Start(context.Background()))
-		p.Require().NoError(fetcher.FetchPrices(context.Background()))
+		require.NoError(t, fetcher.Start(context.Background()))
+		require.NoError(t, fetcher.FetchPrices(context.Background()))
 		fetcher.Stop()
 	})
 
-	p.Run("errors on slinky.Prices failure", func() {
+	t.Run("errors on slinky.Prices failure", func(t *testing.T) {
 		slinky.On("Start", mock.Anything).Return(nil).Once()
 		slinky.On("Prices", mock.Anything, mock.Anything).
 			Return(&types.QueryPricesResponse{}, fmt.Errorf("foobar")).Once()
@@ -81,12 +57,12 @@ func (p *PriceFetcherTestSuite) TestPriceFetcher() {
 			logger,
 		)
 
-		p.Require().NoError(fetcher.Start(context.Background()))
-		p.Require().Errorf(fetcher.FetchPrices(context.Background()), "foobar")
+		require.NoError(t, fetcher.Start(context.Background()))
+		require.Errorf(t, fetcher.FetchPrices(context.Background()), "foobar")
 		fetcher.Stop()
 	})
 
-	p.Run("errors on slinky.Prices returning invalid currency pairs", func() {
+	t.Run("errors on slinky.Prices returning invalid currency pairs", func(t *testing.T) {
 		slinky.On("Start", mock.Anything).Return(nil).Once()
 		slinky.On("Prices", mock.Anything, mock.Anything).
 			Return(&types.QueryPricesResponse{
@@ -101,12 +77,12 @@ func (p *PriceFetcherTestSuite) TestPriceFetcher() {
 			logger,
 		)
 
-		p.Require().NoError(fetcher.Start(context.Background()))
-		p.Require().Errorf(fetcher.FetchPrices(context.Background()), "incorrectly formatted CurrencyPair")
+		require.NoError(t, fetcher.Start(context.Background()))
+		require.Errorf(t, fetcher.FetchPrices(context.Background()), "incorrectly formatted CurrencyPair")
 		fetcher.Stop()
 	})
 
-	p.Run("no-ops on marketPairFetcher currency pair not found", func() {
+	t.Run("no-ops on marketPairFetcher currency pair not found", func(t *testing.T) {
 		slinky.On("Start", mock.Anything).Return(nil).Once()
 		slinky.On("Prices", mock.Anything, mock.Anything).
 			Return(&types.QueryPricesResponse{
@@ -123,12 +99,12 @@ func (p *PriceFetcherTestSuite) TestPriceFetcher() {
 			slinky,
 			logger,
 		)
-		p.Require().NoError(fetcher.Start(context.Background()))
-		p.Require().NoError(fetcher.FetchPrices(context.Background()))
+		require.NoError(t, fetcher.Start(context.Background()))
+		require.NoError(t, fetcher.FetchPrices(context.Background()))
 		fetcher.Stop()
 	})
 
-	p.Run("continues on non-parsable price data", func() {
+	t.Run("continues on non-parsable price data", func(t *testing.T) {
 		slinky.On("Start", mock.Anything).Return(nil).Once()
 		slinky.On("Prices", mock.Anything, mock.Anything).
 			Return(&types.QueryPricesResponse{
@@ -145,12 +121,12 @@ func (p *PriceFetcherTestSuite) TestPriceFetcher() {
 			slinky,
 			logger,
 		)
-		p.Require().NoError(fetcher.Start(context.Background()))
-		p.Require().NoError(fetcher.FetchPrices(context.Background()))
+		require.NoError(t, fetcher.Start(context.Background()))
+		require.NoError(t, fetcher.FetchPrices(context.Background()))
 		fetcher.Stop()
 	})
 
-	p.Run("no-ops on empty price response", func() {
+	t.Run("no-ops on empty price response", func(t *testing.T) {
 		slinky.On("Start", mock.Anything).Return(nil).Once()
 		slinky.On("Prices", mock.Anything, mock.Anything).
 			Return(&types.QueryPricesResponse{
@@ -164,8 +140,8 @@ func (p *PriceFetcherTestSuite) TestPriceFetcher() {
 			slinky,
 			logger,
 		)
-		p.Require().NoError(fetcher.Start(context.Background()))
-		p.Require().NoError(fetcher.FetchPrices(context.Background()))
+		require.NoError(t, fetcher.Start(context.Background()))
+		require.NoError(t, fetcher.FetchPrices(context.Background()))
 		fetcher.Stop()
 	})
 }
