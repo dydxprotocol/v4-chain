14f2a64d5ae8bfd8012c2ce8903e8f50b1ab119d
affan <affan@dydx.exchange>
2024-03-22 14:00:08 -0400

    fix protos

--- indexer/packages/v4-protos/src/codegen/gogoproto/bundle.ts
+++ indexer/packages/v4-protos/src/codegen/gogoproto/bundle.ts
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-import * as _98 from "./gogo";
-export const gogoproto = { ..._98
-=======
-import * as _104 from "./gogo";
-export const gogoproto = { ..._104
->>>>>>> e545bbfe ([OTE-221] Add query for PendingSendPacket (#1176))
+import * as _99 from "./gogo";
+export const gogoproto = { ..._99
 };
--- indexer/packages/v4-protos/src/codegen/google/bundle.ts
+++ indexer/packages/v4-protos/src/codegen/google/bundle.ts
@@ -1,33 +1,16 @@
-<<<<<<< HEAD
-import * as _99 from "./api/annotations";
-import * as _100 from "./api/http";
-import * as _101 from "./protobuf/descriptor";
-import * as _102 from "./protobuf/duration";
-import * as _103 from "./protobuf/timestamp";
-import * as _104 from "./protobuf/any";
+import * as _100 from "./api/annotations";
+import * as _101 from "./api/http";
+import * as _102 from "./protobuf/descriptor";
+import * as _103 from "./protobuf/duration";
+import * as _104 from "./protobuf/timestamp";
+import * as _105 from "./protobuf/any";
 export namespace google {
-  export const api = { ..._99,
-    ..._100
+  export const api = { ..._100,
+    ..._101
   };
-  export const protobuf = { ..._101,
-    ..._102,
+  export const protobuf = { ..._102,
     ..._103,
-    ..._104
-=======
-import * as _105 from "./api/annotations";
-import * as _106 from "./api/http";
-import * as _107 from "./protobuf/descriptor";
-import * as _108 from "./protobuf/duration";
-import * as _109 from "./protobuf/timestamp";
-import * as _110 from "./protobuf/any";
-export namespace google {
-  export const api = { ..._105,
-    ..._106
-  };
-  export const protobuf = { ..._107,
-    ..._108,
-    ..._109,
-    ..._110
->>>>>>> e545bbfe ([OTE-221] Add query for PendingSendPacket (#1176))
+    ..._104,
+    ..._105
   };
 }
