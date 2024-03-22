d76e6668d3594170e3e1d4436053af8ce0681987
affan <affan@dydx.exchange>
2024-03-22 10:36:23 -0400

    nit

--- protocol/x/ratelimit/types/keys.go
+++ protocol/x/ratelimit/types/keys.go
@@ -32,17 +32,17 @@ func GetPendingSendPacketKey(channelId string, sequenceNumber uint64) []byte {
 	return []byte(fmt.Sprintf("%s_%d", channelId, sequenceNumber))
 }
 
-func SplitPendingSendPacketKey(key []byte) (channelId string, sequenceNumber uint64, err error) {
-	err = nil
+func SplitPendingSendPacketKey(key []byte) (string, uint64, error) {
+	err := error(nil)
 	parts := bytes.Split(key, []byte("_"))
 	if len(parts) != 2 {
 		err = fmt.Errorf("unexpected PendingSendPacket key format: %s", key)
-		return
+		return "", 0, err
 	}
-	channelId = string(parts[0])
-	// convert parts[1] to uint64 parts[1] is is a byte array with numeric characters of variable length
+	channelId := string(parts[0])
 
+	// convert parts[1] to uint64 parts[1] is a byte array with numeric characters of variable length
 	sequenceNumberInt, _ := strconv.Atoi(string(parts[1]))
-	sequenceNumber = uint64(sequenceNumberInt)
-	return
+	sequenceNumber := uint64(sequenceNumberInt)
+	return channelId, sequenceNumber, err
 }
