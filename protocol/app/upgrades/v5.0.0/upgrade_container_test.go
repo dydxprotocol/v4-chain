2432bbfa00003328c0a5914335044f97a8b2a354
Teddy Ding <teddy@dydx.exchange>
2024-03-26 15:50:46 -0400

    postUpgradeCheckLiquidityTiers

--- protocol/app/upgrades/v5.0.0/upgrade_container_test.go
+++ protocol/app/upgrades/v5.0.0/upgrade_container_test.go
@@ -38,6 +38,7 @@ func preUpgradeChecks(node *containertest.Node, t *testing.T) {
 
 func postUpgradeChecks(node *containertest.Node, t *testing.T) {
 	postUpgradecheckPerpetualMarketType(node, t)
+	postUpgradeCheckLiquidityTiers(node, t)
 	// Add test for your upgrade handler logic below
 }
 
@@ -71,4 +72,59 @@ func postUpgradecheckPerpetualMarketType(node *containertest.Node, t *testing.T)
 	for _, perpetual := range perpetualsList.Perpetual {
 		assert.Equal(t, perpetuals.PerpetualMarketType_PERPETUAL_MARKET_TYPE_CROSS, perpetual.Params.MarketType)
 	}
+}
+
+func postUpgradeCheckLiquidityTiers(node *containertest.Node, t *testing.T) {
+	resp, err := containertest.Query(
+		node,
+		perpetuals.NewQueryClient,
+		perpetuals.QueryClient.AllLiquidityTiers,
+		&perpetuals.QueryAllLiquidityTiersRequest{},
+	)
+	require.NoError(t, err)
+
+	liquidityTiersResponse := &perpetuals.QueryAllLiquidityTiersResponse{}
+	err = proto.UnmarshalText(resp.String(), liquidityTiersResponse)
+	require.NoError(t, err)
+
+	assert.Equal(t, 4, len(liquidityTiersResponse.LiquidityTiers))
+	assert.Equal(t, perpetuals.LiquidityTier{
+		Id:                     0,
+		Name:                   "Large-Cap",
+		InitialMarginPpm:       50_000,
+		MaintenanceFractionPpm: 600_000,
+		ImpactNotional:         10_000_000_000,
+		OpenInterestLowerCap:   uint64(0),
+		OpenInterestUpperCap:   uint64(0),
+	}, liquidityTiersResponse.LiquidityTiers[0])
+
+	assert.Equal(t, perpetuals.LiquidityTier{
+		Id:                     1,
+		Name:                   "Mid-Cap",
+		InitialMarginPpm:       100_000,
+		MaintenanceFractionPpm: 500_000,
+		ImpactNotional:         5_000_000_000,
+		OpenInterestLowerCap:   uint64(25_000_000_000_000),
+		OpenInterestUpperCap:   uint64(50_000_000_000_000),
+	}, liquidityTiersResponse.LiquidityTiers[1])
+
+	assert.Equal(t, perpetuals.LiquidityTier{
+		Id:                     2,
+		Name:                   "Long-Tail",
+		InitialMarginPpm:       200_000,
+		MaintenanceFractionPpm: 500_000,
+		ImpactNotional:         2_500_000_000,
+		OpenInterestLowerCap:   uint64(10_000_000_000_000),
+		OpenInterestUpperCap:   uint64(20_000_000_000_000),
+	}, liquidityTiersResponse.LiquidityTiers[2])
+
+	assert.Equal(t, perpetuals.LiquidityTier{
+		Id:                     3,
+		Name:                   "Safety",
+		InitialMarginPpm:       1_000_000,
+		MaintenanceFractionPpm: 200_000,
+		ImpactNotional:         2_500_000_000,
+		OpenInterestLowerCap:   uint64(500_000_000_000),
+		OpenInterestUpperCap:   uint64(1_000_000_000_000),
+	}, liquidityTiersResponse.LiquidityTiers[3])
 }
