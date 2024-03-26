076270e0a0bf57949d9772965025877863a0c65a
affan <affan@dydx.exchange>
2024-03-26 13:49:34 -0400

    Update LiquidityTierUpsertEvent to add OI caps

--- protocol/indexer/events/liquidity_tier_test.go
+++ protocol/indexer/events/liquidity_tier_test.go
@@ -12,12 +12,16 @@ func TestNewLiquidityTierUpsertEvent_Success(t *testing.T) {
 		"Large-Cap",
 		50000,
 		600000,
+		0,
+		1000000,
 	)
 	expectedLiquidityTierUpsertEventProto := &LiquidityTierUpsertEventV1{
 		Id:                     0,
 		Name:                   "Large-Cap",
 		InitialMarginPpm:       50000,
 		MaintenanceFractionPpm: 600000,
+		OpenInterestLowerCap:   0,
+		OpenInterestUpperCap:   1000000,
 	}
 	require.Equal(t, expectedLiquidityTierUpsertEventProto, liquidityTierUpsertEvent)
 }
