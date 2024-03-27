a75510040168e71ce8cf146993857c63cb5f7de5
Jonathan Fung <jonathan@dydx.exchange>
2024-03-27 15:21:40 -0400

    unused import

--- indexer/services/vulcan/__tests__/lib/on-message.test.ts
+++ indexer/services/vulcan/__tests__/lib/on-message.test.ts
@@ -1,7 +1,7 @@
 import { logger, stats } from '@dydxprotocol-indexer/base';
 import { createKafkaMessage, KafkaTopics } from '@dydxprotocol-indexer/kafka';
 import { OffChainUpdateV1 } from '@dydxprotocol-indexer/v4-protos';
-import { IHeaders, KafkaMessage } from 'kafkajs';
+import { KafkaMessage } from 'kafkajs';
 import { onMessage } from '../../src/lib/on-message';
 import { OrderPlaceHandler } from '../../src/handlers/order-place-handler';
 import { OrderRemoveHandler } from '../../src/handlers/order-remove-handler';
