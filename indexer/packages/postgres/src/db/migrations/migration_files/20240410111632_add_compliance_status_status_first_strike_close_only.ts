acb5d2386b482d5e99528f1f032890524bf229af
Will Liu <will@dydx.exchange>
2024-04-10 14:37:13 -0400

    use formatAlterTableEnumSql

--- indexer/packages/postgres/src/db/migrations/migration_files/20240410111632_add_compliance_status_status_first_strike_close_only.ts
+++ indexer/packages/postgres/src/db/migrations/migration_files/20240410111632_add_compliance_status_status_first_strike_close_only.ts
@@ -1,19 +1,19 @@
 import * as Knex from 'knex';
 
+import { formatAlterTableEnumSql } from '../helpers';
+
 export async function up(knex: Knex): Promise<void> {
-  return knex.schema.raw(`
-    ALTER TABLE "compliance_status"
-    DROP CONSTRAINT "compliance_status_status_check",
-    ADD CONSTRAINT "compliance_status_status_check" 
-    CHECK (status IN ('COMPLIANT', 'FIRST_STRIKE_CLOSE_ONLY', 'FIRST_STRIKE', 'CLOSE_ONLY', 'BLOCKED'))
-  `);
+  return knex.raw(formatAlterTableEnumSql(
+    'compliance_status',
+    'status',
+    ['COMPLIANT', 'FIRST_STRIKE_CLOSE_ONLY', 'FIRST_STRIKE', 'CLOSE_ONLY', 'BLOCKED'],
+  ));
 }
 
 export async function down(knex: Knex): Promise<void> {
-  return knex.schema.raw(`
-    ALTER TABLE "compliance_status"
-    DROP CONSTRAINT "compliance_status_status_check",
-    ADD CONSTRAINT "compliance_status_status_check" 
-    CHECK (status IN ('COMPLIANT', 'FIRST_STRIKE', 'CLOSE_ONLY', 'BLOCKED'))
-  `);
+  return knex.raw(formatAlterTableEnumSql(
+    'compliance_status',
+    'status',
+    ['COMPLIANT', 'FIRST_STRIKE', 'CLOSE_ONLY', 'BLOCKED'],
+  ));
 }
