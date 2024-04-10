b9b031b3a42b96dbe31e9bf3f546fd8ab07ee58d
Jay Yu <103467857+jayy04@users.noreply.github.com>
2024-04-10 12:45:42 -0400

    Add upgrade handler for v4.1.0

---
+++ protocol/app/upgrades/v4.1.0/constants.go
@@ -0,0 +1,13 @@
+package v_4_0_0
+
+import (
+	"github.com/dydxprotocol/v4-chain/protocol/app/upgrades"
+)
+
+const (
+	UpgradeName = "v4.1.0"
+)
+
+var Upgrade = upgrades.Upgrade{
+	UpgradeName: UpgradeName,
+}
