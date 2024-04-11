cc29d3255b10d469a32be425d56b478a8481cf23
Tian <tian@dydx.exchange>
2024-04-10 23:33:07 -0400

    add OwnerShares and AllVaults query and cli (#1362)

--- indexer/packages/v4-protos/src/codegen/dydxprotocol/vault/query.lcd.ts
+++ indexer/packages/v4-protos/src/codegen/dydxprotocol/vault/query.lcd.ts
@@ -1,5 +1,6 @@
+import { setPaginationParams } from "../../helpers";
 import { LCDClient } from "@osmonauts/lcd";
-import { QueryParamsRequest, QueryParamsResponseSDKType, QueryVaultRequest, QueryVaultResponseSDKType } from "./query";
+import { QueryParamsRequest, QueryParamsResponseSDKType, QueryVaultRequest, QueryVaultResponseSDKType, QueryAllVaultsRequest, QueryAllVaultsResponseSDKType, QueryOwnerSharesRequest, QueryOwnerSharesResponseSDKType } from "./query";
 export class LCDQueryClient {
   req: LCDClient;
 
@@ -11,6 +12,8 @@ export class LCDQueryClient {
     this.req = requestClient;
     this.params = this.params.bind(this);
     this.vault = this.vault.bind(this);
+    this.allVaults = this.allVaults.bind(this);
+    this.ownerShares = this.ownerShares.bind(this);
   }
   /* Queries the Params. */
 
@@ -25,6 +28,38 @@ export class LCDQueryClient {
   async vault(params: QueryVaultRequest): Promise<QueryVaultResponseSDKType> {
     const endpoint = `dydxprotocol/vault/vault/${params.type}/${params.number}`;
     return await this.req.get<QueryVaultResponseSDKType>(endpoint);
+  }
+  /* Queries all vaults. */
+
+
+  async allVaults(params: QueryAllVaultsRequest = {
+    pagination: undefined
+  }): Promise<QueryAllVaultsResponseSDKType> {
+    const options: any = {
+      params: {}
+    };
+
+    if (typeof params?.pagination !== "undefined") {
+      setPaginationParams(options, params.pagination);
+    }
+
+    const endpoint = `dydxprotocol/vault/vault`;
+    return await this.req.get<QueryAllVaultsResponseSDKType>(endpoint, options);
+  }
+  /* Queries owner shares of a vault. */
+
+
+  async ownerShares(params: QueryOwnerSharesRequest): Promise<QueryOwnerSharesResponseSDKType> {
+    const options: any = {
+      params: {}
+    };
+
+    if (typeof params?.pagination !== "undefined") {
+      setPaginationParams(options, params.pagination);
+    }
+
+    const endpoint = `dydxprotocol/vault/owner_shares/${params.type}/${params.number}`;
+    return await this.req.get<QueryOwnerSharesResponseSDKType>(endpoint, options);
   }
 
 }
