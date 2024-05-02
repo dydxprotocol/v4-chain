fb6c113b4c0372e28c03eb7b9ed6d627d8eb3e8a
vincentwschau <99756290+vincentwschau@users.noreply.github.com>
2024-05-02 13:46:54 -0400

    [CT-804] Fix stateful order removal event ordering. (#1456)

--- indexer/services/ender/src/lib/block-processor.ts
+++ indexer/services/ender/src/lib/block-processor.ts
@@ -55,6 +55,7 @@ const TXN_EVENT_SUBTYPE_VERSION_TO_VALIDATOR_MAPPING: Record<string, ValidatorIn
 const BLOCK_EVENT_SUBTYPE_VERSION_TO_VALIDATOR_MAPPING: Record<string, ValidatorInitializer> = {
   [serializeSubtypeAndVersion(DydxIndexerSubtypes.FUNDING.toString(), 1)]: FundingValidator,
   [serializeSubtypeAndVersion(DydxIndexerSubtypes.TRADING_REWARD.toString(), 1)]: TradingRewardsValidator,
+  [serializeSubtypeAndVersion(DydxIndexerSubtypes.STATEFUL_ORDER.toString(), 1)]: StatefulOrderValidator,
   [serializeSubtypeAndVersion(DydxIndexerSubtypes.OPEN_INTEREST_UPDATE.toString(), 1)]: OpenInterestUpdateValidator,
 };
 
