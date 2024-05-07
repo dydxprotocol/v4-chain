fae1504182f52a21cf1fca7651f33b62d949a841
Vincent Chau <99756290+vincentwschau@users.noreply.github.com>
2024-05-07 17:09:45 -0400

    All tests in one file.

--- indexer/services/socks/__tests__/lib/message-forwarder.test.ts
+++ indexer/services/socks/__tests__/lib/message-forwarder.test.ts
@@ -12,6 +12,7 @@ import {
   kafka,
   startConsumer,
   TRADES_WEBSOCKET_MESSAGE_VERSION,
+  SUBACCOUNTS_WEBSOCKET_MESSAGE_VERSION,
 } from '@dydxprotocol-indexer/kafka';
 import { MessageForwarder } from '../../src/lib/message-forwarder';
 import WebSocket from 'ws';
@@ -26,11 +27,14 @@ import {
   WebsocketEvents,
 } from '../../src/types';
 import { Admin } from 'kafkajs';
-import { TradeMessage } from '@dydxprotocol-indexer/v4-protos';
+import { SubaccountMessage, TradeMessage } from '@dydxprotocol-indexer/v4-protos';
 import { dbHelpers, testMocks, perpetualMarketRefresher } from '@dydxprotocol-indexer/postgres';
 import {
   btcClobPairId,
   btcTicker,
+  defaultChildAccNumber,
+  defaultChildSubaccountId,
+  defaultSubaccountId,
   ethClobPairId,
   ethTicker,
 } from '../constants';
@@ -51,6 +55,24 @@ describe('message-forwarder', () => {
     contents: '{}',
     clobPairId: btcClobPairId,
     version: TRADES_WEBSOCKET_MESSAGE_VERSION,
+  };
+
+  const baseSubaccountMessage: SubaccountMessage = {
+    blockHeight: '2',
+    transactionIndex: 2,
+    eventIndex: 2,
+    contents: '{}',
+    subaccountId: defaultSubaccountId,
+    version: SUBACCOUNTS_WEBSOCKET_MESSAGE_VERSION,
+  };
+
+  const childSubaccountMessage: SubaccountMessage = {
+    blockHeight: '2',
+    transactionIndex: 2,
+    eventIndex: 2,
+    contents: '{}',
+    subaccountId: defaultChildSubaccountId,
+    version: SUBACCOUNTS_WEBSOCKET_MESSAGE_VERSION,
   };
 
   const btcTradesMessages: TradeMessage[] = [
@@ -101,7 +123,33 @@ describe('message-forwarder', () => {
     },
   ];
 
+  const subaccountMessages: SubaccountMessage[] = [
+    {
+      ...baseSubaccountMessage,
+      contents: JSON.stringify({ val: '1' }),
+    },
+    {
+      ...baseSubaccountMessage,
+      contents: JSON.stringify({ val: '2' }),
+    },
+  ];
+
+  const childSubaccountMessages: SubaccountMessage[] = [
+    {
+      ...childSubaccountMessage,
+      contents: JSON.stringify({ val: '1' }),
+    },
+    {
+      ...childSubaccountMessage,
+      contents: JSON.stringify({ val: '2' }),
+    },
+  ];
+
   const mockAxiosResponse: Object = { a: 'b' };
+  const subaccountInitialMessage: Object = {
+    ...mockAxiosResponse,
+    orders: mockAxiosResponse,
+  };
 
   beforeAll(async () => {
     await dbHelpers.migrate();
@@ -233,6 +281,149 @@ describe('message-forwarder', () => {
     });
   });
 
+  it('Batch sends subaccount messages', (done: jest.DoneCallback) => {
+    const channel: Channel = Channel.V4_ACCOUNTS;
+    const id: string = `${defaultSubaccountId.owner}/${defaultSubaccountId.number}`;
+
+    const messageForwarder: MessageForwarder = new MessageForwarder(subscriptions, index);
+    subscriptions.start(messageForwarder.forwardToClient);
+    messageForwarder.start();
+
+    const ws = new WebSocket(WS_HOST);
+    let connectionId: string;
+
+    ws.on(WebsocketEvents.MESSAGE, async (message) => {
+      const msg: OutgoingMessage = JSON.parse(message.toString()) as OutgoingMessage;
+      if (msg.message_id === 0) {
+        connectionId = msg.connection_id;
+      }
+
+      if (msg.message_id === 1) {
+        // Check that the initial message is correct.
+        checkInitialMessage(
+          msg as SubscribedMessage,
+          connectionId,
+          channel,
+          id,
+          subaccountInitialMessage,
+        );
+
+        // await each message to ensure they are sent in order
+        for (const subaccountMessage of subaccountMessages) {
+          await producer.send({
+            topic: WebsocketTopics.TO_WEBSOCKETS_SUBACCOUNTS,
+            messages: [{
+              value: Buffer.from(
+                Uint8Array.from(
+                  SubaccountMessage.encode(subaccountMessage).finish(),
+                ),
+              ),
+              partition: 0,
+              timestamp: `${Date.now()}`,
+            }],
+          });
+        }
+      }
+
+      if (msg.message_id >= 2) {
+        const batchMsg: ChannelBatchDataMessage = JSON.parse(
+          message.toString(),
+        ) as ChannelBatchDataMessage;
+
+        checkBatchMessage(
+          batchMsg,
+          connectionId,
+          channel,
+          id,
+          SUBACCOUNTS_WEBSOCKET_MESSAGE_VERSION,
+          subaccountMessages,
+        );
+        done();
+      }
+    });
+
+    ws.on('open', () => {
+      ws.send(JSON.stringify({
+        type: IncomingMessageType.SUBSCRIBE,
+        channel,
+        id,
+        batched: true,
+      }));
+    });
+  });
+
+  it('Batch sends subaccount messages to parent subaccount channel', (done: jest.DoneCallback) => {
+    const channel: Channel = Channel.V4_PARENT_ACCOUNTS;
+    const id: string = `${defaultSubaccountId.owner}/${defaultSubaccountId.number}`;
+
+    const messageForwarder: MessageForwarder = new MessageForwarder(subscriptions, index);
+    subscriptions.start(messageForwarder.forwardToClient);
+    messageForwarder.start();
+
+    const ws = new WebSocket(WS_HOST);
+    let connectionId: string;
+
+    ws.on(WebsocketEvents.MESSAGE, async (message) => {
+      const msg: OutgoingMessage = JSON.parse(message.toString()) as OutgoingMessage;
+      if (msg.message_id === 0) {
+        connectionId = msg.connection_id;
+      }
+
+      if (msg.message_id === 1) {
+        // Check that the initial message is correct.
+        checkInitialMessage(
+          msg as SubscribedMessage,
+          connectionId,
+          channel,
+          id,
+          subaccountInitialMessage,
+        );
+
+        // await each message to ensure they are sent in order
+        for (const subaccountMessage of childSubaccountMessages) {
+          await producer.send({
+            topic: WebsocketTopics.TO_WEBSOCKETS_SUBACCOUNTS,
+            messages: [{
+              value: Buffer.from(
+                Uint8Array.from(
+                  SubaccountMessage.encode(subaccountMessage).finish(),
+                ),
+              ),
+              partition: 0,
+              timestamp: `${Date.now()}`,
+            }],
+          });
+        }
+      }
+
+      if (msg.message_id >= 2) {
+        const batchMsg: ChannelBatchDataMessage = JSON.parse(
+          message.toString(),
+        ) as ChannelBatchDataMessage;
+
+        checkBatchMessage(
+          batchMsg,
+          connectionId,
+          channel,
+          id,
+          SUBACCOUNTS_WEBSOCKET_MESSAGE_VERSION,
+          subaccountMessages,
+          defaultChildAccNumber,
+        );
+        done();
+      }
+    });
+
+    ws.on('open', () => {
+      ws.send(JSON.stringify({
+        type: IncomingMessageType.SUBSCRIBE,
+        channel,
+        id,
+        batched: true,
+      }));
+    });
+  });
+
   it('forwards messages', (done: jest.DoneCallback) => {
     const channel: Channel = Channel.V4_TRADES;
     const id: string = ethTicker;
@@ -320,6 +511,29 @@ function checkInitialMessage(
   expect(subscribedMessage.channel).toBe(channel);
   expect(subscribedMessage.id).toBe(id);
   expect(subscribedMessage.contents).toEqual(initialMessage);
+}
+
+function checkBatchMessage(
+  batchMsg: ChannelBatchDataMessage,
+  connectionId: string,
+  channel: string,
+  id: string,
+  version: string,
+  expectedMessages: {contents: string}[],
+  subaccountNumber?: number,
+): void {
+  expect(batchMsg.connection_id).toBe(connectionId);
+  expect(batchMsg.type).toBe(OutgoingMessageType.CHANNEL_BATCH_DATA);
+  expect(batchMsg.channel).toBe(channel);
+  expect(batchMsg.id).toBe(id);
+  expect(batchMsg.contents.length).toBe(expectedMessages.length);
+  expect(batchMsg.version).toBe(version);
+  expect(batchMsg.subaccountNumber).toBe(subaccountNumber);
+  batchMsg.contents.forEach(
+    (individualMessage: Object, idx: number) => {
+      expect(individualMessage).toEqual(JSON.parse(expectedMessages[idx].contents));
+    },
+  );
 }
 
 function checkVersionedBatchMessage(
