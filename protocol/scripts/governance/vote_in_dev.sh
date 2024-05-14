394d83ed4f6323c9581108dfd862e81f306266e6
Christopher Li <Christopherfooli@gmail.com>
2024-05-14 12:32:47 -0400

    nits

--- protocol/scripts/governance/vote_in_dev.sh
+++ protocol/scripts/governance/vote_in_dev.sh
@@ -1,4 +1,4 @@
-./build/dydxprotocold tx gov vote $1 yes --from alice --broadcast-mode sync --fees 5000000000000000adv4tnt --yes
-./build/dydxprotocold tx gov vote $1 yes --from bob --broadcast-mode sync --fees 5000000000000000adv4tnt --yes
-./build/dydxprotocold tx gov vote $1 yes --from carl --broadcast-mode sync --fees 5000000000000000adv4tnt --yes
-./build/dydxprotocold tx gov vote $1 yes --from dave --broadcast-mode sync --fees 5000000000000000adv4tnt --yes
+./build/dydxprotocold tx gov vote "$1" yes --from alice --broadcast-mode sync --fees 5000000000000000adv4tnt --yes
+./build/dydxprotocold tx gov vote "$1" yes --from bob --broadcast-mode sync --fees 5000000000000000adv4tnt --yes
+./build/dydxprotocold tx gov vote "$1" yes --from carl --broadcast-mode sync --fees 5000000000000000adv4tnt --yes
+./build/dydxprotocold tx gov vote "$1" yes --from dave --broadcast-mode sync --fees 5000000000000000adv4tnt --yes
