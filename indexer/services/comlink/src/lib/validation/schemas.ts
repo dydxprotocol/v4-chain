b6a6c55def1e3d87695fdf8f17eee7c73ef0f433
vincentwschau <99756290+vincentwschau@users.noreply.github.com>
2024-05-14 16:04:52 -0400

    Fix subaccount validation to account for updated subaccount limit. (#1513)

--- indexer/services/comlink/src/lib/validation/schemas.ts
+++ indexer/services/comlink/src/lib/validation/schemas.ts
@@ -1,8 +1,11 @@
-import { perpetualMarketRefresher } from '@dydxprotocol-indexer/postgres';
+import {
+  perpetualMarketRefresher,
+  MAX_PARENT_SUBACCOUNTS,
+  CHILD_SUBACCOUNT_MULTIPLIER,
+} from '@dydxprotocol-indexer/postgres';
 import { checkSchema, ParamSchema } from 'express-validator';
 
 import config from '../../config';
-import { MAX_SUBACCOUNT_NUMBER } from '../../constants';
 
 export const CheckSubaccountSchema = checkSchema({
   address: {
@@ -12,9 +15,9 @@ export const CheckSubaccountSchema = checkSchema({
   subaccountNumber: {
     in: ['params', 'query'],
     isInt: {
-      options: { gt: -1, lt: MAX_SUBACCOUNT_NUMBER + 1 },
+      options: { gt: -1, lt: MAX_PARENT_SUBACCOUNTS * CHILD_SUBACCOUNT_MULTIPLIER + 1 },
     },
-    errorMessage: 'subaccountNumber must be a non-negative integer less than 128',
+    errorMessage: 'subaccountNumber must be a non-negative integer less than 128001',
   },
 });
 
@@ -26,7 +29,7 @@ export const CheckParentSubaccountSchema = checkSchema({
   parentSubaccountNumber: {
     in: ['params', 'query'],
     isInt: {
-      options: { gt: -1, lt: MAX_SUBACCOUNT_NUMBER + 1 },
+      options: { gt: -1, lt: MAX_PARENT_SUBACCOUNTS },
     },
     errorMessage: 'parentSubaccountNumber must be a non-negative integer less than 128',
   },
