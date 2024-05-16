e2f00d274dc8628521fe97baf831e7dedeb1b3d7
Alejandro Gala <alegala21@gmail.com>
2024-05-15 20:40:46 -0700

    fix sending cli integration tests

--- protocol/x/subaccounts/client/testutil/cli_helpers.go
+++ protocol/x/subaccounts/client/testutil/cli_helpers.go
@@ -1,28 +1,27 @@
 package testutil
 
 import (
+	"bytes"
 	"fmt"
+	"os/exec"
 
-	sacli "github.com/StreamFinance-Protocol/stream-chain/protocol/x/subaccounts/client/cli"
-	sdk "github.com/cosmos/cosmos-sdk/types"
-
-	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/testutil"
-	clitestutil "github.com/cosmos/cosmos-sdk/testutil/cli"
 )
 
 // MsgQuerySubaccountExec executes a query for the given subaccount id.
 func MsgQuerySubaccountExec(
-	clientCtx client.Context,
-	owner sdk.AccAddress,
+	owner string,
 	number uint32,
 ) (testutil.BufferWriter, error) {
-	return clitestutil.ExecTestCLICmd(
-		clientCtx,
-		sacli.CmdShowSubaccount(),
-		[]string{
-			owner.String(),
-			fmt.Sprint(number),
-		},
-	)
+
+	queryCmd := exec.Command("bash", "-c", "docker exec interchain-security-instance interchain-security-cd query subaccounts show-subaccount "+owner+" "+fmt.Sprint(number)+" --node tcp://7.7.8.4:26658 -o json")
+	var transferOut bytes.Buffer
+	var stdTransferErr bytes.Buffer
+	queryCmd.Stdout = &transferOut
+	queryCmd.Stderr = &stdTransferErr
+	err := queryCmd.Run()
+	if err != nil {
+		return nil, err
+	}
+	return &transferOut, nil
 }
