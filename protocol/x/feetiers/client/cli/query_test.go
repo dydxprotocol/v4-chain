95886daaac6baee3222b2dc1eb931cbff23ff759
Alejandro Gala <alegala21@gmail.com>
2024-05-17 14:22:39 -0700

    fee teir test

--- protocol/x/feetiers/client/cli/query_test.go
+++ protocol/x/feetiers/client/cli/query_test.go
@@ -8,7 +8,6 @@ import (
 	"strconv"
 	"testing"
 
-	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/stretchr/testify/require"
 
 	"github.com/StreamFinance-Protocol/stream-chain/protocol/testutil/network"
@@ -17,30 +16,6 @@ import (
 
 // Prevent strconv unused error
 var _ = strconv.IntSize
-
-func setupNetwork(
-	t *testing.T,
-) (
-	*network.Network,
-	client.Context,
-) {
-	t.Helper()
-	cfg := network.DefaultConfig(nil)
-
-	// Init state.
-	state := types.GenesisState{}
-	require.NoError(t, cfg.Codec.UnmarshalJSON(cfg.GenesisState[types.ModuleName], &state))
-
-	state = *types.DefaultGenesis()
-
-	buf, err := cfg.Codec.MarshalJSON(&state)
-	require.NoError(t, err)
-	cfg.GenesisState[types.ModuleName] = buf
-	net := network.New(t, cfg)
-	ctx := net.Validators[0].ClientCtx
-
-	return net, ctx
-}
 
 func TestQueryPerpetualFeeParams(t *testing.T) {
 
@@ -58,17 +33,6 @@ func TestQueryPerpetualFeeParams(t *testing.T) {
 	require.Equal(t, types.DefaultGenesis().Params, resp.Params)
 }
 
-// func TestQueryPerpetualFeeParams(t *testing.T) {
-// 	net, ctx := setupNetwork(t)
-
-// 	out, err := clitestutil.ExecTestCLICmd(ctx, cli.CmdQueryPerpetualFeeParams(), []string{})
-
-// 	require.NoError(t, err)
-// 	var resp types.QueryPerpetualFeeParamsResponse
-// 	require.NoError(t, net.Config.Codec.UnmarshalJSON(out.Bytes(), &resp))
-// 	require.Equal(t, types.DefaultGenesis().Params, resp.Params)
-// }
-
 func TestQueryUserFeeTier(t *testing.T) {
 
 	cfg := network.DefaultConfig(nil)
@@ -83,13 +47,3 @@ func TestQueryUserFeeTier(t *testing.T) {
 	data := out.Bytes()
 	require.NoError(t, cfg.Codec.UnmarshalJSON(data, &resp))
 }
-
-// func TestQueryUserFeeTier(t *testing.T) {
-// 	net, ctx := setupNetwork(t)
-
-// 	out, err := clitestutil.ExecTestCLICmd(ctx, cli.CmdQueryUserFeeTier(), []string{"alice"})
-
-// 	require.NoError(t, err)
-// 	var resp types.QueryUserFeeTierResponse
-// 	require.NoError(t, net.Config.Codec.UnmarshalJSON(out.Bytes(), &resp))
-// }
