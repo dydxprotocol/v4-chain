dfa8ffc70cb43457c2caba2575e259a88835db68
Alejandro Gala <alegala21@gmail.com>
2024-05-17 14:23:34 -0700

    vest tests

--- protocol/x/vest/client/cli/query_vest_entry_test.go
+++ protocol/x/vest/client/cli/query_vest_entry_test.go
@@ -12,40 +12,13 @@ import (
 	"github.com/StreamFinance-Protocol/stream-chain/protocol/testutil/network"
 	"github.com/StreamFinance-Protocol/stream-chain/protocol/x/vest/types"
 	tmcli "github.com/cometbft/cometbft/libs/cli"
-	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/stretchr/testify/require"
 )
 
 // Prevent strconv unused error
 var _ = strconv.IntSize
 
-func setupNetwork(
-	t *testing.T,
-) (
-	*network.Network,
-	client.Context,
-) {
-	t.Helper()
-	cfg := network.DefaultConfig(nil)
-
-	// Init state.
-	state := types.GenesisState{}
-	require.NoError(t, cfg.Codec.UnmarshalJSON(cfg.GenesisState[types.ModuleName], &state))
-
-	state = *types.DefaultGenesis()
-
-	buf, err := cfg.Codec.MarshalJSON(&state)
-	require.NoError(t, err)
-	cfg.GenesisState[types.ModuleName] = buf
-	net := network.New(t, cfg)
-	ctx := net.Validators[0].ClientCtx
-
-	return net, ctx
-}
-
 func TestQueryVestEntry(t *testing.T) {
-
-	// net, ctx := setupNetwork(t)
 
 	queryAndCheckVestEntry(t, "rewards_vester", types.DefaultGenesis().VestEntries[0])
 	queryAndCheckVestEntry(t, "rewards_vester", types.DefaultGenesis().VestEntries[1])
@@ -72,22 +45,3 @@ func queryAndCheckVestEntry(
 	require.NoError(t, cfg.Codec.UnmarshalJSON(data, &resp))
 	require.Equal(t, types.DefaultGenesis().VestEntries[1], resp.Entry)
 }
-
-// func queryAndCheckVestEntry(
-// 	t *testing.T,
-// 	ctx client.Context,
-// 	net *network.Network,
-// 	vester_account string,
-// 	expectedEntry types.VestEntry,
-// ) {
-// 	out, err := clitestutil.ExecTestCLICmd(ctx, cli.CmdQueryVestEntry(), []string{
-// 		"rewards_vester",
-// 		fmt.Sprintf("--%s=json", tmcli.OutputFlag), // specify output format as json
-// 	})
-
-// 	require.NoError(t, err)
-// 	var resp types.QueryVestEntryResponse
-// 	outBytes := out.Bytes()
-// 	require.NoError(t, net.Config.Codec.UnmarshalJSON(outBytes, &resp))
-// 	require.Equal(t, types.DefaultGenesis().VestEntries[1], resp.Entry)
-// }
