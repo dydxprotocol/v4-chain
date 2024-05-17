2ecabe282a04a350a0cb21209d991d76967edbd4
Alejandro Gala <alegala21@gmail.com>
2024-05-17 14:23:13 -0700

    sending tests

--- protocol/x/sending/client/cli/sending_cli_test.go
+++ protocol/x/sending/client/cli/sending_cli_test.go
@@ -3,10 +3,8 @@
 package cli_test
 
 import (
-	"bytes"
 	"fmt"
 	"math/big"
-	"os/exec"
 	"testing"
 	"time"
 
@@ -56,81 +54,9 @@ func (s *SendingIntegrationTestSuite) SetupTest() {
 	// Set min gas prices to zero so that we can submit transactions with zero gas price.
 	s.cfg.MinGasPrices = fmt.Sprintf("0%s", sdk.DefaultBondDenom)
 
-	// // Setting genesis state for Sending.
-	// state := types.GenesisState{}
-
-	// buf, err := s.cfg.Codec.MarshalJSON(&state)
-	// s.NoError(err)
-	// s.cfg.GenesisState[types.ModuleName] = buf
-
-	// // Setting genesis state for Subaccounts.
-	// // Two subaccounts with non-zero USDC balances are added to the genesis state,
-	// // so that we can initiate transfers from these subaccounts and observe the changes in
-	// // their USDC positions.
-	// sastate := satypes.GenesisState{}
-	// sastate.Subaccounts = append(
-	// 	sastate.Subaccounts,
-	// 	satypes.Subaccount{
-	// 		Id: &satypes.SubaccountId{Owner: s.validatorAddress.String(), Number: subaccountNumberZero},
-	// 		AssetPositions: []*satypes.AssetPosition{
-	// 			&constants.Usdc_Asset_500,
-	// 		},
-	// 		PerpetualPositions: []*satypes.PerpetualPosition{},
-	// 	},
-	// 	satypes.Subaccount{
-	// 		Id: &satypes.SubaccountId{Owner: s.validatorAddress.String(), Number: subaccountNumberOne},
-	// 		AssetPositions: []*satypes.AssetPosition{
-	// 			&constants.Usdc_Asset_500,
-	// 		},
-	// 		PerpetualPositions: []*satypes.PerpetualPosition{},
-	// 	},
-	// )
-
-	// sagenesis := "\".app_state.subaccounts.subaccounts = [{\\\"id\\\": {\\\"number\\\": \\\"0\\\", \\\"owner\\\": \\\"dydxvaloper1eeeggku6dzk3mv7wph3zq035rhtd890s7hkzft\\\"}, \\\"asset_positions\\\": [{\\\"asset_id\\\": \\\"0\\\", \\\"quantums\\\": \\\"500000000\\\"}], \\\"perpetual_positions\\\": []}, {\\\"id\\\": {\\\"number\\\": \\\"1\\\", \\\"owner\\\": \\\"dydxvaloper1eeeggku6dzk3mv7wph3zq035rhtd890s7hkzft\\\"}, \\\"asset_positions\\\": [{\\\"asset_id\\\": \\\"0\\\", \\\"quantums\\\": \\\"500000000\\\"}], \\\"perpetual_positions\\\": []}]\"\"\""
-
-	// sabuf, err := s.cfg.Codec.MarshalJSON(&sastate)
-	// s.Require().NoError(err)
-	// s.cfg.GenesisState[satypes.ModuleName] = sabuf
-
-	// // Ensure that no funding-related epochs will occur during this test.
-	// epstate := constants.GenerateEpochGenesisStateWithoutFunding()
-
-	// epgenesis := "\".app_state.epochs.epoch_info_list = [{\\\"name\\\": \\\"funding-sample\\\", \\\"next_tick\\\": \\\"1747543084\\\", \\\"duration\\\": \\\"31536000\\\", \\\"current_epoch\\\": \\\"0\\\", \\\"current_epoch_start_block\\\": \\\"0\\\", \\\"fast_forward_next_tick\\\": false}, {\\\"name\\\": \\\"funding-tick\\\", \\\"next_tick\\\": \\\"1747543084\\\", \\\"duration\\\": \\\"31536000\\\", \\\"current_epoch\\\": \\\"0\\\", \\\"current_epoch_start_block\\\": \\\"0\\\", \\\"fast_forward_next_tick\\\": false}] \"\"\""
-
-	// epbuf, err := s.cfg.Codec.MarshalJSON(&epstate)
-	// s.Require().NoError(err)
-	// s.cfg.GenesisState[epochstypes.ModuleName] = epbuf
-
-	// s.network = network.New(s.T(), s.cfg)
-	// _, err = s.network.WaitForHeight(1)
 	genesisChanges := "\".app_state.subaccounts.subaccounts = [{\\\"id\\\": {\\\"number\\\": \\\"0\\\", \\\"owner\\\": \\\"dydx1eeeggku6dzk3mv7wph3zq035rhtd890smfq5z6\\\"}, \\\"asset_positions\\\": [{\\\"asset_id\\\": \\\"0\\\", \\\"quantums\\\": \\\"500000000\\\"}], \\\"perpetual_positions\\\": []}, {\\\"id\\\": {\\\"number\\\": \\\"1\\\", \\\"owner\\\": \\\"dydx1eeeggku6dzk3mv7wph3zq035rhtd890smfq5z6\\\"}, \\\"asset_positions\\\": [{\\\"asset_id\\\": \\\"0\\\", \\\"quantums\\\": \\\"500000000\\\"}], \\\"perpetual_positions\\\": []}] | .app_state.epochs.epoch_info_list = [{\\\"name\\\": \\\"funding-sample\\\", \\\"next_tick\\\": \\\"1747543084\\\", \\\"duration\\\": \\\"31536000\\\", \\\"current_epoch\\\": \\\"0\\\", \\\"current_epoch_start_block\\\": \\\"0\\\", \\\"fast_forward_next_tick\\\": false}, {\\\"name\\\": \\\"funding-tick\\\", \\\"next_tick\\\": \\\"1747543084\\\", \\\"duration\\\": \\\"31536000\\\", \\\"current_epoch\\\": \\\"0\\\", \\\"current_epoch_start_block\\\": \\\"0\\\", \\\"fast_forward_next_tick\\\": false}, {\\\"name\\\": \\\"stats-epoch\\\", \\\"next_tick\\\": \\\"1747543084\\\", \\\"duration\\\": \\\"31536000\\\", \\\"current_epoch\\\": \\\"0\\\", \\\"current_epoch_start_block\\\": \\\"0\\\", \\\"fast_forward_next_tick\\\": false}]\" \"\""
-	setupCmd := exec.Command("bash", "-c", "cd ../../../../ethos/ethos-chain && ./e2e-setup -setup "+genesisChanges)
-	fmt.Println("Running setup command", setupCmd.String())
-	var out bytes.Buffer
-	var stderr bytes.Buffer
-	setupCmd.Stdout = &out
-	setupCmd.Stderr = &stderr
-	err := setupCmd.Run()
-	if err != nil {
-		s.T().Fatalf("Failed to set up environment: %v, stdout: %s, stderr: %s", err, out.String(), stderr.String())
-	}
+	network.DeployCustomNetwork(genesisChanges)
 
-	s.Require().NoError(err)
-
-}
-
-func (s *SendingIntegrationTestSuite) CleanUpDocker() {
-	stopCmd := exec.Command("bash", "-c", "docker stop interchain-security-instance")
-	if err := stopCmd.Run(); err != nil {
-		s.T().Fatalf("Failed to stop Docker container: %v", err)
-	}
-	fmt.Println("Stopped Docker container")
-	// Remove the Docker container
-	removeCmd := exec.Command("bash", "-c", "docker rm interchain-security-instance")
-	if err := removeCmd.Run(); err != nil {
-		s.T().Fatalf("Failed to remove Docker container: %v", err)
-	}
-	fmt.Println("Removed Docker container")
 }
 
 // TestCLISending_Success sends a transfer from one subaccount to another (with the same owner and different numbers).
@@ -145,7 +71,7 @@ func (s *SendingIntegrationTestSuite) TestCLISending_Success() {
 		new(big.Int).SetUint64(499_000_000),
 		new(big.Int).SetUint64(501_000_000),
 	)
-	s.CleanUpDocker()
+	network.CleanupCustomNetwork()
 }
 
 // TestCLISending_InsufficientBalance attempts to send a transfer from one subaccount to
@@ -160,7 +86,7 @@ func (s *SendingIntegrationTestSuite) TestCLISending_InsufficientBalance() {
 		new(big.Int).SetUint64(500_000_000),
 		new(big.Int).SetUint64(500_000_000),
 	)
-	s.CleanUpDocker()
+	network.CleanupCustomNetwork()
 }
 
 // TestCLISending_Nonexistent sends a transfer from one subaccount to
@@ -175,7 +101,7 @@ func (s *SendingIntegrationTestSuite) TestCLISending_Nonexistent() {
 		new(big.Int).SetUint64(499_000_000),
 		new(big.Int).SetUint64(1_000_000),
 	)
-	s.CleanUpDocker()
+	network.CleanupCustomNetwork()
 }
 
 func (s *SendingIntegrationTestSuite) sendTransferAndVerifyBalance(
@@ -185,37 +111,13 @@ func (s *SendingIntegrationTestSuite) sendTransferAndVerifyBalance(
 	expectedSenderQuoteBalance *big.Int,
 	expectedRecipientQuoteBalance *big.Int,
 ) {
-	// val := s.network.Validators[0]
-	// ctx := val.ClientCtx
+
 	cfg := network.DefaultConfig(nil)
-
-	createTransferCmd := exec.Command("bash", "-c", fmt.Sprintf("docker exec interchain-security-instance interchain-security-cd tx sending create-transfer dydx1eeeggku6dzk3mv7wph3zq035rhtd890smfq5z6 %d dydx1eeeggku6dzk3mv7wph3zq035rhtd890smfq5z6 %d %d --from dydx1eeeggku6dzk3mv7wph3zq035rhtd890smfq5z6 --chain-id consu --home /consu/validatoralice --node tcp://7.7.8.4:26658 --keyring-backend test -y -o json", senderSubaccountNumber, recipientSubaccountNumber, amount))
-	var transferOut bytes.Buffer
-	var stdTransferErr bytes.Buffer
-	createTransferCmd.Stdout = &transferOut
-	createTransferCmd.Stderr = &stdTransferErr
-	err := createTransferCmd.Run()
+	transferTx := fmt.Sprintf("docker exec interchain-security-instance interchain-security-cd tx sending create-transfer dydx1eeeggku6dzk3mv7wph3zq035rhtd890smfq5z6 %d dydx1eeeggku6dzk3mv7wph3zq035rhtd890smfq5z6 %d %d --from dydx1eeeggku6dzk3mv7wph3zq035rhtd890smfq5z6 --chain-id consu --home /consu/validatoralice --node tcp://7.7.8.4:26658 --keyring-backend test -y -o json", senderSubaccountNumber, recipientSubaccountNumber, amount)
+	_, _, err := network.QueryCustomNetwork(transferTx)
 	if err != nil {
-		s.T().Fatalf("Failed to send transfer: %v, stdout: %s, stderr: %s", err, transferOut.String(), stdTransferErr.String())
+		s.T().Fatalf("failed to send transfer: %v", err)
 	}
-	// // Send the transfer from sender to recipient.
-	// _, err := testutil.MsgCreateTransferExec(
-	// 	ctx,
-	// 	s.validatorAddress,
-	// 	senderSubaccountNumber,
-	// 	s.validatorAddress,
-	// 	recipientSubaccountNumber,
-	// 	amount,
-	// )
-	// s.Require().NoError(err)
-
-	// currentHeight, err := s.network.LatestHeight()
-	// s.Require().NoError(err)
-
-	// Wait for a few blocks to ensure the transfer was complated.
-	// _, err = s.network.WaitForHeight(currentHeight + 3)
-	// s.Require().NoError(err)
-
 	time.Sleep(5 * time.Second)
 
 	// Query both subaccounts.
