b97cf377d63f132db0ffc1457c815ec637beca15
Alejandro Gala <alegala21@gmail.com>
2024-05-17 14:24:25 -0700

    reward tests

--- protocol/x/rewards/client/cli/query_params_test.go
+++ protocol/x/rewards/client/cli/query_params_test.go
@@ -12,36 +12,11 @@ import (
 	"github.com/StreamFinance-Protocol/stream-chain/protocol/testutil/network"
 	"github.com/StreamFinance-Protocol/stream-chain/protocol/x/rewards/types"
 	tmcli "github.com/cometbft/cometbft/libs/cli"
-	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/stretchr/testify/require"
 )
 
 // Prevent strconv unused error
 var _ = strconv.IntSize
-
-func setupNetwork(
-	t *testing.T,
-) (
-	*network.Network,
-	client.Context,
-) {
-	t.Helper()
-	cfg := network.DefaultConfig(nil)
-
-	// Init state.
-	state := types.GenesisState{}
-	require.NoError(t, cfg.Codec.UnmarshalJSON(cfg.GenesisState[types.ModuleName], &state))
-
-	state = *types.DefaultGenesis()
-
-	buf, err := cfg.Codec.MarshalJSON(&state)
-	require.NoError(t, err)
-	cfg.GenesisState[types.ModuleName] = buf
-	net := network.New(t, cfg)
-	ctx := net.Validators[0].ClientCtx
-
-	return net, ctx
-}
 
 func TestQueryParams(t *testing.T) {
 
@@ -61,18 +36,3 @@ func TestQueryParams(t *testing.T) {
 	require.NoError(t, cfg.Codec.UnmarshalJSON(data, &resp))
 	require.Equal(t, types.DefaultGenesis().Params, resp.Params)
 }
-
-// func TestQueryParams(t *testing.T) {
-// 	net, ctx := setupNetwork(t)
-
-// 	out, err := clitestutil.ExecTestCLICmd(
-// 		ctx,
-// 		cli.CmdQueryParams(),
-// 		[]string{fmt.Sprintf("--%s=json", tmcli.OutputFlag)},
-// 	)
-
-// 	require.NoError(t, err)
-// 	var resp types.QueryParamsResponse
-// 	require.NoError(t, net.Config.Codec.UnmarshalJSON(out.Bytes(), &resp))
-// 	require.Equal(t, types.DefaultGenesis().Params, resp.Params)
-// }
