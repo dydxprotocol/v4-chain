d5794da17121e1e9fcca871fd2868e1449f22f52
Alejandro Gala <alegala21@gmail.com>
2024-05-17 14:23:44 -0700

    stat tests

--- protocol/x/stats/client/cli/query_test.go
+++ protocol/x/stats/client/cli/query_test.go
@@ -8,7 +8,6 @@ import (
 	"strconv"
 	"testing"
 
-	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/stretchr/testify/require"
 
 	"github.com/StreamFinance-Protocol/stream-chain/protocol/testutil/network"
@@ -17,30 +16,6 @@ import (
 
 // Prevent strconv unused error
 var _ = strconv.IntSize
-
-func setupNetwork(
-	t *testing.T,
-) (
-	*network.Network,
-	client.Context,
-) {
-	t.Helper()
-	cfg := network.DefaultConfig(nil)
-
-	// Init state.
-	state := types.GenesisState{}
-	require.NoError(t, cfg.Codec.UnmarshalJSON(cfg.GenesisState[types.ModuleName], &state))
-
-	state = *types.DefaultGenesis()
-
-	buf, err := cfg.Codec.MarshalJSON(&state)
-	require.NoError(t, err)
-	cfg.GenesisState[types.ModuleName] = buf
-	net := network.New(t, cfg)
-	ctx := net.Validators[0].ClientCtx
-
-	return net, ctx
-}
 
 func TestQueryParams(t *testing.T) {
 
@@ -58,17 +33,6 @@ func TestQueryParams(t *testing.T) {
 	require.Equal(t, types.DefaultGenesis().Params, resp.Params)
 }
 
-// func TestQueryParams(t *testing.T) {
-// 	net, ctx := setupNetwork(t)
-
-// 	out, err := clitestutil.ExecTestCLICmd(ctx, cli.CmdQueryParams(), []string{})
-
-// 	require.NoError(t, err)
-// 	var resp types.QueryParamsResponse
-// 	require.NoError(t, net.Config.Codec.UnmarshalJSON(out.Bytes(), &resp))
-// 	require.Equal(t, types.DefaultGenesis().Params, resp.Params)
-// }
-
 func TestQueryStatsMetadata(t *testing.T) {
 
 	cfg := network.DefaultConfig(nil)
@@ -83,16 +47,6 @@ func TestQueryStatsMetadata(t *testing.T) {
 	data := out.Bytes()
 	require.NoError(t, cfg.Codec.UnmarshalJSON(data, &resp))
 }
-
-// func TestQueryStatsMetadata(t *testing.T) {
-// 	net, ctx := setupNetwork(t)
-
-// 	out, err := clitestutil.ExecTestCLICmd(ctx, cli.CmdQueryStatsMetadata(), []string{})
-
-// 	require.NoError(t, err)
-// 	var resp types.QueryStatsMetadataResponse
-// 	require.NoError(t, net.Config.Codec.UnmarshalJSON(out.Bytes(), &resp))
-// }
 
 func TestQueryGlobalStats(t *testing.T) {
 
@@ -109,16 +63,6 @@ func TestQueryGlobalStats(t *testing.T) {
 	require.NoError(t, cfg.Codec.UnmarshalJSON(data, &resp))
 }
 
-// func TestQueryGlobalStats(t *testing.T) {
-// 	net, ctx := setupNetwork(t)
-
-// 	out, err := clitestutil.ExecTestCLICmd(ctx, cli.CmdQueryGlobalStats(), []string{})
-
-// 	require.NoError(t, err)
-// 	var resp types.QueryGlobalStatsResponse
-// 	require.NoError(t, net.Config.Codec.UnmarshalJSON(out.Bytes(), &resp))
-// }
-
 func TestQueryUserStats(t *testing.T) {
 
 	cfg := network.DefaultConfig(nil)
@@ -133,13 +77,3 @@ func TestQueryUserStats(t *testing.T) {
 	data := out.Bytes()
 	require.NoError(t, cfg.Codec.UnmarshalJSON(data, &resp))
 }
-
-// func TestQueryUserStats(t *testing.T) {
-// 	net, ctx := setupNetwork(t)
-
-// 	out, err := clitestutil.ExecTestCLICmd(ctx, cli.CmdQueryUserStats(), []string{"alice"})
-
-// 	require.NoError(t, err)
-// 	var resp types.QueryUserStatsResponse
-// 	require.NoError(t, net.Config.Codec.UnmarshalJSON(out.Bytes(), &resp))
-// }
