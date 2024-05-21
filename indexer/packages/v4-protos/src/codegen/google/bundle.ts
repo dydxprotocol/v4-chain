b0efc55330cbd03dacf63abf09d9817a6f058fff
Alejandro Gala <alegala21@gmail.com>
2024-05-21 13:56:56 -0700

    proto build

--- indexer/packages/v4-protos/src/codegen/google/bundle.ts
+++ indexer/packages/v4-protos/src/codegen/google/bundle.ts
@@ -1,16 +1,16 @@
-import * as _100 from "./api/annotations";
-import * as _101 from "./api/http";
-import * as _102 from "./protobuf/descriptor";
-import * as _103 from "./protobuf/duration";
-import * as _104 from "./protobuf/timestamp";
-import * as _105 from "./protobuf/any";
+import * as _93 from "./api/annotations";
+import * as _94 from "./api/http";
+import * as _95 from "./protobuf/descriptor";
+import * as _96 from "./protobuf/duration";
+import * as _97 from "./protobuf/timestamp";
+import * as _98 from "./protobuf/any";
 export namespace google {
-  export const api = { ..._100,
-    ..._101
+  export const api = { ..._93,
+    ..._94
   };
-  export const protobuf = { ..._102,
-    ..._103,
-    ..._104,
-    ..._105
+  export const protobuf = { ..._95,
+    ..._96,
+    ..._97,
+    ..._98
   };
 }
