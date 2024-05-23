2edc797ae9bc84be4225b704f73653ae7aa5a0a6
EC2 Default User <ec2-user@ip-172-31-31-127.ec2.internal>
2024-05-23 00:54:50 +0000

    env changes

--- e2e-testing/run-containerized-env.sh
+++ e2e-testing/run-containerized-env.sh
@@ -1,5 +1,5 @@
 cd ../protocol
-make e2etest-build-image
+make build-e2e-ethos-test
 cd ../e2e-testing
 docker-compose -f docker-compose-e2e-test.yml build --no-cache
 docker-compose -f docker-compose-e2e-test.yml up
