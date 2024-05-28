6f9916c93434c092f39c40ac2a40bd3559e2eb1b
Brendan Chou <3680392+BrendanChou@users.noreply.github.com>
2024-05-28 11:13:13 -0400

    fix build/lint

--- protocol/x/clob/types/pending_updates.go
+++ protocol/x/clob/types/pending_updates.go
@@ -169,7 +169,7 @@ func (p *PendingUpdates) AddPerpetualFill(
 		totalFee = big.NewInt(0)
 	}
 
-	bigFeeQuoteQuantums := lib.BigMulPpm(bigFillQuoteQuantums, feePpm, true)
+	bigFeeQuoteQuantums := lib.BigMulPpm(bigFillQuoteQuantums, lib.BigI(feePpm), true)
 
 	totalFee.Add(
 		totalFee,
