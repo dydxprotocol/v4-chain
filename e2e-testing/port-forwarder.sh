ef91461e32d351b08ca3db0f63f6ee1f2b42b1a2
EC2 Default User <ec2-user@ip-172-31-31-127.ec2.internal>
2024-06-03 21:07:35 +0000

    combine commands

--- e2e-testing/port-forwarder.sh
+++ e2e-testing/port-forwarder.sh
@@ -1,2 +1,2 @@
 
-docker exec -it interchain-security-instance bash -c "yum install -y socat && socat TCP-LISTEN:26658,fork TCP:7.7.8.253:26658"
+docker exec -it interchain-security-instance bash -c "yum install -y socat && socat TCP-LISTEN:26658,fork TCP:7.7.8.253:26658" -d
