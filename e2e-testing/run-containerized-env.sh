d25f81fa1546bc98c5b31f41f1d385f244f36f60
Peter Benitez <67120424+PeterB103@users.noreply.github.com>
2024-06-03 17:22:01 -0700

    Update run-containerized-env.sh
    fixing merge conflict

--- e2e-testing/run-containerized-env.sh
+++ e2e-testing/run-containerized-env.sh
@@ -1,9 +1,7 @@
 cd ../protocol
 make e2etest-build-image
 cd ../e2e-testing
-<<<<<<< clob-updates
+
 #when building w/o  a cache you must split the command into two
-=======
->>>>>>> v4-stable
 docker-compose -f docker-compose-e2e-test.yml build --no-cache
 docker-compose -f docker-compose-e2e-test.yml up
