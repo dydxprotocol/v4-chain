9a9e75997430395411df7ebe38cd7c055efb5228
affan <affan@dydx.exchange>
2024-06-11 13:21:45 -0400

    cleanup

--- protocol/contracts/dydx-messages-example/src/msg.rs
+++ protocol/contracts/dydx-messages-example/src/msg.rs
@@ -33,19 +33,6 @@ pub enum ExecuteMsg {
 }
 
 #[cw_serde]
-#[derive(QueryResponses)]
-pub enum QueryMsg {
-    /// Returns a human-readable representation of the arbiter.
-    #[returns(ArbiterResponse)]
-    Arbiter {},
-    /// Returns the current market price for the given market id.
-    /// This is a custom query that is not part of the cosmwasm standard queries.
-    /// It is used to demonstrate how to query the dydx module.
-    #[returns(MarketPriceResponse)]
-    MarketPrice { id: u32 },
-}
-
-#[cw_serde]
 pub struct ArbiterResponse {
     pub arbiter: Addr,
 }
