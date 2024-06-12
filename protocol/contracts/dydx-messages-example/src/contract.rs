7632d007fbe6fe5e8839c3d528f5d011a79daf81
affan <affan@dydx.exchange>
2024-06-12 12:02:30 -0400

    Cleanup and consolidate msg enums

--- protocol/contracts/dydx-messages-example/src/contract.rs
+++ protocol/contracts/dydx-messages-example/src/contract.rs
@@ -9,7 +9,7 @@ use crate::error::ContractError;
 use crate::msg::{ArbiterResponse, ExecuteMsg, InstantiateMsg};
 use crate::state::{Config, CONFIG};
 use cw2::set_contract_version;
-use crate::dydx_msg::{SendingMsg};
+use dydx_cosmwasm::DydxMsg;
 use dydx_cosmwasm::{DydxQuerier, DydxQueryWrapper, SubaccountId};
 use dydx_cosmwasm::DydxQuery;
 
@@ -34,108 +34,45 @@ pub fn execute(
     env: Env,
     info: MessageInfo,
     msg: ExecuteMsg,
-) -> Result<Response<SendingMsg>, ContractError> {
+) -> Result<Response<DydxMsg>, ContractError> {
     match msg {
-        ExecuteMsg::DepositToSubaccount { sender, recipient, asset_id, quantums } => {
-            let deposit = SendingMsg::DepositToSubaccount {
-                sender: sender,
-                recipient: recipient,
-                asset_id: asset_id,
-                quantums: quantums,
-            };
-            Ok(Response::new().add_message(deposit))
-        }
-        ExecuteMsg::WithdrawFromSubaccount { sender, recipient, asset_id, quantums } => {
-            let withdraw = SendingMsg::WithdrawFromSubaccount {
-                sender: sender,
-                recipient: recipient,
-                asset_id: asset_id,
-                quantums: quantums,
-            };
-            Ok(Response::new().add_message(withdraw))
-        }
-        ExecuteMsg::PlaceOrder { order } => {
-            let place_order = SendingMsg::PlaceOrder {
-                order: order,
-            };
-            Ok(Response::new().add_message(place_order))
-        }
-        ExecuteMsg::CancelOrder { order_id, good_til_block, good_til_block_time } => {
-            let cancel_order = SendingMsg::CancelOrder {
-                order_id: order_id,
-                good_til_block: good_til_block,
-                good_til_block_time: good_til_block_time,
-            };
-            Ok(Response::new().add_message(cancel_order))
-        }
-    }
-}
-
-fn execute_approve(
-    deps: DepsMut,
-    env: Env,
-    info: MessageInfo,
-    quantity: Option<u64>,
-    //quantity: Option<Vec<Coin>>,
-) -> Result<Response<SendingMsg>, ContractError> {
-    let config = CONFIG.load(deps.storage)?;
-    if info.sender != config.arbiter {
-        return Err(ContractError::Unauthorized {});
-    }
-
-    // throws error if the contract is expired
-    if let Some(expiration) = config.expiration {
-        if expiration.is_expired(&env.block) {
-            return Err(ContractError::Expired { expiration });
+        ExecuteMsg::DydxMsg(dydx_msg) => {
+            match dydx_msg{
+                DydxMsg::DepositToSubaccount { sender, recipient, asset_id, quantums } => {
+                    let deposit = DydxMsg::DepositToSubaccount {
+                        sender: sender,
+                        recipient: recipient,
+                        asset_id: asset_id,
+                        quantums: quantums,
+                    };
+                    Ok(Response::new().add_message(deposit))
+                }
+                DydxMsg::WithdrawFromSubaccount { sender, recipient, asset_id, quantums } => {
+                    let withdraw = DydxMsg::WithdrawFromSubaccount {
+                        sender: sender,
+                        recipient: recipient,
+                        asset_id: asset_id,
+                        quantums: quantums,
+                    };
+                    Ok(Response::new().add_message(withdraw))
+                }
+                DydxMsg::PlaceOrder { order } => {
+                    let place_order = DydxMsg::PlaceOrder {
+                        order: order,
+                    };
+                    Ok(Response::new().add_message(place_order))
+                }
+                DydxMsg::CancelOrder { order_id, good_til_oneof } => {
+                    let cancel_order = DydxMsg::CancelOrder {
+                        order_id: order_id,
+                        good_til_oneof: good_til_oneof,
+                    };
+                    Ok(Response::new().add_message(cancel_order))
+                }
+                _ => Err(ContractError::InvalidDydxMsg{}),
+            }
         }
     }
-
-    let balance = deps.querier.query_balance(&env.contract.address, "ibc/8E27BA2D5493AF5636760E354E46004562C46AB7EC0CC4C1CA14E9E20E2545B5".to_string())?.amount.u128() as u64;
-    //let balance = deps.querier.query_all_balances(&env.contract.address)?;
-
-    let amount = if let Some(quantity) = quantity {
-        quantity
-    } else {
-        // release everything
-        // Querier guarantees to return up-to-date data, including funds sent in this handle message
-        // https://github.com/CosmWasm/wasmd/blob/master/x/wasm/internal/keeper/keeper.go#L185-L192
-        balance
-    };
-    Ok(send_tokens(env.contract.address, config.recipient, amount, "approve"))
-}
-
-fn execute_refund(deps: DepsMut, env: Env, _info: MessageInfo) -> Result<Response<SendingMsg>, ContractError> {
-    let config = CONFIG.load(deps.storage)?;
-    // anyone can try to refund, as long as the contract is expired
-    if let Some(expiration) = config.expiration {
-        if !expiration.is_expired(&env.block) {
-            return Err(ContractError::NotExpired {});
-        }
-    } else {
-        return Err(ContractError::NotExpired {});
-    }
-
-    // Querier guarantees to return up-to-date data, including funds sent in this handle message
-    // https://github.com/CosmWasm/wasmd/blob/master/x/wasm/internal/keeper/keeper.go#L185-L192
-    let balance = deps.querier.query_balance(&env.contract.address, "ibc/8E27BA2D5493AF5636760E354E46004562C46AB7EC0CC4C1CA14E9E20E2545B5".to_string())?.amount.u128() as u64;
-    Ok(send_tokens(env.contract.address, config.source, balance, "refund"))
-}
-
-// this is a helper to move the tokens, so the business logic is easy to read
-fn send_tokens(from_address: Addr, to_address: Addr, amount: u64, action: &str) -> Response<SendingMsg> {
-    let deposit = SendingMsg::DepositToSubaccount {
-        sender: from_address.clone().into(),
-        recipient: SubaccountId {
-            owner: to_address.clone().into(),
-            number: 0,
-        },
-        asset_id: 0,
-        quantums: amount,
-    };
-    Response::new()
-        .add_message(deposit)
-        .add_attribute("action", action)
-        .add_attribute("to", to_address)
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
