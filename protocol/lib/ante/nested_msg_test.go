556b53ffa2d1bf5cc8fca05af9dcacda6f239f40
EC2 Default User <ec2-user@ip-172-31-31-127.ec2.internal>
2024-06-17 23:54:42 +0000

    fix linting issues

--- protocol/lib/ante/nested_msg_test.go
+++ protocol/lib/ante/nested_msg_test.go
@@ -16,9 +16,7 @@ import (
 )
 
 var (
-	invalidInnerMsgErr_Unsupported = fmt.Errorf("Invalid nested msg: unsupported msg type")
 	invalidInnerMsgErr_AppInjected = fmt.Errorf("Invalid nested msg: app-injected msg type")
-	invalidInnerMsgErr_Nested      = fmt.Errorf("Invalid nested msg: double-nested msg type")
 	invalidInnerMsgErr_Dydx        = fmt.Errorf("Invalid nested msg for MsgExec: dydx msg type")
 )
 
@@ -126,101 +124,3 @@ func TestValidateNestedMsg(t *testing.T) {
 		})
 	}
 }
-
-// func TestValidateNestedMsg_IterateEachMsgSample(t *testing.T) {
-// 	type addMsgType string
-// 	const (
-// 		unsupportedMsg addMsgType = "AddUnsupportedMsg"
-// 		appInjectedMsg addMsgType = "AddAppInjectedMsg"
-// 		nestedMsg      addMsgType = "AddNestedMsg"
-// 	)
-
-// 	tests := map[string]struct {
-// 		innerMsgs  []sdk.Msg
-// 		addMsgType addMsgType
-
-// 		expectedErr error
-// 	}{
-// 		"Invalid: SingleMsg / AddUnsupportedMsg=true": {
-// 			innerMsgs:   []sdk.Msg{},
-// 			addMsgType:  unsupportedMsg,
-// 			expectedErr: invalidInnerMsgErr_Unsupported,
-// 		},
-// 		"Invalid: MultiMsgs / AddUnsupportedMsg=true": {
-// 			innerMsgs:   []sdk.Msg{&bank.MsgSend{}, &bank.MsgMultiSend{}},
-// 			addMsgType:  unsupportedMsg,
-// 			expectedErr: invalidInnerMsgErr_Unsupported,
-// 		},
-// 		"Invalid: SingleMsg / AddAppInjectedMsg=true": {
-// 			innerMsgs:   []sdk.Msg{},
-// 			addMsgType:  appInjectedMsg,
-// 			expectedErr: invalidInnerMsgErr_AppInjected,
-// 		},
-// 		"Invalid: MultiMsgs / AddAppInjectedMsg=true": {
-// 			innerMsgs:   []sdk.Msg{&bank.MsgSend{}, &bank.MsgMultiSend{}},
-// 			addMsgType:  appInjectedMsg,
-// 			expectedErr: invalidInnerMsgErr_AppInjected,
-// 		},
-// 		"Invalid: SingleMsg / AddNestedMsg=true": {
-// 			innerMsgs:   []sdk.Msg{},
-// 			addMsgType:  nestedMsg,
-// 			expectedErr: invalidInnerMsgErr_Nested,
-// 		},
-// 		"Invalid: MultiMsgs / AddNestedMsg=true": {
-// 			innerMsgs:   []sdk.Msg{&bank.MsgSend{}, &bank.MsgMultiSend{}},
-// 			addMsgType:  nestedMsg,
-// 			expectedErr: invalidInnerMsgErr_Nested,
-// 		},
-// 	}
-
-// 	type testCase struct {
-// 		name        string
-// 		msgs        []sdk.Msg
-// 		expectedErr error
-// 	}
-// 	allTestCases := make([]testCase, 0, len(tests))
-
-// 	unsupportedCnt := 0
-// 	appInjectedCnt := 0
-// 	nestedCnt := 0
-// 	for tcName, tc := range tests {
-// 		var msgSampleTestCase map[string]sdk.Msg
-
-// 		switch tc.addMsgType {
-// 		case unsupportedMsg:
-// 			unsupportedCnt++
-// 			msgSampleTestCase = appmsgs.UnsupportedMsgSamples
-// 		case appInjectedMsg:
-// 			appInjectedCnt++
-// 			msgSampleTestCase = appmsgs.AppInjectedMsgSamples
-// 		case nestedMsg:
-// 			nestedCnt++
-// 			msgSampleTestCase = appmsgs.NestedMsgSamples
-// 		default:
-// 			panic(fmt.Errorf("unexpected addMsgType: %s", tc.addMsgType))
-// 		}
-
-// 		allSampleMsgs := testmsgs.GetNonNilSampleMsgs(msgSampleTestCase)
-// 		for _, sampleMsg := range allSampleMsgs {
-// 			testName := fmt.Sprintf("%s / %s", tcName, sampleMsg.Name)
-// 			testMsgs := append(tc.innerMsgs, sampleMsg.Msg)
-// 			require.True(t, len(testMsgs) > 0 && len(testMsgs) <= 3)
-// 			allTestCases = append(allTestCases, testCase{testName, testMsgs, tc.expectedErr})
-// 		}
-// 	}
-
-// 	expectedTotalCnt := 0
-// 	expectedTotalCnt += unsupportedCnt * len(testmsgs.GetNonNilSampleMsgs(appmsgs.UnsupportedMsgSamples))
-// 	expectedTotalCnt += appInjectedCnt * len(testmsgs.GetNonNilSampleMsgs(appmsgs.AppInjectedMsgSamples))
-// 	expectedTotalCnt += nestedCnt * len(testmsgs.GetNonNilSampleMsgs(appmsgs.NestedMsgSamples))
-// 	require.Len(t, allTestCases, expectedTotalCnt)
-
-// 	for _, tc := range allTestCases {
-// 		t.Run(tc.name, func(t *testing.T) {
-// 			nestedMsg, err := gov.NewMsgSubmitProposal(tc.msgs, nil, "", "", "", "", false)
-// 			require.NoError(t, err)
-// 			result := ante.ValidateNestedMsg(nestedMsg)
-// 			require.Equal(t, tc.expectedErr, result)
-// 		})
-// 	}
-// }
