2fc4e0e5d1b932f46b9b4941ee61ae27ff541a63
Shrenuj Bansal <shrenuj@dydx.exchange>
2024-06-20 17:39:20 -0400

    Add query for market mapper revenue share params
    Signed-off-by: Shrenuj Bansal <shrenuj@dydx.exchange>

--- indexer/packages/v4-protos/src/codegen/dydxprotocol/revshare/query.rpc.Query.ts
+++ indexer/packages/v4-protos/src/codegen/dydxprotocol/revshare/query.rpc.Query.ts
@@ -1,18 +1,35 @@
 import { Rpc } from "../../helpers";
+import * as _m0 from "protobufjs/minimal";
 import { QueryClient, createProtobufRpcClient } from "@cosmjs/stargate";
+import { QueryMarketMapperRevenueShareParams, QueryMarketMapperRevenueShareParamsResponse } from "./query";
 /** Query defines the gRPC querier service. */
 
-export interface Query {}
+export interface Query {
+  /** MarketMapperRevenueShareParams queries the revenue share params for the market mapper */
+  marketMapperRevenueShareParams(request?: QueryMarketMapperRevenueShareParams): Promise<QueryMarketMapperRevenueShareParamsResponse>;
+}
 export class QueryClientImpl implements Query {
   private readonly rpc: Rpc;
 
   constructor(rpc: Rpc) {
     this.rpc = rpc;
+    this.marketMapperRevenueShareParams = this.marketMapperRevenueShareParams.bind(this);
+  }
+
+  marketMapperRevenueShareParams(request: QueryMarketMapperRevenueShareParams = {}): Promise<QueryMarketMapperRevenueShareParamsResponse> {
+    const data = QueryMarketMapperRevenueShareParams.encode(request).finish();
+    const promise = this.rpc.request("dydxprotocol.revshare.Query", "MarketMapperRevenueShareParams", data);
+    return promise.then(data => QueryMarketMapperRevenueShareParamsResponse.decode(new _m0.Reader(data)));
   }
 
 }
 export const createRpcQueryExtension = (base: QueryClient) => {
   const rpc = createProtobufRpcClient(base);
   const queryService = new QueryClientImpl(rpc);
-  return {};
+  return {
+    marketMapperRevenueShareParams(request?: QueryMarketMapperRevenueShareParams): Promise<QueryMarketMapperRevenueShareParamsResponse> {
+      return queryService.marketMapperRevenueShareParams(request);
+    }
+
+  };
 };
