896f1bb228e33b7caad3d0f37c52d8dcdf0f3114
Mohammed Affan <affan@dydx.exchange>
2024-06-20 13:14:25 -0400

    [OTE-415] Add test for getting perpetual clob details (#1739)

--- protocol/x/clob/keeper/clob_pair_test.go
+++ protocol/x/clob/keeper/clob_pair_test.go
@@ -1198,3 +1198,28 @@ func TestAcquireNextClobPairID(t *testing.T) {
 	nextClobPairID = ks.ClobKeeper.AcquireNextClobPairID(ks.Ctx)
 	require.Equal(t, nextClobPairIDFromStore+1, nextClobPairID)
 }
+
+func TestGetPerpetualClobPair(t *testing.T) {
+	memClob := memclob.NewMemClobPriceTimePriority(false)
+	mockIndexerEventManager := &mocks.IndexerEventManager{}
+	ks := keepertest.NewClobKeepersTestContext(t, memClob, &mocks.BankKeeper{}, mockIndexerEventManager)
+	items := keepertest.CreateNClobPair(t,
+		ks.ClobKeeper,
+		ks.PerpetualsKeeper,
+		ks.PricesKeeper,
+		ks.Ctx,
+		2,
+		mockIndexerEventManager,
+	)
+
+	for _, item := range items {
+		perpetualClobItem, err := ks.ClobKeeper.GetPerpetualClobDetails(ks.Ctx,
+			item.GetClobPairId(),
+		)
+		require.NoError(t, err)
+		perpetual, err := ks.PerpetualsKeeper.GetPerpetual(ks.Ctx, clobtest.MustPerpetualId(item))
+		require.NoError(t, err)
+		require.Equal(t, perpetualClobItem.ClobPair, item)
+		require.Equal(t, perpetualClobItem.Perpetual, perpetual)
+	}
+}
