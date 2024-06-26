79a31430dce3f72884738c25db39cfd3a4365ea2
dydxwill <119354122+dydxwill@users.noreply.github.com>
2024-06-26 17:01:09 -0400

    [CT-971] Add flag for socks batch processing (#1777)

--- indexer/services/socks/src/lib/message-forwarder.ts
+++ indexer/services/socks/src/lib/message-forwarder.ts
@@ -3,8 +3,12 @@ import {
   logger,
   InfoObject,
 } from '@dydxprotocol-indexer/base';
-import { updateOnMessageFunction } from '@dydxprotocol-indexer/kafka';
-import { KafkaMessage } from 'kafkajs';
+import { updateOnBatchFunction, updateOnMessageFunction } from '@dydxprotocol-indexer/kafka';
+import {
+  Batch,
+  EachBatchPayload,
+  KafkaMessage,
+} from 'kafkajs';
 import _ from 'lodash';
 
 import config from '../config';
@@ -61,16 +65,113 @@ export class MessageForwarder {
       throw new Error('MessageForwarder already started');
     }
 
-    // Kafkajs requires the function passed into `eachMessage` be an async function.
-    // eslint-disable-next-line @typescript-eslint/require-await
-    updateOnMessageFunction(async (topic, message): Promise<void> => {
-      return this.onMessage(topic, message);
-    });
+    if (config.BATCH_PROCESSING_ENABLED) {
+      logger.info({
+        at: 'consumers#connect',
+        message: 'Batch processing enabled',
+      });
+      updateOnBatchFunction(async (payload: EachBatchPayload): Promise<void> => {
+        return this.onBatch(payload);
+      });
+    } else {
+      logger.info({
+        at: 'consumers#connect',
+        message: 'Batch processing disabled. Processing each message individually',
+      });
+      // Kafkajs requires the function passed into `eachMessage` be an async function.
+      // eslint-disable-next-line @typescript-eslint/require-await
+      updateOnMessageFunction(async (topic, message): Promise<void> => {
+        return this.onMessage(topic, message);
+      });
+    }
 
     this.started = true;
     this.batchSending = setInterval(
       () => { this.forwardBatchedMessages(); },
       BATCH_SEND_INTERVAL_MS,
+    );
+  }
+
+  public async onBatch(
+    payload: EachBatchPayload,
+  ): Promise<void> {
+    const batch: Batch = payload.batch;
+    const topic: string = batch.topic;
+    const partition: string = batch.partition.toString();
+    const metricTags: Record<string, string> = { topic, partition };
+    if (batch.isEmpty()) {
+      logger.error({
+        at: 'on-batch#onBatch',
+        message: 'Empty batch',
+        ...metricTags,
+      });
+      return;
+    }
+
+    const startTime: number = Date.now();
+    const firstMessageTimestamp: number = Number(batch.messages[0].timestamp);
+    const batchTimeInQueue: number = startTime - firstMessageTimestamp;
+    const batchInfo = {
+      firstMessageTimestamp: new Date(firstMessageTimestamp).toISOString(),
+      batchTimeInQueue,
+      messagesInBatch: batch.messages.length,
+      firstOffset: batch.firstOffset(),
+      lastOffset: batch.lastOffset(),
+      ...metricTags,
+    };
+
+    logger.info({
+      at: 'on-batch#onBatch',
+      message: 'Received batch',
+      ...batchInfo,
+    });
+    stats.timing(
+      'socks.batch_time_in_queue',
+      batchTimeInQueue,
+      metricTags,
+    );
+
+    let lastCommitTime: number = startTime;
+    for (let i = 0; i < batch.messages.length; i++) {
+      const message: KafkaMessage = batch.messages[i];
+      await this.onMessage(batch.topic, message);
+
+      // Commit every KAFKA_BATCH_PROCESSING_COMMIT_FREQUENCY_MS to reduce number of roundtrips, and
+      // also prevent disconnecting from the broker due to inactivity.
+      const now: number = Date.now();
+      if (now - lastCommitTime > config.KAFKA_BATCH_PROCESSING_COMMIT_FREQUENCY_MS) {
+        logger.info({
+          at: 'on-batch#onBatch',
+          message: 'Committing offsets and sending heart beat',
+          ...batchInfo,
+        });
+        payload.resolveOffset(message.offset);
+        await Promise.all([
+          payload.heartbeat(),
+          // commitOffsetsIfNecessary will respect autoCommitThreshold and will not commit if
+          // fewer messages than the threshold have been processed since the last commit.
+          payload.commitOffsetsIfNecessary(),
+        ]);
+        lastCommitTime = now;
+      }
+    }
+
+    const batchProcessingTime: number = Date.now() - startTime;
+    logger.info({
+      at: 'on-batch#onBatch',
+      message: 'Finished Processing Batch',
+      batchProcessingTime,
+      ...batchInfo,
+    });
+    stats.timing(
+      'socks.batch_processing_time',
+      batchProcessingTime,
+      metricTags,
+    );
+    stats.timing(
+      'socks.batch_size',
+      batch.messages.length,
+      metricTags,
     );
   }
 
@@ -228,6 +329,7 @@ export class MessageForwarder {
       stats.increment(
         `${config.SERVICE_NAME}.forward_message_with_subscribers`,
         1,
+        config.MESSAGE_FORWARDER_STATSD_SAMPLE_RATE,
       );
     }
   }
