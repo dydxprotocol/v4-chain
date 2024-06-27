9fc86b02985c397dd90e4f20e2c22dea6a66c6e1
server <alegala21@gmail.com>
2024-06-27 23:17:16 +0000

    bug fixes

--- protocol/testing/e2e/funding/funding_e2e_test.go
+++ protocol/testing/e2e/funding/funding_e2e_test.go
@@ -483,7 +483,7 @@ func TestFunding(t *testing.T) {
 					tApp.App,
 					testapp.MustMakeCheckTxOptions{
 						AccAddressForSigning: transfer.Transfer.Sender.Owner,
-						Gas:                  110_000,
+						Gas:                  120_000,
 						FeeAmt:               constants.TestFeeCoins_5Cents,
 					},
 					&transfer,
