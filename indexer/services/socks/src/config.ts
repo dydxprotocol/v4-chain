495adf6879e26c8874ce1aa77531b548f0c25c8c
Will Liu <will@dydx.exchange>
2024-06-28 14:53:32 -0400

    set defaults

--- indexer/services/socks/src/config.ts
+++ indexer/services/socks/src/config.ts
@@ -30,8 +30,8 @@ export const configSchema = {
     default: 250,
   }),
 
-  WS_HEARTBEAT_INTERVAL_MS: parseInteger(),
-  WS_HEARTBEAT_TIMEOUT_MS: parseInteger(),
+  WS_HEARTBEAT_INTERVAL_MS: parseInteger({ default: 30_000 }),
+  WS_HEARTBEAT_TIMEOUT_MS: parseInteger({ default: 10_000 }),
 
   RATE_LIMIT_ENABLED: parseBoolean({ default: true }),
   RATE_LIMIT_SUBSCRIBE_POINTS: parseNumber({ default: 2 }),
