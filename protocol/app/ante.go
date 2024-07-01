3b69f08037f653cfeaa39f2f59f6c66cb950bc70
aljo242 <alex@skip.money>
2024-07-01 11:41:24 -0400

    fix ante

--- protocol/app/ante.go
+++ protocol/app/ante.go
@@ -6,6 +6,7 @@ import (
 	errorsmod "cosmossdk.io/errors"
 	"cosmossdk.io/store/cachemulti"
 	storetypes "cosmossdk.io/store/types"
+
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
