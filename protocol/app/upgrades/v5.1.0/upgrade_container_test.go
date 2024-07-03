9bcc74356d71660ba0109d141d434e1de897f9f2
vincentwschau <99756290+vincentwschau@users.noreply.github.com>
2024-07-02 23:31:17 -0400

    Add upgrade handler for v5.1.0 (#1836)
    (cherry picked from commit 7aa0952aaecb243a92648ccc67f2453b69135c2f)
    
    # Conflicts:
    #	protocol/app/upgrades.go
    #	protocol/testing/containertest/containertest.sh
    #	protocol/testing/containertest/preupgrade_genesis.json
    #	protocol/testing/containertest/testnet.go

---
+++ protocol/app/upgrades/v5.1.0/upgrade_container_test.go
@@ -0,0 +1,42 @@
+//go:build all || container_test
+
+package v_5_1_0_test
+
+import (
+	"testing"
+
+	v_5_1_0 "github.com/dydxprotocol/v4-chain/protocol/app/upgrades/v5.1.0"
+	"github.com/dydxprotocol/v4-chain/protocol/testing/containertest"
+	"github.com/dydxprotocol/v4-chain/protocol/testutil/constants"
+	"github.com/stretchr/testify/require"
+)
+
+func TestStateUpgrade(t *testing.T) {
+	testnet, err := containertest.NewTestnetWithPreupgradeGenesis()
+	require.NoError(t, err, "failed to create testnet - is docker daemon running?")
+	err = testnet.Start()
+	require.NoError(t, err)
+	defer testnet.MustCleanUp()
+	node := testnet.Nodes["alice"]
+	nodeAddress := constants.AliceAccAddress.String()
+
+	preUpgradeSetups(node, t)
+	preUpgradeChecks(node, t)
+
+	err = containertest.UpgradeTestnet(nodeAddress, t, node, v_5_1_0.UpgradeName)
+	require.NoError(t, err)
+
+	postUpgradeChecks(node, t)
+}
+
+func preUpgradeSetups(node *containertest.Node, t *testing.T) {
+	// Add setup for upgrade handler logic
+}
+
+func preUpgradeChecks(node *containertest.Node, t *testing.T) {
+	// Add test for your upgrade handler logic below
+}
+
+func postUpgradeChecks(node *containertest.Node, t *testing.T) {
+	// Add test for your upgrade handler logic below
+}
