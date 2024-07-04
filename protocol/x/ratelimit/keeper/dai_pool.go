7f428fa3f4949dd486de6c8bc4e6a92a5080a69f
EC2 Default User <ec2-user@ip-172-31-44-29.eu-north-1.compute.internal>
2024-07-04 19:42:29 +0000

    Remove redeem function from dai pool logic

--- protocol/x/ratelimit/keeper/dai_pool.go
+++ protocol/x/ratelimit/keeper/dai_pool.go
@@ -43,28 +43,6 @@ func (k Keeper) GetTradingDAIFromSDAIAmount(ctx sdk.Context, sDAI *big.Int) (*bi
 	return tDAI, nil
 }
 
-// Converts tDAI to corresponding amount of sDAI, implementing the following maker code
-/*
-	https://etherscan.deth.net/address/0x83f20f44975d03b1b09e64809b757c47f942beea
-	function redeem(uint256 shares, address receiver, address owner) external returns (uint256 assets) {
-		uint256 chi = (block.timestamp > pot.rho()) ? pot.drip() : pot.chi();
-		assets = shares * chi / RAY;
-		_burn(assets, shares, receiver, owner);
-	}
-*/
-func (k Keeper) GetSDAIFromTradingDAIAmount(ctx sdk.Context, tradingDAI *big.Int) (*big.Int, error) {
-	// Get the current sDAI price
-	sDAIPrice, found := k.GetSDAIPrice(ctx)
-	if !found {
-		return nil, errors.New("sDAI price not found")
-	}
-
-	// Calculate shares = tradingDAI * RAY / sDAIPrice
-	sDAI := new(big.Int).Mul(tradingDAI, sDAIPrice)
-	sDAI = sDAI.Div(sDAI, new(big.Int).Exp(big.NewInt(types.BASE_10), big.NewInt(types.SDAI_DECIMALS), nil))
-	return sDAI, nil
-}
-
 // Inspired by the following maker code.
 /*
 	// https://etherscan.deth.net/address/0x83f20f44975d03b1b09e64809b757c47f942beea
@@ -116,37 +94,6 @@ func (k Keeper) MintTradingDAIToUserAccount(
 
 	if err := k.bankKeeper.SendCoinsFromModuleToAccount(ctx, types.PoolAccount, userAddr, tradingDAICoins); err != nil {
 		return errorsmod.Wrap(err, "failed to send trading DAI to recipient account")
-	}
-
-	return nil
-}
-
-func (k Keeper) RedeemSDAIFromTradingDAI(
-	ctx sdk.Context,
-	userAddr sdk.AccAddress,
-	tDAIAmount *big.Int,
-) error {
-
-	sDAIAmount, err := k.GetSDAIFromTradingDAIAmount(ctx, tDAIAmount)
-	if err != nil {
-		return errorsmod.Wrap(err, "failed to convert trading DAI to sDAI")
-	}
-
-	sDAICoins := sdk.NewCoins(sdk.NewCoin(types.SDaiDenom, sdkmath.NewIntFromBigInt(sDAIAmount)))
-	tradingDAICoins := sdk.NewCoins(sdk.NewCoin(types.TradingDAIDenom, sdkmath.NewIntFromBigInt(tDAIAmount)))
-
-	if err := k.bankKeeper.SendCoinsFromAccountToModule(ctx, userAddr, types.PoolAccount, tradingDAICoins); err != nil {
-		return errorsmod.Wrap(err, "failed to send trading DAI to recipient account")
-	}
-
-	if err := k.bankKeeper.BurnCoins(
-		ctx, types.PoolAccount, tradingDAICoins,
-	); err != nil {
-		return errorsmod.Wrap(err, "failed to burn trading DAI transferred to the pool account")
-	}
-
-	if err := k.bankKeeper.SendCoinsFromModuleToAccount(ctx, types.PoolAccount, userAddr, sDAICoins); err != nil {
-		return errorsmod.Wrap(err, "failed to send sDAI to user account")
 	}
 
 	return nil
