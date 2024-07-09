c25143f817b44c01ebb8f74ee550e1b5146d4d3f
ttl33 <19664986+ttl33@users.noreply.github.com>
2024-07-09 12:03:36 -0400

    [TRA-461] Remove `x/vault/MsgWithdrawFromVault` (#1859)

--- protocol/testutil/constants/vault.go
+++ protocol/testutil/constants/vault.go
@@ -5,11 +5,6 @@ import (
 	"github.com/dydxprotocol/v4-chain/protocol/x/vault/types"
 )
 
-type VaultState struct {
-	VaultId     types.VaultId
-	OwnerShares []*types.OwnerShare
-}
-
 var (
 	Vault_Clob0 = types.VaultId{
 		Type:   types.VaultType_VAULT_TYPE_CLOB,
@@ -18,29 +13,6 @@ var (
 	Vault_Clob1 = types.VaultId{
 		Type:   types.VaultType_VAULT_TYPE_CLOB,
 		Number: 1,
-	}
-
-	Vault_Clob0_SingleOwner_Alice0_1000 = VaultState{
-		VaultId: Vault_Clob0,
-		OwnerShares: []*types.OwnerShare{
-			{
-				Owner:  Alice_Num0.Owner,
-				Shares: &types.NumShares{NumShares: dtypes.NewInt(1000)},
-			},
-		},
-	}
-	Vault_Clob0_MultiOwner_Alice0_1000_Bob0_2500 = VaultState{
-		VaultId: Vault_Clob0,
-		OwnerShares: []*types.OwnerShare{
-			{
-				Owner:  Alice_Num0.Owner,
-				Shares: &types.NumShares{NumShares: dtypes.NewInt(1000)},
-			},
-			{
-				Owner:  Bob_Num0.Owner,
-				Shares: &types.NumShares{NumShares: dtypes.NewInt(2500)},
-			},
-		},
 	}
 
 	MsgDepositToVault_Clob0_Alice0_100 = &types.MsgDepositToVault{
