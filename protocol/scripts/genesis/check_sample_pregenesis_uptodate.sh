a530105d478c85a4df3e77981a5ccc1daa43e358
aljo242 <alex@skip.money>
2024-07-15 10:30:30 -0400

    final fixes

--- protocol/scripts/genesis/check_sample_pregenesis_uptodate.sh
+++ protocol/scripts/genesis/check_sample_pregenesis_uptodate.sh
@@ -15,6 +15,7 @@ diff_output=$(diff "/tmp/prod-chain/.dydxprotocol/config/sorted_genesis.json.str
 if [ -z "$diff_output" ]; then
     echo "./scripts/genesis/sample_pregenesis.json is up-to-date"
 else
+    # shellcheck disable=SC2006
     echo "./scripts/genesis/sample_pregenesis.json is not up-to-date. Run `make update-sample-pregenesis` to update."
     echo "$diff_output"
     exit 1
