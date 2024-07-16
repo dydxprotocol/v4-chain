a22ff5bf6f0d691db4b8fd3ceacee70066f9990c
Shrenuj Bansal <shrenuj@dydx.exchange>
2024-07-16 15:00:12 -0400

    Add a wrapper function for clob pair creation

---
+++ protocol/x/listing/types/constants.go
@@ -0,0 +1,9 @@
+package types
+
+const (
+	DefaultStepBaseQuantums uint64 = 1_000_000
+
+	SubticksPerTick_LongTail uint32 = 1_000_000
+
+	DefaultQuantumConversionExponent = -9
+)
--- protocol/x/listing/types/expected_keepers.go
+++ protocol/x/listing/types/expected_keepers.go
@@ -1,1 +1,20 @@
 package types
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/dydxprotocol/v4-chain/protocol/x/clob/types"
+	satypes "github.com/dydxprotocol/v4-chain/protocol/x/subaccounts/types"
+)
+
+type ClobKeeper interface {
+	CreatePerpetualClobPair(
+		ctx sdk.Context,
+		clobPairId uint32,
+		perpetualId uint32,
+		stepSizeBaseQuantums satypes.BaseQuantums,
+		quantumConversionExponent int32,
+		subticksPerTick uint32,
+		status types.ClobPair_Status,
+	) (types.ClobPair, error)
+	AcquireNextClobPairID(ctx sdk.Context) uint32
+}
