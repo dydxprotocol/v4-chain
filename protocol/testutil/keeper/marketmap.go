362557c3730e04085cf845e9ab991a5015ccd380
Chenyao Yu <4844716+chenyaoy@users.noreply.github.com>
2024-07-17 17:29:09 -0400

    add marketmap testutil

---
+++ protocol/testutil/keeper/marketmap.go
@@ -0,0 +1,31 @@
+package keeper
+
+import (
+	dbm "github.com/cosmos/cosmos-db"
+	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/runtime"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/skip-mev/slinky/x/marketmap/types"
+
+	storetypes "cosmossdk.io/store/types"
+	keeper "github.com/skip-mev/slinky/x/marketmap/keeper"
+)
+
+func createMarketMapKeeper(
+	stateStore storetypes.CommitMultiStore,
+	db *dbm.MemDB,
+	cdc *codec.ProtoCodec,
+) (
+	*keeper.Keeper,
+	storetypes.StoreKey,
+) {
+	storeKey := storetypes.NewKVStoreKey(types.StoreKey)
+	stateStore.MountStoreWithDB(storeKey, storetypes.StoreTypeIAVL, db)
+
+	ss := runtime.NewKVStoreService(storeKey)
+	k := keeper.NewKeeper(
+		ss, cdc, sdk.AccAddress("authority"),
+	)
+
+	return k, storeKey
+}
