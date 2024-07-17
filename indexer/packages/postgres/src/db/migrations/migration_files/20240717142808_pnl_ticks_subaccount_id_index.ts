beda939566ae7dd889e7e34a20ec1cce2abf8f0a
affan <affan@dydx.exchange>
2024-07-17 17:24:27 -0400

    spacing

--- indexer/packages/postgres/src/db/migrations/migration_files/20240717142808_pnl_ticks_subaccount_id_index.ts
+++ indexer/packages/postgres/src/db/migrations/migration_files/20240717142808_pnl_ticks_subaccount_id_index.ts
@@ -2,14 +2,14 @@ import * as Knex from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
   await knex.raw(`
-        CREATE INDEX CONCURRENTLY IF NOT EXISTS "pnl_ticks_subaccountid_index" ON "pnl_ticks" ("subaccountId");
-      `);
+    CREATE INDEX CONCURRENTLY IF NOT EXISTS "pnl_ticks_subaccountid_index" ON "pnl_ticks" ("subaccountId");
+    `);
 }
 
 export async function down(knex: Knex): Promise<void> {
   await knex.raw(`
-        DROP INDEX CONCURRENTLY IF EXISTS "pnl_ticks_subaccountid_index";
-      `);
+    DROP INDEX CONCURRENTLY IF EXISTS "pnl_ticks_subaccountid_index";
+    `);
 }
 
 export const config = {
