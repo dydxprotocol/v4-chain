af54233245aa182e3d6056e2a031ac1a596492df
Adam Fraser <adamfraser0@gmail.com>
2024-07-24 12:02:51 -0400

    Update Candles DB migration file
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- indexer/packages/postgres/src/db/migrations/migration_files/20240627152937_candles_add_mid_book_price_open_and_close.ts
+++ indexer/packages/postgres/src/db/migrations/migration_files/20240627152937_candles_add_mid_book_price_open_and_close.ts
@@ -12,7 +12,7 @@ export async function up(knex: Knex): Promise<void> {
 export async function down(knex: Knex): Promise<void> {
   return knex
     .schema
-    .alterTable('wallets', (table) => {
+    .alterTable('candles', (table) => {
       table.dropColumn('orderbookMidPriceOpen');
       table.dropColumn('orderbookMidPriceClose');
     });
