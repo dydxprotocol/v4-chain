77090b36f65fc9ce1b7b4d6951d9e5d225bc8f72
ea-coding <emanueladamiak@gmail.com>
2024-07-30 02:46:19 +0000

    [YBCP-27] Add perp yield index handling for perpetual markets in ender

--- indexer/services/ender/src/scripts/handlers/dydx_perpetual_market_v1_handler.sql
+++ indexer/services/ender/src/scripts/handlers/dydx_perpetual_market_v1_handler.sql
@@ -28,6 +28,7 @@ BEGIN
     perpetual_market_record."liquidityTierId" = (event_data->'liquidityTier')::integer;
     perpetual_market_record."marketType" = 'CROSS';
     perpetual_market_record."baseOpenInterest" = 0;
+    perpetual_market_record."perpYieldIndex" = '0/1';
 
     INSERT INTO perpetual_markets VALUES (perpetual_market_record.*) RETURNING * INTO perpetual_market_record;
 
