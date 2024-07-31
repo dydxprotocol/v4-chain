095613b183bd4e0dc0f76a91fbe6ed5d48742a4f
affan <affan@dydx.exchange>
2024-07-31 16:42:30 -0400

    Blocklist cosmwasm messages not needed for MVP

--- protocol/lib/ante/unsupported_msgs.go
+++ protocol/lib/ante/unsupported_msgs.go
@@ -1,13 +1,13 @@
 package ante
 
 import (
+	wasmtypes "github.com/CosmWasm/wasmd/x/wasm/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	gov "github.com/cosmos/cosmos-sdk/x/gov/types/v1"
 	govbeta "github.com/cosmos/cosmos-sdk/x/gov/types/v1beta1"
 	icacontrollertypes "github.com/cosmos/ibc-go/v8/modules/apps/27-interchain-accounts/controller/types"
 )
 
-// IsUnsupportedMsg returns true if the msg is unsupported by the app.
 func IsUnsupportedMsg(msg sdk.Msg) bool {
 	switch msg.(type) {
 	case
@@ -18,7 +18,19 @@ func IsUnsupportedMsg(msg sdk.Msg) bool {
 		// ------- CosmosSDK default modules
 		// gov
 		*govbeta.MsgSubmitProposal,
-		*gov.MsgCancelProposal:
+		*gov.MsgCancelProposal,
+		// ------- CosmWasm
+		*wasmtypes.MsgClearAdmin,
+		*wasmtypes.MsgIBCCloseChannel,
+		*wasmtypes.MsgIBCSend,
+		*wasmtypes.MsgPinCodes,
+		*wasmtypes.MsgRemoveCodeUploadParamsAddresses,
+		*wasmtypes.MsgSudoContract,
+		*wasmtypes.MsgUnpinCodes,
+		*wasmtypes.MsgUpdateAdmin,
+		*wasmtypes.MsgUpdateContractLabel,
+		*wasmtypes.MsgUpdateInstantiateConfig,
+		*wasmtypes.MsgMigrateContract:
 		return true
 	}
 	return false
