c6e4d92b5a29d3766459973b9b881d4dd59f2c27
Adam Fraser <adamfraser0@gmail.com>
2024-08-02 12:52:14 -0400

    Adam/ct 1059 address issue with orderbookmidprice calculation (#2012)
    (cherry picked from commit 9385c5a6775ebdfd9e39256d3dc3ab991acd117c)

--- indexer/services/ender/src/lib/candles-generator.ts
+++ indexer/services/ender/src/lib/candles-generator.ts
@@ -541,7 +541,7 @@ export async function getOrderbookMidPriceMap(): Promise<{ [ticker: string]: Ord
       perpetualMarket.ticker,
       redisClient,
     );
-    return { [perpetualMarket.ticker]: price === undefined ? undefined : String(price) };
+    return { [perpetualMarket.ticker]: price === undefined ? undefined : price };
   });
 
   const pricesArray = await Promise.all(promises);
