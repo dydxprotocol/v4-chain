6cc92ff8583f58431a6a42e5f7c551c527d91ce5
roy-dydx <133032749+roy-dydx@users.noreply.github.com>
2024-08-05 15:04:54 -0400

    Update mainnet.sh
    Add v5.1.0 binary

--- protocol/testing/mainnet/mainnet.sh
+++ protocol/testing/mainnet/mainnet.sh
@@ -9,7 +9,7 @@ CHAIN_ID="dydx-mainnet-1"
 
 # Define the mapping from version to URL
 declare -A version_to_url
-# version_to_url["v5.1.0"]="https://github.com/dydxprotocol/v4-chain/releases/download/protocol%2Fv5.1.0-dev4/dydxprotocold-v5.1.0-dev4-linux-amd64.tar.gz"
+version_to_url["v5.1.0"]="https://github.com/dydxprotocol/v4-chain/releases/download/protocol%2Fv5.1.1/dydxprotocold-v5.1.1-linux-amd64.tar.gz"
 
 # Define dependencies for this script.
 # `jq` and `dasel` are used to manipulate json and yaml files respectively.
