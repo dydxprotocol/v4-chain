8c3f86bd8900261b768ca510bfde04da5df17e3d
Roy Li <roy@dydx.exchange>
2024-08-07 14:24:32 -0400

    Update mainnet and testnet images for v6.0.0 development

--- protocol/testing/mainnet/vars.sh
+++ protocol/testing/mainnet/vars.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eo pipefail
 
-CURRENT_VERSION_DIR="v5.1.0"
+CURRENT_VERSION_DIR="v6.0.0"
 
 # Full node home directories will be set up for indices 0 to LAST_FULL_NODE_INDEX
 LAST_FULL_NODE_INDEX=5
