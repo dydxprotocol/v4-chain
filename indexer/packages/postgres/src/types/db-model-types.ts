6c7d403f300d923f4a29a5edfd7bf06acc1f4d9f
Adam Fraser <adamfraser0@gmail.com>
2024-08-07 14:04:29 -0400

    Adam/add candles hloc (#2047)
    (cherry picked from commit 73b04dc309fddeefb49503c9f0e28f0fb62ae250)

--- indexer/packages/postgres/src/types/db-model-types.ts
+++ indexer/packages/postgres/src/types/db-model-types.ts
@@ -198,6 +198,8 @@ export interface CandleFromDatabase extends IdBasedModelFromDatabase {
   usdVolume: string;
   trades: number;
   startingOpenInterest: string;
+  orderbookMidPriceOpen?: string | null;
+  orderbookMidPriceClose?: string | null;
 }
 
 export interface PnlTicksFromDatabase extends IdBasedModelFromDatabase {
