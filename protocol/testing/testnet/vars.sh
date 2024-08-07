84eed16514c5062840c4189d55b248a7f809380b
roy-dydx <133032749+roy-dydx@users.noreply.github.com>
2024-08-07 11:36:13 -0400

    Build testnet validator images (#2049)

---
+++ protocol/testing/testnet/vars.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -eo pipefail
+
+CURRENT_VERSION_DIR="v5.2.0"
+
+# Full node home directories will be set up for indices 0 to LAST_FULL_NODE_INDEX
+LAST_FULL_NODE_INDEX=2
+
+# Define monikers for each validator. These are made up strings and can be anything.
+# This also controls in which directory the validator's home will be located. i.e. `/dydxprotocol/chain/.alice`
+MONIKERS=(
+	"dydx-1"
+	"dydx-2"
+)
