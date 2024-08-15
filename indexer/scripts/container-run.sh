6270e79ed2b87f8d2169a6a5944eb037e6fa1e62
Adam Fraser <adamfraser0@gmail.com>
2024-08-15 17:51:32 -0400

    Test not trimming chars in container-run

--- indexer/scripts/container-run.sh
+++ indexer/scripts/container-run.sh
@@ -12,7 +12,14 @@ then
   then
     # Retrieve the secret from secrets manager if it exists
     values=$(aws secretsmanager get-secret-value --secret-id $SECRET_ID | jq -r ".SecretString")
-    env_vars=$(echo $values | tr '\n' ' ' | jq -r "to_entries|map(\"\(.key)=\(.value|tostring)\")|.[]")
+      # Parse the JSON secret into key-value pairs
+    env_vars=$(echo "$values" | jq -r 'to_entries | map(
+      if .key == "FIREBASE_PRIVATE_KEY" then
+        "\(.key)=\(.value|tostring)"
+      else
+        "\(.key)=\(.value|tostring|gsub("\n"; " "))"
+      end
+    ) | .[]')
 
     # Log the environment variables to the console
     echo "Environment Variables:"
