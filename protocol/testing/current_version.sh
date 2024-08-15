d6712a08cb22cd0b9a12ab19e6f965da88dbaf53
roy-dydx <133032749+roy-dydx@users.noreply.github.com>
2024-08-15 14:43:08 -0400

    Simplify container builds (#2089)
    (cherry picked from commit 2aa1cd72e2ef06cb5c1b80481908c706a33d80e4)

---
+++ protocol/testing/current_version.sh
@@ -0,0 +1,5 @@
+CURRENT_VERSION="v6.0.0"
+
+# Define the mapping from version to URL
+declare -A version_to_url
+version_to_url["v5.2.0"]="https://github.com/dydxprotocol/v4-chain/releases/download/protocol%2Fv5.2.0/dydxprotocold-v5.2.0-linux-amd64.tar.gz"
