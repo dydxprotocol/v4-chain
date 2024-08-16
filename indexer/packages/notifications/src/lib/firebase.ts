0a051385c154253363ded32d3f82b6f9d38357bb
Adam Fraser <adamfraser0@gmail.com>
2024-08-15 22:14:32 -0400

    Encode private key as base64

--- indexer/packages/notifications/src/lib/firebase.ts
+++ indexer/packages/notifications/src/lib/firebase.ts
@@ -11,7 +11,7 @@ import config from '../config';
 const initializeFirebaseApp = () => {
   const defaultGoogleApplicationCredentials: { [key: string]: string } = {
     project_id: config.FIREBASE_PROJECT_ID,
-    private_key: config.FIREBASE_PRIVATE_KEY,
+    private_key: Buffer.from(config.FIREBASE_PRIVATE_KEY_BASE64, 'base64').toString('ascii').replace(/\\n/g, '\n'),
     client_email: config.FIREBASE_CLIENT_EMAIL,
   };
 
