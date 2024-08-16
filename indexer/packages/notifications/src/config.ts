865312ebdf9c1f251db77446cd0cf811ade8727a
Adam Fraser <adamfraser0@gmail.com>
2024-08-15 22:14:32 -0400

    Encode private key as base64

--- indexer/packages/notifications/src/config.ts
+++ indexer/packages/notifications/src/config.ts
@@ -12,13 +12,13 @@ export const notificationsConfigSchema = {
   ...baseConfigSchema,
 
   // Private Key for the Google Firebase Messaging project
-  FIREBASE_PRIVATE_KEY: parseString({ default: '' }),
+  FIREBASE_PRIVATE_KEY_BASE64: parseString({ default: 'BASE64_ENCODED_PRIVATE_KEY' }),
 
   // APP ID for the Google Firebase Messaging project
-  FIREBASE_PROJECT_ID: parseString({ default: '' }),
+  FIREBASE_PROJECT_ID: parseString({ default: 'dydx-v4' }),
 
   // Client email for the Google Firebase Messaging project
-  FIREBASE_CLIENT_EMAIL: parseString({ default: '' }),
+  FIREBASE_CLIENT_EMAIL: parseString({ default: 'firebase-adminsdk-f0joo@dydx-v4.iam.gserviceaccount.com' }),
 };
 
 export default parseSchema(notificationsConfigSchema);
