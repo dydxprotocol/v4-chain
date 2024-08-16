35406185052eab36e72d9c8a105a1f440d953aab
roy-dydx <133032749+roy-dydx@users.noreply.github.com>
2024-08-16 13:03:24 -0400

    Unify version name constants in images (#2096)
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

---
+++ protocol/testing/version/version.go
@@ -0,0 +1,6 @@
+package version
+
+import _ "embed"
+
+//go:embed VERSION_CURRENT
+var CurrentVersion string
---
+++ protocol/testing/version/version.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+CURRENT_VERSION=$(<VERSION_CURRENT)
+PREUPGRADE_VERSION=$(<VERSION_PREUPGRADE)
+
+# Define the mapping from version to amd64 URL
+declare -A version_to_url
+version_to_url["$PREUPGRADE_VERSION"]="https://github.com/dydxprotocol/v4-chain/releases/download/protocol%2F$PREUPGRADE_VERSION/dydxprotocold-$PREUPGRADE_VERSION-linux-amd64.tar.gz"
