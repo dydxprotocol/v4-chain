8d7fbcf79c1449e0f86fd658d6149c4d23f43619
Teddy Ding <teddy@dydx.exchange>
2024-08-20 18:15:09 -0400

    Fix upgrade container test (v6 -> vCosmWasm) (#2110)

--- protocol/app/upgrades/v6.1.0/constants.go
+++ protocol/app/upgrades/v6.1.0/constants.go
@@ -1,27 +1,21 @@
-package v_cosmwasm_0
+package v_6_1_0
 
 import (
 	store "cosmossdk.io/store/types"
 	"github.com/dydxprotocol/v4-chain/protocol/app/upgrades"
-	listingtypes "github.com/dydxprotocol/v4-chain/protocol/x/listing/types"
 
 	wasmtypes "github.com/CosmWasm/wasmd/x/wasm/types"
-
-	revsharetypes "github.com/dydxprotocol/v4-chain/protocol/x/revshare/types"
 )
 
 const (
-	UpgradeName = "v6.cosmwasm.0"
+	UpgradeName = "v6.1.0"
 )
 
-// TODO(OTE-535): remove duplicated code from v6 upgrade
 var Upgrade = upgrades.Upgrade{
 	UpgradeName: UpgradeName,
 	StoreUpgrades: store.StoreUpgrades{
 		Added: []string{
-			listingtypes.StoreKey,
 			wasmtypes.StoreKey,
-			revsharetypes.StoreKey,
 		},
 	},
 }
