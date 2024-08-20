76a7fdc4c6e3de931be9cf38c8e8fd15ba9dc73f
affan <affan@dydx.exchange>
2024-08-20 14:02:57 -0400

    add skeleton functions

--- protocol/x/affiliates/keeper/grpc_query.go
+++ protocol/x/affiliates/keeper/grpc_query.go
@@ -1,7 +1,21 @@
 package keeper
 
 import (
+	"context"
+
 	"github.com/dydxprotocol/v4-chain/protocol/x/affiliates/types"
 )
 
 var _ types.QueryServer = Keeper{}
+
+func (k Keeper) AffiliateInfo(c context.Context, req *types.AffiliateInfoRequest) (*types.AffiliateInfoResponse, error) {
+	return nil, nil
+}
+
+func (k Keeper) ReferredBy(ctx context.Context, req *types.ReferredByRequest) (*types.ReferredByResponse, error) {
+	return nil, nil
+}
+
+func (k Keeper) AllAffiliateTiers(c context.Context, req *types.AllAffiliateTiersRequest) (*types.AllAffiliateTiersResponse, error) {
+	return nil, nil
+}
