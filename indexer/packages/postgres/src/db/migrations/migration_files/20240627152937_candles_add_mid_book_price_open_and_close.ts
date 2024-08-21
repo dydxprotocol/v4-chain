77927bd7ffc893bd70eccc42d9622bc749cb4375
Adam Fraser <adamfraser0@gmail.com>
2024-08-21 14:44:11 -0400

    Update 20240627152937_candles_add_mid_book_price_open_and_close.ts

--- indexer/packages/postgres/src/db/migrations/migration_files/20240627152937_candles_add_mid_book_price_open_and_close.ts
+++ indexer/packages/postgres/src/db/migrations/migration_files/20240627152937_candles_add_mid_book_price_open_and_close.ts
@@ -4,13 +4,8 @@ export async function up(knex: Knex): Promise<void> {
   return knex
     .schema
     .alterTable('candles', (table) => {
-<<<<<<< HEAD
-      table.decimal('orderbookMidPriceOpen', null);
-      table.decimal('orderbookMidPriceClose', null);
-=======
       table.decimal('orderbookMidPriceOpen', null).nullable();
       table.decimal('orderbookMidPriceClose', null).nullable();
->>>>>>> 73b04dc3 (Adam/add candles hloc (#2047))
     });
 }
 
