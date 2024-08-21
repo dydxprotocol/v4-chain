416b7ea9e8cd24359a1ce121e1c90c40a46c4523
Shrenuj Bansal <shrenuj@dydx.exchange>
2024-08-21 15:12:20 -0400

    fix build
    Signed-off-by: Shrenuj Bansal <shrenuj@dydx.exchange>

--- protocol/x/listing/keeper/grpc_query_listing_vault_deposit_params.go
+++ protocol/x/listing/keeper/grpc_query_listing_vault_deposit_params.go
@@ -1,15 +1,17 @@
 package keeper
 
 import (
+	"context"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/dydxprotocol/v4-chain/protocol/x/listing/types"
 )
 
 func (k Keeper) ListingVaultDepositParams(
-	ctx sdk.Context,
+	ctx context.Context,
 	req *types.QueryListingVaultDepositParams,
 ) (*types.QueryListingVaultDepositParamsResponse, error) {
-	params := k.GetListingVaultDepositParams(ctx)
+	params := k.GetListingVaultDepositParams(sdk.UnwrapSDKContext(ctx))
 	return &types.QueryListingVaultDepositParamsResponse{
 		Params: params,
 	}, nil
