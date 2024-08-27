b18e1714e9288bf6d48a28e899ff0ad2f6144775
affan <affan@dydx.exchange>
2024-08-27 10:33:15 -0400

    udpate name generation with manual list

--- indexer/services/roundtable/src/tasks/subaccount-username-generator.ts
+++ indexer/services/roundtable/src/tasks/subaccount-username-generator.ts
@@ -3,16 +3,18 @@ import {
   SubaccountUsernamesTable,
   SubaccountsWithoutUsernamesResult,
 } from '@dydxprotocol-indexer/postgres';
-import { generateUsername } from 'unique-username-generator';
+import nouns from '../lib/nouns.json';
+import adjectives from '../lib/adjectives.json';
 
 import config from '../config';
+
 
 export default async function runTask(): Promise<void> {
   const subaccounts:
   SubaccountsWithoutUsernamesResult[] = await
   SubaccountUsernamesTable.getSubaccountsWithoutUsernames();
   for (const subaccount of subaccounts) {
-    const username: string = generateUsername('', config.SUBACCOUNT_USERNAME_NUM_RANDOM_DIGITS);
+    const username: string = generateUsername();
     try {
       // if insert fails, try it in the next roundtable cycle
       // There are roughly 50 Billion possible usernames with 3 random digits
@@ -31,4 +33,18 @@ export default async function runTask(): Promise<void> {
       });
     }
   }
+}
+
+
+function generateUsername(): string {
+  const randomAdjective = adjectives[Math.floor(Math.random() * adjectives.length)];
+  const randomNoun = nouns[Math.floor(Math.random() * nouns.length)];
+  const randomNumber = Math.floor(
+    Math.random() * 1000).toString().padStart(
+      config.SUBACCOUNT_USERNAME_NUM_RANDOM_DIGITS, '0');
+
+  const capitalizedAdjective = randomAdjective.charAt(0).toUpperCase() + randomAdjective.slice(1);
+  const capitalizedNoun = randomNoun.charAt(0).toUpperCase() + randomNoun.slice(1);
+
+  return `${capitalizedAdjective}${capitalizedNoun}${randomNumber}`;
 }
