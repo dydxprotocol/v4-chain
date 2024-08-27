1dc5a84768d0af1dd74c1b59ae5c516319b7733c
affan <affan@dydx.exchange>
2024-08-27 10:46:12 -0400

    use crypto

--- indexer/services/roundtable/src/tasks/subaccount-username-generator.ts
+++ indexer/services/roundtable/src/tasks/subaccount-username-generator.ts
@@ -1,3 +1,5 @@
+import { randomInt } from 'crypto';
+
 import { logger } from '@dydxprotocol-indexer/base';
 import {
   SubaccountUsernamesTable,
@@ -35,13 +37,13 @@ export default async function runTask(): Promise<void> {
 }
 
 function generateUsername(): string {
-  const randomAdjective: string = adjectives[Math.floor(Math.random() * adjectives.length)];
-  const randomNoun: string = nouns[Math.floor(Math.random() * nouns.length)];
-  const randomNumber: string = Math.floor(
-    Math.random() * 1000).toString().padStart(
+  const randomAdjective: string = adjectives[randomInt(0, adjectives.length)];
+  const randomNoun: string = nouns[randomInt(0, nouns.length)];
+  const randomNumber: string = randomInt(0, 1000).toString().padStart(
     config.SUBACCOUNT_USERNAME_NUM_RANDOM_DIGITS, '0');
 
-  const capitalizedAdjective: string = randomAdjective.charAt(0).toUpperCase() + randomAdjective.slice(1);
+  const capitalizedAdjective: string = randomAdjective.charAt(
+    0).toUpperCase() + randomAdjective.slice(1);
   const capitalizedNoun: string = randomNoun.charAt(0).toUpperCase() + randomNoun.slice(1);
 
   return `${capitalizedAdjective}${capitalizedNoun}${randomNumber}`;
