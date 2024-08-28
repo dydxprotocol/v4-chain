44f7fa223daa3c566d7a0efa8992f1d643e7bca7
jerryfan01234 <44346807+jerryfan01234@users.noreply.github.com>
2024-08-28 17:45:00 -0400

    Keplr geoblock new endpoint (#2117)
    (cherry picked from commit a814b332341a7963c935592ad0b58f4894f47c90)
    
    # Conflicts:
    #	indexer/services/comlink/__tests__/controllers/api/v4/compliance-v2-controller.test.ts
    #	indexer/services/comlink/src/controllers/api/v4/compliance-v2-controller.ts

--- indexer/services/comlink/__tests__/controllers/api/v4/compliance-v2-controller.test.ts
+++ indexer/services/comlink/__tests__/controllers/api/v4/compliance-v2-controller.test.ts
@@ -18,6 +18,7 @@ import config from '../../../../src/config';
 import { DateTime } from 'luxon';
 import { ComplianceAction } from '../../../../src/controllers/api/v4/compliance-v2-controller';
 import { ExtendedSecp256k1Signature, Secp256k1 } from '@cosmjs/crypto';
+import { verifyADR36Amino } from '@keplr-wallet/cosmos';
 import { getGeoComplianceReason } from '../../../../src/helpers/compliance/compliance-utils';
 import { isRestrictedCountryHeaders, isWhitelistedAddress } from '@dydxprotocol-indexer/compliance';
 import { toBech32 } from '@cosmjs/encoding';
@@ -39,6 +40,11 @@ jest.mock('@cosmjs/crypto', () => ({
   },
 }));
 
+jest.mock('@keplr-wallet/cosmos', () => ({
+  ...jest.requireActual('@keplr-wallet/cosmos'),
+  verifyADR36Amino: jest.fn(),
+}));
+
 jest.mock('@cosmjs/encoding', () => ({
   toBech32: jest.fn(),
 }));
@@ -46,8 +52,11 @@ jest.mock('@cosmjs/encoding', () => ({
 describe('ComplianceV2Controller', () => {
   const ipAddr: string = '192.168.1.1';
 
-  const ipAddrMock: jest.Mock = (getIpAddr as unknown as jest.Mock);
-  const toBech32Mock: jest.Mock = (toBech32 as unknown as jest.Mock);
+  const verifySignatureMock = Secp256k1.verifySignature as jest.Mock;
+  const fromFixedLengthMock = ExtendedSecp256k1Signature.fromFixedLength as jest.Mock;
+  const verifyADR36AminoMock = verifyADR36Amino as jest.Mock;
+  const ipAddrMock = getIpAddr as jest.Mock;
+  const toBech32Mock = toBech32 as jest.Mock;
 
   beforeAll(async () => {
     await dbHelpers.migrate();
@@ -324,19 +333,32 @@ describe('ComplianceV2Controller', () => {
     });
   });
 
-  describe('POST /geoblock', () => {
+  const geoblockEndpoint = '/v4/compliance/geoblock';
+  const geoblockKeplerEndpoint = '/v4/compliance/geoblock-keplr';
+  const geoblockBody = {
+    address: testConstants.defaultAddress,
+    message: 'Test message',
+    action: ComplianceAction.CONNECT,
+    signedMessage: 'signedmessage123',
+    pubkey: 'asdfasdf',
+    timestamp: 1620000000,
+  };
+  const geoblockKeplrBody = {
+    address: testConstants.defaultAddress,
+    message: 'Test message',
+    action: ComplianceAction.CONNECT,
+    signedMessage: 'signedmessage123',
+    pubkey: 'asdfasdf',
+  };
+  const endpoints = [
+    { endpoint: geoblockEndpoint, description: 'POST /geoblock', body: geoblockBody },
+    { endpoint: geoblockKeplerEndpoint, description: 'POST /geoblock-keplr', body: geoblockKeplrBody },
+  ];
+
+  describe.each(endpoints)('$description endpoint', ({ endpoint, body }) => {
     let getGeoComplianceReasonSpy: jest.SpyInstance;
     let isRestrictedCountryHeadersSpy: jest.SpyInstance;
     let isWhitelistedAddressSpy: jest.SpyInstance;
-
-    const body: any = {
-      address: testConstants.defaultAddress,
-      message: 'Test message',
-      action: ComplianceAction.CONNECT,
-      signedMessage: 'signedmessage123',
-      pubkey: 'asdfasdf',
-      timestamp: 1620000000,
-    };
 
     beforeEach(async () => {
       getGeoComplianceReasonSpy = getGeoComplianceReason as unknown as jest.Mock;
@@ -344,14 +366,11 @@ describe('ComplianceV2Controller', () => {
       isWhitelistedAddressSpy = isWhitelistedAddress as unknown as jest.Mock;
       ipAddrMock.mockReturnValue(ipAddr);
       await testMocks.seedData();
-      jest.mock('@cosmjs/crypto', () => ({
-        Secp256k1: {
-          verifySignature: jest.fn().mockResolvedValue(true),
-        },
-        ExtendedSecp256k1Signature: {
-          fromFixedLength: jest.fn().mockResolvedValue({} as ExtendedSecp256k1Signature),
-        },
-      }));
+      // Mock verification to true to reduce mocking within individual tests
+      verifySignatureMock.mockResolvedValue(true);
+      fromFixedLengthMock.mockResolvedValue({} as ExtendedSecp256k1Signature);
+      verifyADR36AminoMock.mockReturnValue(true);
+
       toBech32Mock.mockReturnValue(testConstants.defaultAddress);
       jest.spyOn(DateTime, 'now').mockReturnValue(DateTime.fromSeconds(1620000000)); // Mock current time
     });
@@ -364,58 +383,74 @@ describe('ComplianceV2Controller', () => {
     });
 
     it('should return 400 for non-dYdX address', async () => {
-      await sendRequest({
-        type: RequestMethod.POST,
-        path: '/v4/compliance/geoblock',
-        body: {
-          ...body,
-          address: '0x123', // Non-dYdX address
-        },
-        expectedStatus: 400,
-      });
+      if (endpoint === geoblockEndpoint) {
+        await sendRequest({
+          type: RequestMethod.POST,
+          path: endpoint,
+          body: {
+            ...body,
+            address: '0x123', // Non-dYdX address
+          },
+          expectedStatus: 400,
+        });
+      }
     });
 
     it('should return 400 for invalid timestamp', async () => {
-      await sendRequest({
-        type: RequestMethod.POST,
-        path: '/v4/compliance/geoblock',
-        body: {
-          ...body,
-          timestamp: 1619996600, // More than 30 seconds difference
-        },
-        expectedStatus: 400,
-      });
+      if (endpoint === geoblockEndpoint) {
+        await sendRequest({
+          type: RequestMethod.POST,
+          path: endpoint,
+          body: {
+            ...body,
+            timestamp: 1619996600, // More than 30 seconds difference
+          },
+          expectedStatus: 400,
+        });
+      }
     });
 
     it('should return 400 for invalid signature', async () => {
-      // Mock verifySignature to return false for this test
-      (Secp256k1.verifySignature as jest.Mock).mockResolvedValueOnce(false);
-
-      await sendRequest({
-        type: RequestMethod.POST,
-        path: '/v4/compliance/geoblock',
-        body,
-        expectedStatus: 400,
-      });
+      if (endpoint === geoblockEndpoint) {
+        // Mock verifySignature to return false for this test
+        (Secp256k1.verifySignature as jest.Mock).mockResolvedValueOnce(false);
+        await sendRequest({
+          type: RequestMethod.POST,
+          path: endpoint,
+          body,
+          expectedStatus: 400,
+        });
+      }
     });
 
     it('should return 400 for incorrect address', async () => {
-      toBech32Mock.mockResolvedValueOnce('invalid_address');
+      if (endpoint === geoblockEndpoint) {
+        toBech32Mock.mockResolvedValueOnce('invalid_address');
+        await sendRequest({
+          type: RequestMethod.POST,
+          path: endpoint,
+          body,
+          expectedStatus: 400,
+        });
+      }
+    });
 
-      await sendRequest({
-        type: RequestMethod.POST,
-        path: '/v4/compliance/geoblock',
-        body,
-        expectedStatus: 400,
-      });
+    it('should return 400 for failed keplr validation', async () => {
+      if (endpoint === geoblockKeplerEndpoint) {
+        (verifyADR36Amino as jest.Mock).mockReturnValueOnce(false);
+        await sendRequest({
+          type: RequestMethod.POST,
+          path: endpoint,
+          body,
+          expectedStatus: 400,
+        });
+      }
     });
 
     it('should process valid request', async () => {
-      (Secp256k1.verifySignature as jest.Mock).mockResolvedValueOnce(true);
-
       const response: any = await sendRequest({
         type: RequestMethod.POST,
-        path: '/v4/compliance/geoblock',
+        path: endpoint,
         body,
       });
 
@@ -424,7 +459,6 @@ describe('ComplianceV2Controller', () => {
     });
 
     it('should return COMPLIANT from a restricted country when whitelisted', async () => {
-      (Secp256k1.verifySignature as jest.Mock).mockResolvedValueOnce(true);
       getGeoComplianceReasonSpy.mockReturnValueOnce(ComplianceReason.US_GEO);
       isRestrictedCountryHeadersSpy.mockReturnValue(true);
       await dbHelpers.clearData();
@@ -435,7 +469,7 @@ describe('ComplianceV2Controller', () => {
       isWhitelistedAddressSpy.mockReturnValue(true);
       const response: any = await sendRequest({
         type: RequestMethod.POST,
-        path: '/v4/compliance/geoblock',
+        path: endpoint,
         body,
         expectedStatus: 200,
       });
@@ -448,14 +482,13 @@ describe('ComplianceV2Controller', () => {
     });
 
     it('should set status to BLOCKED for CONNECT action from a restricted country with no existing compliance status and no wallet', async () => {
-      (Secp256k1.verifySignature as jest.Mock).mockResolvedValueOnce(true);
       getGeoComplianceReasonSpy.mockReturnValueOnce(ComplianceReason.US_GEO);
       isRestrictedCountryHeadersSpy.mockReturnValue(true);
       await dbHelpers.clearData();
 
       const response: any = await sendRequest({
         type: RequestMethod.POST,
-        path: '/v4/compliance/geoblock',
+        path: endpoint,
         body,
         expectedStatus: 200,
       });
@@ -468,19 +501,28 @@ describe('ComplianceV2Controller', () => {
         reason: ComplianceReason.US_GEO,
       }));
 
+<<<<<<< HEAD
+=======
+      expect(stats.increment).toHaveBeenCalledWith(
+        `${config.SERVICE_NAME}.compliance-v2-controller.${endpoint === geoblockEndpoint ? 'geo_block' : 'geo_block_keplr'}.compliance_status_changed.count`,
+        {
+          newStatus: ComplianceStatus.BLOCKED,
+        },
+      );
+
+>>>>>>> a814b332 (Keplr geoblock new endpoint (#2117))
       expect(response.body.status).toEqual(ComplianceStatus.BLOCKED);
       expect(response.body.reason).toEqual(ComplianceReason.US_GEO);
       expect(response.body.updatedAt).toBeDefined();
     });
 
     it('should set status to FIRST_STRIKE_CLOSE_ONLY for CONNECT action from a restricted country with no existing compliance status and a wallet', async () => {
-      (Secp256k1.verifySignature as jest.Mock).mockResolvedValueOnce(true);
       getGeoComplianceReasonSpy.mockReturnValueOnce(ComplianceReason.US_GEO);
       isRestrictedCountryHeadersSpy.mockReturnValue(true);
 
       const response: any = await sendRequest({
         type: RequestMethod.POST,
-        path: '/v4/compliance/geoblock',
+        path: endpoint,
         body: {
           ...body,
           action: ComplianceAction.CONNECT,
@@ -495,6 +537,14 @@ describe('ComplianceV2Controller', () => {
         status: ComplianceStatus.FIRST_STRIKE_CLOSE_ONLY,
         reason: ComplianceReason.US_GEO,
       }));
+<<<<<<< HEAD
+=======
+      expect(stats.increment).toHaveBeenCalledWith(
+        `${config.SERVICE_NAME}.compliance-v2-controller.${endpoint === geoblockEndpoint ? 'geo_block' : 'geo_block_keplr'}.compliance_status_changed.count`,
+        {
+          newStatus: ComplianceStatus.FIRST_STRIKE_CLOSE_ONLY,
+        });
+>>>>>>> a814b332 (Keplr geoblock new endpoint (#2117))
 
       expect(response.body.status).toEqual(ComplianceStatus.FIRST_STRIKE_CLOSE_ONLY);
       expect(response.body.reason).toEqual(ComplianceReason.US_GEO);
@@ -502,12 +552,11 @@ describe('ComplianceV2Controller', () => {
     });
 
     it('should set status to COMPLIANT for any action from a non-restricted country with no existing compliance status', async () => {
-      (Secp256k1.verifySignature as jest.Mock).mockResolvedValueOnce(true);
       isRestrictedCountryHeadersSpy.mockReturnValue(false);
 
       const response: any = await sendRequest({
         type: RequestMethod.POST,
-        path: '/v4/compliance/geoblock',
+        path: endpoint,
         body,
         expectedStatus: 200,
       });
@@ -527,13 +576,12 @@ describe('ComplianceV2Controller', () => {
         address: testConstants.defaultAddress,
         status: ComplianceStatus.COMPLIANT,
       });
-      (Secp256k1.verifySignature as jest.Mock).mockResolvedValueOnce(true);
       getGeoComplianceReasonSpy.mockReturnValueOnce(ComplianceReason.US_GEO);
       isRestrictedCountryHeadersSpy.mockReturnValue(true);
 
       const response: any = await sendRequest({
         type: RequestMethod.POST,
-        path: '/v4/compliance/geoblock',
+        path: endpoint,
         body: {
           ...body,
           action: ComplianceAction.CONNECT,
@@ -560,13 +608,12 @@ describe('ComplianceV2Controller', () => {
         status: ComplianceStatus.FIRST_STRIKE,
         reason: ComplianceReason.US_GEO,
       });
-      (Secp256k1.verifySignature as jest.Mock).mockResolvedValueOnce(true);
       getGeoComplianceReasonSpy.mockReturnValueOnce(ComplianceReason.US_GEO);
       isRestrictedCountryHeadersSpy.mockReturnValue(true);
 
       const response: any = await sendRequest({
         type: RequestMethod.POST,
-        path: '/v4/compliance/geoblock',
+        path: endpoint,
         body: {
           ...body,
           action: ComplianceAction.CONNECT,
@@ -574,6 +621,15 @@ describe('ComplianceV2Controller', () => {
         expectedStatus: 200,
       });
 
+<<<<<<< HEAD
+=======
+      expect(stats.increment).toHaveBeenCalledWith(
+        `${config.SERVICE_NAME}.compliance-v2-controller.${endpoint === geoblockEndpoint ? 'geo_block' : 'geo_block_keplr'}.compliance_status_changed.count`,
+        {
+          newStatus: ComplianceStatus.CLOSE_ONLY,
+        });
+
+>>>>>>> a814b332 (Keplr geoblock new endpoint (#2117))
       const data: ComplianceStatusFromDatabase[] = await ComplianceStatusTable.findAll({}, [], {});
       expect(data).toHaveLength(1);
       expect(data[0]).toEqual(expect.objectContaining({
@@ -595,13 +651,12 @@ describe('ComplianceV2Controller', () => {
         reason: ComplianceReason.US_GEO,
         updatedAt: createdAt,
       });
-      (Secp256k1.verifySignature as jest.Mock).mockResolvedValueOnce(true);
       getGeoComplianceReasonSpy.mockReturnValueOnce(ComplianceReason.US_GEO);
       isRestrictedCountryHeadersSpy.mockReturnValue(true);
 
       const response: any = await sendRequest({
         type: RequestMethod.POST,
-        path: '/v4/compliance/geoblock',
+        path: endpoint,
         body: {
           ...body,
           action: ComplianceAction.CONNECT,
@@ -629,19 +684,26 @@ describe('ComplianceV2Controller', () => {
         status: ComplianceStatus.FIRST_STRIKE_CLOSE_ONLY,
         reason: ComplianceReason.US_GEO,
       });
-      (Secp256k1.verifySignature as jest.Mock).mockResolvedValueOnce(true);
       getGeoComplianceReasonSpy.mockReturnValueOnce(ComplianceReason.US_GEO);
       isRestrictedCountryHeadersSpy.mockReturnValue(true);
 
       const response: any = await sendRequest({
         type: RequestMethod.POST,
-        path: '/v4/compliance/geoblock',
+        path: endpoint,
         body: {
           ...body,
           action: ComplianceAction.INVALID_SURVEY,
         },
         expectedStatus: 200,
       });
+<<<<<<< HEAD
+=======
+      expect(stats.increment).toHaveBeenCalledWith(
+        `${config.SERVICE_NAME}.compliance-v2-controller.${endpoint === geoblockEndpoint ? 'geo_block' : 'geo_block_keplr'}.compliance_status_changed.count`,
+        {
+          newStatus: ComplianceStatus.CLOSE_ONLY,
+        });
+>>>>>>> a814b332 (Keplr geoblock new endpoint (#2117))
 
       const data: ComplianceStatusFromDatabase[] = await ComplianceStatusTable.findAll({}, [], {});
       expect(data).toHaveLength(1);
@@ -662,19 +724,26 @@ describe('ComplianceV2Controller', () => {
         status: ComplianceStatus.FIRST_STRIKE_CLOSE_ONLY,
         reason: ComplianceReason.US_GEO,
       });
-      (Secp256k1.verifySignature as jest.Mock).mockResolvedValueOnce(true);
       getGeoComplianceReasonSpy.mockReturnValueOnce(ComplianceReason.US_GEO);
       isRestrictedCountryHeadersSpy.mockReturnValue(true);
 
       const response: any = await sendRequest({
         type: RequestMethod.POST,
-        path: '/v4/compliance/geoblock',
+        path: endpoint,
         body: {
           ...body,
           action: ComplianceAction.VALID_SURVEY,
         },
         expectedStatus: 200,
       });
+<<<<<<< HEAD
+=======
+      expect(stats.increment).toHaveBeenCalledWith(
+        `${config.SERVICE_NAME}.compliance-v2-controller.${endpoint === geoblockEndpoint ? 'geo_block' : 'geo_block_keplr'}.compliance_status_changed.count`,
+        {
+          newStatus: ComplianceStatus.FIRST_STRIKE,
+        });
+>>>>>>> a814b332 (Keplr geoblock new endpoint (#2117))
 
       const data: ComplianceStatusFromDatabase[] = await ComplianceStatusTable.findAll({}, [], {});
       expect(data).toHaveLength(1);
