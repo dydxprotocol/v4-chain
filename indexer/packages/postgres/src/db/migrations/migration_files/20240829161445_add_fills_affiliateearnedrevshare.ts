2eb3323fa8b19a9d223957f3699239985ab4acc2
Jerry Fan <jf3101@columbia.edu>
2024-08-29 16:29:21 -0400

    split migratiion to two steps

--- indexer/packages/postgres/src/db/migrations/migration_files/20240829161445_add_fills_affiliateearnedrevshare.ts
+++ indexer/packages/postgres/src/db/migrations/migration_files/20240829161445_add_fills_affiliateearnedrevshare.ts
@@ -3,13 +3,11 @@ import * as Knex from 'knex';
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.table('fills', (table) => {
     table.string('affiliateEarnedRevShare').notNullable().defaultTo('0');
-    table.index('subaccountId');
   });
 }
 
 export async function down(knex: Knex): Promise<void> {
   await knex.schema.table('fills', (table) => {
-    table.dropIndex('subaccountId');
     table.dropColumn('affiliateEarnedRevShare');
   });
 }
