f68806ed1a91e08ab5d6cc4bef9930ed27cd2764
ea-coding <emanueladamiak@gmail.com>
2024-09-05 00:17:04 +0200

    [YBCP-74] Robust ratelimit/types testing

--- protocol/x/ratelimit/types/message_update_sdai_conversion_rate_test.go
+++ protocol/x/ratelimit/types/message_update_sdai_conversion_rate_test.go
@@ -4,8 +4,47 @@ import (
 	"testing"
 
 	"github.com/StreamFinance-Protocol/stream-chain/protocol/x/ratelimit/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/stretchr/testify/require"
 )
+
+func TestNewMsgUpdateSDAIConversionRate(t *testing.T) {
+	tests := []struct {
+		name           string
+		sender         string
+		conversionRate string
+		expectedMsg    types.MsgUpdateSDAIConversionRate
+	}{
+		{
+			name:           "Valid message",
+			sender:         "cosmos139f7kncmglres2nf3h4hc4tade85ekfr8sulz5",
+			conversionRate: "1",
+			expectedMsg: types.MsgUpdateSDAIConversionRate{
+				Sender:         "cosmos139f7kncmglres2nf3h4hc4tade85ekfr8sulz5",
+				ConversionRate: "1",
+			},
+		},
+		{
+			name:           "Different conversion rate",
+			sender:         "cosmos139f7kncmglres2nf3h4hc4tade85ekfr8sulz5",
+			conversionRate: "123456789123456789123456789",
+			expectedMsg: types.MsgUpdateSDAIConversionRate{
+				Sender:         "cosmos139f7kncmglres2nf3h4hc4tade85ekfr8sulz5",
+				ConversionRate: "123456789123456789123456789",
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			senderAddr, err := sdk.AccAddressFromBech32(tc.sender)
+			require.NoError(t, err)
+
+			msg := types.NewMsgUpdateSDAIConversionRate(senderAddr, tc.conversionRate)
+			require.Equal(t, &tc.expectedMsg, msg)
+		})
+	}
+}
 
 func TestMsgUpdateSDAIConversionRate_ValidateBasic(t *testing.T) {
 	tests := map[string]struct {
--- protocol/x/ratelimit/types/params_test.go
+++ protocol/x/ratelimit/types/params_test.go
@@ -37,3 +37,126 @@ func TestDefaultSDaiRateLimitParams(t *testing.T) {
 		defaultParams,
 	)
 }
+
+func TestLimitParamsValidate(t *testing.T) {
+	validDenom := "uatom"
+	validPeriod := time.Hour
+	validBaselineMinimum := dtypes.NewIntFromBigInt(big.NewInt(1000000))
+	validBaselineTvlPpm := uint32(500000)
+
+	tests := []struct {
+		name      string
+		params    types.LimitParams
+		expectErr bool
+	}{
+		{
+			name: "valid params",
+			params: types.LimitParams{
+				Denom: validDenom,
+				Limiters: []types.Limiter{
+					{
+						Period:          validPeriod,
+						BaselineMinimum: validBaselineMinimum,
+						BaselineTvlPpm:  validBaselineTvlPpm,
+					},
+				},
+			},
+			expectErr: false,
+		},
+		{
+			name: "invalid denom",
+			params: types.LimitParams{
+				Denom: "invalid denom",
+				Limiters: []types.Limiter{
+					{
+						Period:          validPeriod,
+						BaselineMinimum: validBaselineMinimum,
+						BaselineTvlPpm:  validBaselineTvlPpm,
+					},
+				},
+			},
+			expectErr: true,
+		},
+		{
+			name: "zero period",
+			params: types.LimitParams{
+				Denom: validDenom,
+				Limiters: []types.Limiter{
+					{
+						Period:          0,
+						BaselineMinimum: validBaselineMinimum,
+						BaselineTvlPpm:  validBaselineTvlPpm,
+					},
+				},
+			},
+			expectErr: true,
+		},
+		{
+			name: "negative baseline minimum",
+			params: types.LimitParams{
+				Denom: validDenom,
+				Limiters: []types.Limiter{
+					{
+						Period:          validPeriod,
+						BaselineMinimum: dtypes.NewIntFromBigInt(big.NewInt(-1)),
+						BaselineTvlPpm:  validBaselineTvlPpm,
+					},
+				},
+			},
+			expectErr: true,
+		},
+		{
+			name: "zero baseline minimum",
+			params: types.LimitParams{
+				Denom: validDenom,
+				Limiters: []types.Limiter{
+					{
+						Period:          validPeriod,
+						BaselineMinimum: dtypes.NewInt(0),
+						BaselineTvlPpm:  validBaselineTvlPpm,
+					},
+				},
+			},
+			expectErr: true,
+		},
+		{
+			name: "zero baseline tvl ppm",
+			params: types.LimitParams{
+				Denom: validDenom,
+				Limiters: []types.Limiter{
+					{
+						Period:          validPeriod,
+						BaselineMinimum: validBaselineMinimum,
+						BaselineTvlPpm:  0,
+					},
+				},
+			},
+			expectErr: true,
+		},
+		{
+			name: "baseline tvl ppm exceeds one million",
+			params: types.LimitParams{
+				Denom: validDenom,
+				Limiters: []types.Limiter{
+					{
+						Period:          validPeriod,
+						BaselineMinimum: validBaselineMinimum,
+						BaselineTvlPpm:  1000001,
+					},
+				},
+			},
+			expectErr: true,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			err := tc.params.Validate()
+			if tc.expectErr {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+			}
+		})
+	}
+}
