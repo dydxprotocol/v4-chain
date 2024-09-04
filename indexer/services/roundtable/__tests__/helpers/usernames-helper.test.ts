2bc6adbb5e4dd72b9538981ee817daba81b19444
affan <affan@dydx.exchange>
2024-09-04 08:35:25 -0400

    lint

--- indexer/services/roundtable/__tests__/helpers/usernames-helper.test.ts
+++ indexer/services/roundtable/__tests__/helpers/usernames-helper.test.ts
@@ -1,12 +1,11 @@
 import { generateUsername } from '../../src/helpers/usernames-helper';
 
 describe('usernames-helper', () => {
-    it('Check format of username', () => {
-      const username: string = generateUsername();
-      expect(username.match(/[A-Z]/g)).toHaveLength(2);
-      expect(username.match(/\d/g)).toHaveLength(3);
-      // check length is at the very minimum 7
+  it('Check format of username', () => {
+    const username: string = generateUsername();
+    expect(username.match(/[A-Z]/g)).toHaveLength(2);
+    expect(username.match(/\d/g)).toHaveLength(3);
+    // check length is at the very minimum 7
     expect(username.length).toBeGreaterThanOrEqual(7);
   });
 });
-  
