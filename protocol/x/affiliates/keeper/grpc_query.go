17b8dc35ffc93eab408dbad4f93d5fb82869ded2
Mohammed Affan <affan@dydx.exchange>
2024-09-06 10:58:55 -0400

    [OTE-694]implement queries for affiliates (#2214)

--- protocol/x/affiliates/keeper/grpc_query.go
+++ protocol/x/affiliates/keeper/grpc_query.go
@@ -3,6 +3,10 @@ package keeper
 import (
 	"context"
 
+	errorsmod "cosmossdk.io/errors"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/dydxprotocol/v4-chain/protocol/dtypes"
 	"github.com/dydxprotocol/v4-chain/protocol/x/affiliates/types"
 )
 
@@ -10,15 +14,62 @@ var _ types.QueryServer = Keeper{}
 
 func (k Keeper) AffiliateInfo(c context.Context,
 	req *types.AffiliateInfoRequest) (*types.AffiliateInfoResponse, error) {
-	return nil, nil
+	ctx := sdk.UnwrapSDKContext(c)
+
+	addr, err := sdk.AccAddressFromBech32(req.GetAddress())
+	if err != nil {
+		return nil, errorsmod.Wrapf(types.ErrInvalidAddress, "address: %s, error: %s",
+			req.GetAddress(), err.Error())
+	}
+
+	tierLevel, feeSharePpm, err := k.GetTierForAffiliate(ctx, addr.String())
+	if err != nil {
+		return nil, err
+	}
+
+	referredVolume, err := k.GetReferredVolume(ctx, req.GetAddress())
+	if err != nil {
+		return nil, err
+	}
+
+	stakedAmount := k.statsKeeper.GetStakedAmount(ctx, req.GetAddress())
+
+	return &types.AffiliateInfoResponse{
+		Tier:           tierLevel,
+		FeeSharePpm:    feeSharePpm,
+		ReferredVolume: dtypes.NewIntFromBigInt(referredVolume),
+		StakedAmount:   dtypes.NewIntFromBigInt(stakedAmount),
+	}, nil
 }
 
 func (k Keeper) ReferredBy(ctx context.Context,
 	req *types.ReferredByRequest) (*types.ReferredByResponse, error) {
-	return nil, nil
+	sdkCtx := sdk.UnwrapSDKContext(ctx)
+
+	affiliateAddr, exists := k.GetReferredBy(sdkCtx, req.GetAddress())
+	if !exists {
+		return &types.ReferredByResponse{}, errorsmod.Wrapf(
+			types.ErrAffiliateNotFound,
+			"affiliate not found for address: %s",
+			req.GetAddress(),
+		)
+	}
+
+	return &types.ReferredByResponse{
+		AffiliateAddress: affiliateAddr,
+	}, nil
 }
 
 func (k Keeper) AllAffiliateTiers(c context.Context,
 	req *types.AllAffiliateTiersRequest) (*types.AllAffiliateTiersResponse, error) {
-	return nil, nil
+	ctx := sdk.UnwrapSDKContext(c)
+
+	affiliateTiers, err := k.GetAllAffiliateTiers(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return &types.AllAffiliateTiersResponse{
+		Tiers: affiliateTiers,
+	}, nil
 }
