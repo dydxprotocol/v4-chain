c36722988ff0b0e4d38a83bfd9df745a84044be2
Adam Fraser <adamfraser0@gmail.com>
2024-09-09 15:48:25 -0400

    Improved code comments

--- indexer/packages/notifications/src/config.ts
+++ indexer/packages/notifications/src/config.ts
@@ -12,12 +12,15 @@ export const notificationsConfigSchema = {
   ...baseConfigSchema,
 
   // Private Key for the Google Firebase Messaging project
+  // default is a dummy value
   FIREBASE_PRIVATE_KEY_BASE64: parseString({ default: 'LS0tLS1CRUdJTiBQUklWQVRFIEtFWS0tLS0tClBMQUNFSE9MREVSX0tFWV9GT1JfREVWRUxPUE1FTlQKLS0tLS1FTkQgUFJJVkFURSBLRVktLS0tLQo=' }),
 
   // APP ID for the Google Firebase Messaging project
+  // default is a dummy value
   FIREBASE_PROJECT_ID: parseString({ default: 'dydx-v4' }),
 
   // Client email for the Google Firebase Messaging project
+  // default is a dummy value
   FIREBASE_CLIENT_EMAIL: parseString({ default: 'firebase-adminsdk-f0joo@dydx-v4.iam.gserviceaccount.com' }),
 };
 
--- indexer/packages/notifications/src/localization.ts
+++ indexer/packages/notifications/src/localization.ts
@@ -5,6 +5,15 @@ import {
 } from './types';
 
 function replacePlaceholders(template: string, variables: Record<string, string>): string {
+  // The template string contains placeholders in the format "{KEY}".
+  // For example: 'Your order for {AMOUNT} {MARKET} was filled at ${AVERAGE_PRICE}'.
+  // This function replaces these placeholders with corresponding values
+  // from the "variables" object.
+  // If the key inside "{}" exists in the "variables" object, it is replaced
+  // with the matching value.
+  // If the key does not exist, the placeholder remains unchanged in the resulting string.
+  // The .replace method uses a regular expression to find all words inside "{}"
+  // and replaces them as described.
   return template.replace(/{(\w+)}/g, (_, key) => variables[key] || `{${key}}`);
 }
 
