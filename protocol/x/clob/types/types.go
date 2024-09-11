a2682b87ec0799dc9eb7b0e8f4afbe098aea164c
affan <affan@dydx.exchange>
2024-09-11 16:13:20 -0400

    add comment

--- protocol/x/clob/types/types.go
+++ protocol/x/clob/types/types.go
@@ -9,6 +9,12 @@ type FillForProcess interface {
 	MakerFeeQuoteQuantums() *big.Int
 	FillQuoteQuantums() *big.Int
 	ProductId() uint32
+	// MonthlyRollingTakerVolumeQuantums is the total taker volume for
+	// the given taker address in the last 30 days. This rolling volume
+	// does not include stats of the current block being processed.
+	// If there are multiple fills for the taker address in the
+	// same block, this volume will not be included in the function
+	// below
 	MonthlyRollingTakerVolumeQuantums() uint64
 }
 
