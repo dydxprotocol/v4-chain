32223c239619178f16ef29bc3d8765f9e7283b9a
Will Liu <will@dydx.exchange>
2024-09-11 17:23:50 -0400

    re-add

--- indexer/services/ender/src/scripts/handlers/dydx_funding_handler.sql
+++ indexer/services/ender/src/scripts/handlers/dydx_funding_handler.sql
@@ -1,5 +1,19 @@
 CREATE OR REPLACE FUNCTION dydx_funding_handler(
     block_height int, block_time timestamp, event_data jsonb, event_index int, transaction_index int) RETURNS jsonb AS $$
+/**
+  Parameters:
+    - block_height: the height of the block being processing.
+    - block_time: the time of the block being processed.
+    - event_data: The 'data' field of the IndexerTendermintEvent (https://github.com/dydxprotocol/v4-chain/blob/9ed26bd/proto/dydxprotocol/indexer/indexer_manager/event.proto#L25)
+        converted to JSON format. Conversion to JSON is expected to be done by JSON.stringify.
+    - event_index: The 'event_index' of the IndexerTendermintEvent.
+    - transaction_index: The transaction_index of the IndexerTendermintEvent after the conversion that takes into
+        account the block_event (https://github.com/dydxprotocol/v4-chain/blob/9ed26bd/indexer/services/ender/src/lib/helper.ts#L41)
+  Returns: JSON object containing fields:
+    - perpetual_markets: A mapping from perpetual market id to the associated perpetual market in perpetual-market-model format (https://github.com/dydxprotocol/v4-chain/blob/9ed26bd/indexer/packages/postgres/src/models/perpetual-market-model.ts).
+    - errors: An array containing an error string (or NULL if no error occurred) for each FundingEventUpdate.
+  (Note that no text should exist before the function declaration to ensure that exception line numbers are correct.)
+*/
 DECLARE
     PPM_EXPONENT constant numeric = -6;
     FUNDING_RATE_FROM_PROTOCOL_IN_HOURS constant numeric = 8;
@@ -14,7 +28,7 @@ DECLARE
     funding_update jsonb;
     perpetual_markets_response jsonb = jsonb_build_object();
     funding_update_response jsonb = jsonb_build_object();
-    errors_response jsonb[] = '{}';
+    errors_response jsonb[];
     event_id bytea;
 
     -- Declare JSONB objects for the maps
@@ -52,7 +66,7 @@ BEGIN
 
         CASE event_data->'type'
             WHEN TYPE_PREMIUM_SAMPLE THEN
-                -- Do nothing for premium samples
+                /** Here we just need to return the associated perpetual market. */
             WHEN TYPE_FUNDING_RATE_AND_INDEX THEN
                 -- Retrieve the latest oracle price for the marketId
                 IF oracle_price_map->(perpetual_market_map->(perpetual_market_id::text)->>'marketId') IS NULL THEN
