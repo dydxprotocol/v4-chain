46a1c88ed9f6934d26404eda541c203db0389893
roy-dydx <133032749+roy-dydx@users.noreply.github.com>
2024-09-16 16:20:57 -0400

    Add instance to vulcan metrics (#2265)

--- indexer/services/vulcan/src/lib/on-message.ts
+++ indexer/services/vulcan/src/lib/on-message.ts
@@ -1,4 +1,5 @@
 import {
+  getInstanceId,
   logger,
   stats,
   ParseMessageError,
@@ -42,9 +43,17 @@ function getMessageType(update: OffChainUpdateV1): string {
 }
 
 export async function onMessage(message: KafkaMessage): Promise<void> {
-  stats.increment(`${config.SERVICE_NAME}.received_kafka_message`, 1);
+  stats.increment(
+    `${config.SERVICE_NAME}.received_kafka_message`,
+    1,
+    { instance: getInstanceId() },
+  );
   if (!message || !message.value || !message.timestamp) {
-    stats.increment(`${config.SERVICE_NAME}.empty_kafka_message`, 1);
+    stats.increment(
+      `${config.SERVICE_NAME}.empty_kafka_message`,
+      1,
+      { instance: getInstanceId() },
+    );
     logger.error({
       at: 'onMessage#onMessage',
       message: 'Empty message',
@@ -59,6 +68,7 @@ export async function onMessage(message: KafkaMessage): Promise<void> {
     STATS_NO_SAMPLING,
     {
       topic: KafkaTopics.TO_VULCAN,
+      instance: getInstanceId(),
     },
   );
 
@@ -71,6 +81,7 @@ export async function onMessage(message: KafkaMessage): Promise<void> {
       {
         topic: KafkaTopics.TO_VULCAN,
         event_type: String(message.headers?.event_type),
+        instance: getInstanceId(),
       },
     );
   }
@@ -130,6 +141,7 @@ export async function onMessage(message: KafkaMessage): Promise<void> {
         {
           topic: KafkaTopics.TO_VULCAN,
           event_type: String(headers?.event_type),
+          instance: getInstanceId(),
         },
       );
     }
@@ -164,6 +176,7 @@ export async function onMessage(message: KafkaMessage): Promise<void> {
       {
         success: success.toString(),
         messageType: getMessageType(update),
+        instance: getInstanceId(),
       },
     );
   }
