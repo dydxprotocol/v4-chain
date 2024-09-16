d964f26e8079d4329a15814791b0f138dc712131
Mohammed Affan <affan@dydx.exchange>
2024-09-16 11:08:02 -0400

    [OTE-695] Aggregate referred volume in endblocker (#2218)

--- protocol/x/affiliates/types/expected_keepers.go
+++ protocol/x/affiliates/types/expected_keepers.go
@@ -5,10 +5,12 @@ import (
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	revsharetypes "github.com/dydxprotocol/v4-chain/protocol/x/revshare/types"
+	stattypes "github.com/dydxprotocol/v4-chain/protocol/x/stats/types"
 )
 
 type StatsKeeper interface {
 	GetStakedAmount(ctx sdk.Context, delegatorAddr string) *big.Int
+	GetBlockStats(ctx sdk.Context) *stattypes.BlockStats
 }
 
 type RevShareKeeper interface {
