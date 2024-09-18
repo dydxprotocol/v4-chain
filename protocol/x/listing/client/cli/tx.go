ba731b00e3163f7c3ff553b4300d564c11eaa81f
shrenujb <98204323+shrenujb@users.noreply.github.com>
2024-09-17 21:05:08 -0400

    only create clob pair during deliver tx and cli cmd for pml (#2272)
    Signed-off-by: Shrenuj Bansal <shrenuj@dydx.exchange>

--- protocol/x/listing/client/cli/tx.go
+++ protocol/x/listing/client/cli/tx.go
@@ -3,6 +3,10 @@ package cli
 import (
 	"fmt"
 
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/client/tx"
+	satypes "github.com/dydxprotocol/v4-chain/protocol/x/subaccounts/types"
+
 	"github.com/spf13/cobra"
 
 	"github.com/cosmos/cosmos-sdk/client"
@@ -18,6 +22,39 @@ func GetTxCmd() *cobra.Command {
 		SuggestionsMinimumDistance: 2,
 		RunE:                       client.ValidateCmd,
 	}
+
+	cmd.AddCommand(CmdCreateMarketPermissionless())
+
+	return cmd
+}
+
+// CmdCreateMarketPermissionless is the CLI command for creating a permissionless market.
+func CmdCreateMarketPermissionless() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "create-market [ticker] [address]",
+		Short: "Create new market with permissionless access",
+		Args:  cobra.ExactArgs(2),
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+			ticker, address := args[0], args[1]
+
+			// Create MsgCreateMarketPermissionless.
+			msg := &types.MsgCreateMarketPermissionless{
+				Ticker: ticker,
+				SubaccountId: &satypes.SubaccountId{
+					Owner:  address,
+					Number: 0,
+				},
+			}
+
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
 
 	return cmd
 }
