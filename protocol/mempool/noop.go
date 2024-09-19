4930b4b4eaae05dda8444c9cf976cf29308c274c
taehoon <19664986+ttl33@users.noreply.github.com>
2024-09-19 09:53:27 -0400

    work!

--- protocol/mempool/noop.go
+++ protocol/mempool/noop.go
@@ -11,6 +11,7 @@ import (
 // sign
 // another sign
 // think it should work now
+// okay should really work now
 // TODO(DEC-1194): this is copied from SDK. Remove this forked impl in favor of SDK's NoOpMempool.
 var _ mempool.Mempool = (*noOpMempool)(nil)
 
