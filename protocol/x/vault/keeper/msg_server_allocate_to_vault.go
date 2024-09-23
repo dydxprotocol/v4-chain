b0034d0c16dd82cc75077ed9a30fb4291ed31a34
shrenujb <98204323+shrenujb@users.noreply.github.com>
2024-09-23 18:17:49 -0400

    Add TransferToVault keeper method (#2327)
    Signed-off-by: Shrenuj Bansal <shrenuj@dydx.exchange>

--- protocol/x/vault/keeper/msg_server_allocate_to_vault.go
+++ protocol/x/vault/keeper/msg_server_allocate_to_vault.go
@@ -6,9 +6,6 @@ import (
 	errorsmod "cosmossdk.io/errors"
 
 	"github.com/dydxprotocol/v4-chain/protocol/lib"
-	assetstypes "github.com/dydxprotocol/v4-chain/protocol/x/assets/types"
-	clobtypes "github.com/dydxprotocol/v4-chain/protocol/x/clob/types"
-	sendingtypes "github.com/dydxprotocol/v4-chain/protocol/x/sending/types"
 	"github.com/dydxprotocol/v4-chain/protocol/x/vault/types"
 )
 
@@ -29,40 +26,8 @@ func (k msgServer) AllocateToVault(
 		)
 	}
 
-	// Check if vault has a corresponding clob pair.
-	_, exists := k.Keeper.clobKeeper.GetClobPair(ctx, clobtypes.ClobPairId(msg.VaultId.Number))
-	if !exists {
-		return nil, types.ErrClobPairNotFound
-	}
-
-	// If vault doesn't exist:
-	// 1. initialize params with `STAND_BY` status.
-	// 2. add vault to address store.
-	_, exists = k.Keeper.GetVaultParams(ctx, msg.VaultId)
-	if !exists {
-		err := k.Keeper.SetVaultParams(
-			ctx,
-			msg.VaultId,
-			types.VaultParams{
-				Status: types.VaultStatus_VAULT_STATUS_STAND_BY,
-			},
-		)
-		if err != nil {
-			return nil, err
-		}
-		k.Keeper.AddVaultToAddressStore(ctx, msg.VaultId)
-	}
-
-	// Transfer from main vault to the specified vault.
-	if err := k.Keeper.sendingKeeper.ProcessTransfer(
-		ctx,
-		&sendingtypes.Transfer{
-			Sender:    types.MegavaultMainSubaccount,
-			Recipient: *msg.VaultId.ToSubaccountId(),
-			AssetId:   assetstypes.AssetUsdc.Id,
-			Amount:    msg.QuoteQuantums.BigInt().Uint64(), // validated to be positive above.
-		},
-	); err != nil {
+	err := k.Keeper.AllocateToVault(ctx, msg.VaultId, msg.QuoteQuantums.BigInt())
+	if err != nil {
 		return nil, err
 	}
 
