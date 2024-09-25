b5d9dd2333ed690f89462f475c880630863e1de4
soletty <solal@notebooklabs.xyz>
2024-09-25 04:46:15 +0400

    fix

--- indexer/services/ender/src/scripts/handlers/dydx_update_perpetual_handler.sql
+++ indexer/services/ender/src/scripts/handlers/dydx_update_perpetual_handler.sql
@@ -18,7 +18,7 @@ BEGIN
     perpetual_market_record."atomicResolution" = (event_data->'atomicResolution')::integer;
     perpetual_market_record."liquidityTierId" = (event_data->'liquidityTier')::integer;
     perpetual_market_record."dangerIndexPpm" = (event_data->'dangerIndexPpm')::integer;
-    perpetual_market_record."isolatedMarketMaxCumulativeInsuranceFundDeltaPerBlock" = (event_data->'isolatedMarketMaxCumulativeInsuranceFundDeltaPerBlock')::bigint;
+    perpetual_market_record."isolatedMarketMaxCumulativeInsuranceFundDeltaPerBlock" = (event_data->>'isolatedMarketMaxCumulativeInsuranceFundDeltaPerBlock')::bigint;
 
     UPDATE perpetual_markets
     SET
