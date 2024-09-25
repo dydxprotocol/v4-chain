9fcbcc878ef9f0e7eb8be043e043aa29bbf2d420
dydxwill <119354122+dydxwill@users.noreply.github.com>
2024-09-25 15:24:30 -0400

    Add sql script latency metrics (#2356)

--- indexer/services/ender/src/handlers/asset-handler.ts
+++ indexer/services/ender/src/handlers/asset-handler.ts
@@ -1,3 +1,4 @@
+import { stats } from '@dydxprotocol-indexer/base';
 import {
   AssetFromDatabase,
   AssetModel,
@@ -6,6 +7,7 @@ import {
 import { AssetCreateEventV1 } from '@dydxprotocol-indexer/v4-protos';
 import * as pg from 'pg';
 
+import config from '../config';
 import { ConsolidatedKafkaEvent } from '../lib/types';
 import { Handler } from './handler';
 
@@ -18,6 +20,12 @@ export class AssetCreationHandler extends Handler<AssetCreateEventV1> {
 
   // eslint-disable-next-line @typescript-eslint/require-await
   public async internalHandle(resultRow: pg.QueryResultRow): Promise<ConsolidatedKafkaEvent[]> {
+    // Handle latency from resultRow
+    stats.timing(
+      `${config.SERVICE_NAME}.handle_asset_event.sql_latency`,
+      Number(resultRow.latency),
+      this.generateTimingStatsOptions(),
+    );
     const asset: AssetFromDatabase = AssetModel.fromJson(
       resultRow.asset) as AssetFromDatabase;
     assetRefresher.addAsset(asset);
