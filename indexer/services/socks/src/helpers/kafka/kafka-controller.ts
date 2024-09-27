0a3b3b8d6031a7a44192b082871267aa6f1616a6
roy-dydx <133032749+roy-dydx@users.noreply.github.com>
2024-09-27 15:18:43 -0400

    Create consumer using rack id (#2352) (#2393)

--- indexer/services/socks/src/helpers/kafka/kafka-controller.ts
+++ indexer/services/socks/src/helpers/kafka/kafka-controller.ts
@@ -2,18 +2,19 @@ import { logger } from '@dydxprotocol-indexer/base';
 import {
   WebsocketTopics,
   consumer,
+  initConsumer,
   stopConsumer,
 } from '@dydxprotocol-indexer/kafka';
 
 export async function connect(): Promise<void> {
-  await consumer.connect();
+  await initConsumer();
 
   logger.info({
     at: 'kafka-controller#connect',
     message: 'Connected to Kafka',
   });
 
-  await consumer.subscribe({ topics: Object.values(WebsocketTopics) });
+  await consumer!.subscribe({ topics: Object.values(WebsocketTopics) });
 }
 
 export async function disconnect(): Promise<void> {
