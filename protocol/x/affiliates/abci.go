806dc05a6fc8e0591cd34f4581abfb8746a57ce8
affan <affan@dydx.exchange>
2024-10-01 12:25:41 -0400

    fix other log

--- protocol/x/affiliates/abci.go
+++ protocol/x/affiliates/abci.go
@@ -1,8 +1,6 @@
 package affiliates
 
 import (
-	"runtime/debug"
-
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/dydxprotocol/v4-chain/protocol/lib/log"
 	"github.com/dydxprotocol/v4-chain/protocol/x/affiliates/keeper"
@@ -13,7 +11,6 @@ func EndBlocker(
 	keeper *keeper.Keeper,
 ) {
 	if err := keeper.AggregateAffiliateReferredVolumeForFills(ctx); err != nil {
-		log.ErrorLog(ctx, "error aggregating affiliate volume for fills", "error",
-			err, "stack", string(debug.Stack()))
+		log.ErrorLogWithError(ctx, "error aggregating affiliate volume for fills", err)
 	}
 }
