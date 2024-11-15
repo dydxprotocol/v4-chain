f408f717f348454673a01b8548f7f242b6c2c869
Teddy Ding <teddy@dydx.exchange>
2024-11-15 10:27:42 -0500

    fix(affiliates): Generate username with address, not subaccount id (#2575)

--- indexer/services/roundtable/src/helpers/usernames-helper.ts
+++ indexer/services/roundtable/src/helpers/usernames-helper.ts
@@ -7,11 +7,11 @@ import nouns from './nouns.json';
 const suffixCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
 
 export function generateUsernameForSubaccount(
-  subaccountId: string,
+  address: string,
   subaccountNum: number,
   nounce: number = 0, // incremented in case of collision
 ): string {
-  const rng = seedrandom(`${subaccountId}/${subaccountNum}/${nounce}`);
+  const rng = seedrandom(`${address}/${subaccountNum}/${nounce}`);
   const randomAdjective: string = adjectives[Math.floor(rng() * adjectives.length)];
   const randomNoun: string = nouns[Math.floor(rng() * nouns.length)];
   const randomSuffix: string = Array.from(
