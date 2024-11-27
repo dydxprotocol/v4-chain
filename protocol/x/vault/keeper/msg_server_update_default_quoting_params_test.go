37a349567987ce6972168e68ca888518ba1659a7
Tian Qin <tian@dydx.exchange>
2024-11-27 12:08:52 -0500

    allow megavault operator to set default quoting params

--- protocol/x/vault/keeper/msg_server_update_default_quoting_params_test.go
+++ protocol/x/vault/keeper/msg_server_update_default_quoting_params_test.go
@@ -9,28 +9,39 @@ import (
 	testapp "github.com/dydxprotocol/v4-chain/protocol/testutil/app"
 	"github.com/dydxprotocol/v4-chain/protocol/testutil/constants"
 
+	"github.com/cometbft/cometbft/types"
 	"github.com/dydxprotocol/v4-chain/protocol/x/vault/keeper"
-	"github.com/dydxprotocol/v4-chain/protocol/x/vault/types"
+	vaulttypes "github.com/dydxprotocol/v4-chain/protocol/x/vault/types"
 	"github.com/stretchr/testify/require"
 )
 
 func TestMsgUpdateDefaultQuotingParams(t *testing.T) {
 	tests := map[string]struct {
 		// Msg.
-		msg *types.MsgUpdateDefaultQuotingParams
+		msg *vaulttypes.MsgUpdateDefaultQuotingParams
+		// Operator.
+		operator string
 		// Expected error
 		expectedErr string
 	}{
-		"Success. Update to default": {
-			msg: &types.MsgUpdateDefaultQuotingParams{
+		"Success. Update to default. Gov Authority": {
+			msg: &vaulttypes.MsgUpdateDefaultQuotingParams{
 				Authority:            lib.GovModuleAddress.String(),
-				DefaultQuotingParams: types.DefaultQuotingParams(),
+				DefaultQuotingParams: vaulttypes.DefaultQuotingParams(),
 			},
+			operator: constants.AliceAccAddress.String(),
+		},
+		"Success. Update to default. Operator Authority": {
+			msg: &vaulttypes.MsgUpdateDefaultQuotingParams{
+				Authority:            constants.CarlAccAddress.String(),
+				DefaultQuotingParams: vaulttypes.DefaultQuotingParams(),
+			},
+			operator: constants.CarlAccAddress.String(),
 		},
 		"Success. Update to non-default": {
-			msg: &types.MsgUpdateDefaultQuotingParams{
+			msg: &vaulttypes.MsgUpdateDefaultQuotingParams{
 				Authority: lib.GovModuleAddress.String(),
-				DefaultQuotingParams: types.QuotingParams{
+				DefaultQuotingParams: vaulttypes.QuotingParams{
 					Layers:                           3,
 					SpreadMinPpm:                     234_567,
 					SpreadBufferPpm:                  6_789,
@@ -40,18 +51,20 @@ func TestMsgUpdateDefaultQuotingParams(t *testing.T) {
 					ActivationThresholdQuoteQuantums: dtypes.NewInt(2_121_343_787),
 				},
 			},
+			operator: constants.AliceAccAddress.String(),
 		},
 		"Failure - Invalid Authority": {
-			msg: &types.MsgUpdateDefaultQuotingParams{
+			msg: &vaulttypes.MsgUpdateDefaultQuotingParams{
 				Authority:            constants.AliceAccAddress.String(),
-				DefaultQuotingParams: types.DefaultQuotingParams(),
+				DefaultQuotingParams: vaulttypes.DefaultQuotingParams(),
 			},
+			operator:    constants.BobAccAddress.String(),
 			expectedErr: "invalid authority",
 		},
 		"Failure - Invalid Params": {
-			msg: &types.MsgUpdateDefaultQuotingParams{
+			msg: &vaulttypes.MsgUpdateDefaultQuotingParams{
 				Authority: lib.GovModuleAddress.String(),
-				DefaultQuotingParams: types.QuotingParams{
+				DefaultQuotingParams: vaulttypes.QuotingParams{
 					Layers:                           3,
 					SpreadMinPpm:                     4_000,
 					SpreadBufferPpm:                  2_000,
@@ -61,13 +74,26 @@ func TestMsgUpdateDefaultQuotingParams(t *testing.T) {
 					ActivationThresholdQuoteQuantums: dtypes.NewInt(1_000_000_000),
 				},
 			},
-			expectedErr: types.ErrInvalidOrderSizePctPpm.Error(),
+			operator:    constants.AliceAccAddress.String(),
+			expectedErr: vaulttypes.ErrInvalidOrderSizePctPpm.Error(),
 		},
 	}
 
 	for name, tc := range tests {
 		t.Run(name, func(t *testing.T) {
-			tApp := testapp.NewTestAppBuilder(t).Build()
+			tApp := testapp.NewTestAppBuilder(t).WithGenesisDocFn(func() (genesis types.GenesisDoc) {
+				genesis = testapp.DefaultGenesis()
+				// Set megavault operator.
+				testapp.UpdateGenesisDocWithAppStateForModule(
+					&genesis,
+					func(genesisState *vaulttypes.GenesisState) {
+						genesisState.OperatorParams = vaulttypes.OperatorParams{
+							Operator: tc.operator,
+						}
+					},
+				)
+				return genesis
+			}).Build()
 			ctx := tApp.InitChain()
 			k := tApp.App.VaultKeeper
 			ms := keeper.NewMsgServerImpl(k)
@@ -75,7 +101,7 @@ func TestMsgUpdateDefaultQuotingParams(t *testing.T) {
 			_, err := ms.UpdateDefaultQuotingParams(ctx, tc.msg)
 			if tc.expectedErr != "" {
 				require.ErrorContains(t, err, tc.expectedErr)
-				require.Equal(t, types.DefaultQuotingParams(), k.GetDefaultQuotingParams(ctx))
+				require.Equal(t, vaulttypes.DefaultQuotingParams(), k.GetDefaultQuotingParams(ctx))
 			} else {
 				require.NoError(t, err)
 				require.Equal(t, tc.msg.DefaultQuotingParams, k.GetDefaultQuotingParams(ctx))
