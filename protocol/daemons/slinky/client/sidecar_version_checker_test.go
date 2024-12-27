4a56c6094a59f796745e2a104593da9b13fedd4b
Chenyao Yu <4844716+chenyaoy@users.noreply.github.com>
2024-12-27 10:16:45 -0500

    Fix sidecar version checker

--- protocol/daemons/slinky/client/sidecar_version_checker_test.go
+++ protocol/daemons/slinky/client/sidecar_version_checker_test.go
@@ -2,6 +2,7 @@ package client_test
 
 import (
 	"context"
+	"strings"
 	"testing"
 
 	"cosmossdk.io/log"
@@ -43,6 +44,36 @@ func TestSidecarVersionChecker(t *testing.T) {
 		require.NoError(t, fetcher.Start(context.Background()))
 		require.ErrorContains(t, fetcher.CheckSidecarVersion(context.Background()),
 			"Sidecar version 0.0.0 is less than minimum required version")
+		fetcher.Stop()
+	})
+
+	t.Run("Checks sidecar version passes with prefix before version", func(t *testing.T) {
+		slinky := mocks.NewOracleClient(t)
+		slinky.On("Stop").Return(nil)
+		slinky.On("Start", mock.Anything).Return(nil).Once()
+		require.True(t, strings.HasPrefix(client.MinSidecarVersion, "v"), "MinSidecarVersion must start with 'v'")
+
+		slinky.On("Version", mock.Anything, mock.Anything).
+			Return(&types.QueryVersionResponse{
+				Version: client.MinSidecarVersion[1:], // Remove the "v" prefix
+			}, nil)
+		fetcher = client.NewSidecarVersionChecker(slinky, logger)
+		require.NoError(t, fetcher.Start(context.Background()))
+		require.NoError(t, fetcher.CheckSidecarVersion(context.Background()))
+		fetcher.Stop()
+	})
+
+	t.Run("Checks sidecar version passes without v prefix", func(t *testing.T) {
+		slinky := mocks.NewOracleClient(t)
+		slinky.On("Stop").Return(nil)
+		slinky.On("Start", mock.Anything).Return(nil).Once()
+		slinky.On("Version", mock.Anything, mock.Anything).
+			Return(&types.QueryVersionResponse{
+				Version: "tests/integration/v1.2.0",
+			}, nil)
+		fetcher = client.NewSidecarVersionChecker(slinky, logger)
+		require.NoError(t, fetcher.Start(context.Background()))
+		require.NoError(t, fetcher.CheckSidecarVersion(context.Background()))
 		fetcher.Stop()
 	})
 
