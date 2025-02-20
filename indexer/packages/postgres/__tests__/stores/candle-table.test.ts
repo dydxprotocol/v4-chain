edf28beff27e88e48be88df369863480d0f416b7
Tian <tian@dydx.exchange>
2025-02-20 17:51:05 -0500

    initialize candles cache based on latest indexed block time (#2725)

--- indexer/packages/postgres/__tests__/stores/candle-table.test.ts
+++ indexer/packages/postgres/__tests__/stores/candle-table.test.ts
@@ -1,10 +1,17 @@
-import { CandleFromDatabase, CandleUpdateObject } from '../../src/types';
+import { DateTime } from 'luxon';
+
+import {
+  CandleFromDatabase, CandleResolution, CandlesMap, CandleUpdateObject,
+} from '../../src/types';
+import * as BlockTable from '../../src/stores/block-table';
 import * as CandleTable from '../../src/stores/candle-table';
 import {
   clearData,
   teardown,
 } from '../../src/helpers/db-helpers';
-import { defaultCandle, defaultCandleId, defaultPerpetualMarket2 } from '../helpers/constants';
+import {
+  defaultCandle, defaultCandleId, defaultPerpetualMarket2,
+} from '../helpers/constants';
 
 describe('CandleTable', () => {
   afterEach(async () => {
@@ -76,5 +83,61 @@ describe('CandleTable', () => {
     );
 
     expect(candle).toEqual(expect.objectContaining(updatedCandle));
+  });
+
+  it('Successfully finds candles map based on latest block', async () => {
+    const latestBlockTime: DateTime = DateTime.utc(2022, 6, 1);
+    const candleMinuteResThreeHoursAgo = {
+      ...defaultCandle,
+      resolution: CandleResolution.ONE_MINUTE,
+      startedAt: latestBlockTime.minus({ hours: 3 }).toISO(),
+    };
+    const candleFiveMinuteResTwoHoursAgo = {
+      ...defaultCandle,
+      resolution: CandleResolution.FIVE_MINUTES,
+      startedAt: latestBlockTime.minus({ hours: 2 }).toISO(),
+    };
+    const candleHourResThreeDaysAgo = {
+      ...defaultCandle,
+      resolution: CandleResolution.FOUR_HOURS,
+      startedAt: latestBlockTime.minus({ days: 3 }).toISO(),
+    };
+    const candleDayResOneDayAgo = {
+      ...defaultCandle,
+      resolution: CandleResolution.ONE_DAY,
+      startedAt: latestBlockTime.minus({ days: 1 }).toISO(),
+    };
+    await Promise.all([
+      // Create two blocks with block time 1 second apart.
+      BlockTable.create({
+        blockHeight: '1',
+        time: latestBlockTime.minus({ seconds: 1 }).toISO(),
+      }),
+      BlockTable.create({
+        blockHeight: '2',
+        time: latestBlockTime.toISO(),
+      }),
+      // Create candles.
+      CandleTable.create(candleMinuteResThreeHoursAgo), // should not be part of candles map
+      CandleTable.create(candleFiveMinuteResTwoHoursAgo), // should be part of candles map
+      CandleTable.create(candleHourResThreeDaysAgo), // should not be part of candles map
+      CandleTable.create(candleDayResOneDayAgo), // should be part of candles map
+    ]);
+
+    const candlesMap: CandlesMap = await CandleTable.findCandlesMap(
+      [defaultCandle.ticker],
+      latestBlockTime.toISO(),
+    );
+
+    expect(Object.keys(candlesMap)).toEqual([defaultCandle.ticker]);
+    expect(
+      Object.keys(candlesMap[defaultCandle.ticker]),
+    ).toEqual([CandleResolution.FIVE_MINUTES, CandleResolution.ONE_DAY]);
+    expect(
+      candlesMap[defaultCandle.ticker][CandleResolution.FIVE_MINUTES],
+    ).toEqual(expect.objectContaining(candleFiveMinuteResTwoHoursAgo));
+    expect(
+      candlesMap[defaultCandle.ticker][CandleResolution.ONE_DAY],
+    ).toEqual(expect.objectContaining(candleDayResOneDayAgo));
   });
 });
