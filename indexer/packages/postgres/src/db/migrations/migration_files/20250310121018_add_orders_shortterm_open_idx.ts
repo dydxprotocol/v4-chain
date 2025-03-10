95f43b79942812058c03bd7a597c75d3ab9e7c24
Teddy Ding <teddy@dydx.exchange>
2025-03-10 12:24:00 -0400

    update index

--- indexer/packages/postgres/src/db/migrations/migration_files/20250310121018_add_orders_shortterm_open_idx.ts
+++ indexer/packages/postgres/src/db/migrations/migration_files/20250310121018_add_orders_shortterm_open_idx.ts
@@ -4,7 +4,7 @@ export async function up(knex: Knex): Promise<void> {
   await knex.raw(`
     CREATE INDEX CONCURRENTLY IF NOT EXISTS "orders_open_shortterm_idx" 
     ON "orders" ("goodTilBlock")
-    WHERE status = 'OPEN' AND "orderFlags" = 0;
+    WHERE status = 'OPEN' AND "orderFlags" = 0 AND "goodTilBlock" IS NOT NULL;
   `);
 }
 
