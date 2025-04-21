44c6e4bd3285900e12d44560188bd16ac004f92b
lolted <teddy@dydx.exchange>
2025-04-21 15:45:34 -0400

    perf(vault queries): Cache MV/Vault PNL on request (#2783)

--- indexer/services/comlink/__tests__/controllers/api/v4/historical-pnl-controller.test.ts
+++ indexer/services/comlink/__tests__/controllers/api/v4/historical-pnl-controller.test.ts
@@ -7,7 +7,7 @@ import {
 } from '@dydxprotocol-indexer/postgres';
 import { PnlTicksResponseObject, RequestMethod } from '../../../../src/types';
 import request from 'supertest';
-import { sendRequest } from '../../../helpers/helpers';
+import { pnlTickCreateObjectToResponseObject, sendRequest } from '../../../helpers/helpers';
 
 describe('pnlTicks-controller#V4', () => {
   beforeAll(async () => {
@@ -43,23 +43,17 @@ describe('pnlTicks-controller#V4', () => {
           `&subaccountNumber=${testConstants.defaultSubaccount.subaccountNumber}`,
       });
 
-      const expectedPnlTickResponse: PnlTicksResponseObject = {
+      const expectedPnlTickResponse
+      : PnlTicksResponseObject = pnlTickCreateObjectToResponseObject({
         ...testConstants.defaultPnlTick,
-        id: PnlTicksTable.uuid(
-          testConstants.defaultPnlTick.subaccountId,
-          testConstants.defaultPnlTick.createdAt,
-        ),
-      };
+      });
 
-      const expectedPnlTick2Response: any = {
+      const expectedPnlTick2Response
+      : PnlTicksResponseObject = pnlTickCreateObjectToResponseObject({
         ...testConstants.defaultPnlTick,
         createdAt,
         blockHeight,
-        id: PnlTicksTable.uuid(
-          testConstants.defaultPnlTick.subaccountId,
-          createdAt,
-        ),
-      };
+      });
 
       expect(response.body.historicalPnl).toEqual(
         expect.arrayContaining([
@@ -99,23 +93,17 @@ describe('pnlTicks-controller#V4', () => {
           `&subaccountNumber=${testConstants.defaultSubaccount.subaccountNumber}&page=2&limit=1`,
       });
 
-      const expectedPnlTickResponse: PnlTicksResponseObject = {
+      const expectedPnlTickResponse
+      : PnlTicksResponseObject = pnlTickCreateObjectToResponseObject({
         ...testConstants.defaultPnlTick,
-        id: PnlTicksTable.uuid(
-          testConstants.defaultPnlTick.subaccountId,
-          testConstants.defaultPnlTick.createdAt,
-        ),
-      };
+      });
 
-      const expectedPnlTick2Response: PnlTicksResponseObject = {
+      const expectedPnlTick2Response
+      : PnlTicksResponseObject = pnlTickCreateObjectToResponseObject({
         ...testConstants.defaultPnlTick,
         createdAt,
         blockHeight,
-        id: PnlTicksTable.uuid(
-          testConstants.defaultPnlTick.subaccountId,
-          createdAt,
-        ),
-      };
+      });
 
       expect(responsePage1.body.pageSize).toStrictEqual(1);
       expect(responsePage1.body.offset).toStrictEqual(0);
@@ -164,15 +152,12 @@ describe('pnlTicks-controller#V4', () => {
           `&createdBeforeOrAtHeight=${blockHeight}`,
       });
 
-      const expectedPnlTick2Response: PnlTicksResponseObject = {
+      const expectedPnlTick2Response
+      : PnlTicksResponseObject = pnlTickCreateObjectToResponseObject({
         ...testConstants.defaultPnlTick,
         createdAt,
         blockHeight,
-        id: PnlTicksTable.uuid(
-          testConstants.defaultPnlTick.subaccountId,
-          createdAt,
-        ),
-      };
+      });
 
       expect(response.body.historicalPnl).toEqual(
         expect.arrayContaining([
@@ -205,13 +190,10 @@ describe('pnlTicks-controller#V4', () => {
           `&createdOnOrAfterHeight=${testConstants.defaultPnlTick.blockHeight}`,
       });
 
-      const expectedPnlTickResponse: PnlTicksResponseObject = {
+      const expectedPnlTickResponse
+      : PnlTicksResponseObject = pnlTickCreateObjectToResponseObject({
         ...testConstants.defaultPnlTick,
-        id: PnlTicksTable.uuid(
-          testConstants.defaultPnlTick.subaccountId,
-          testConstants.defaultPnlTick.createdAt,
-        ),
-      };
+      });
 
       expect(response.body.historicalPnl).toEqual(
         expect.arrayContaining([
