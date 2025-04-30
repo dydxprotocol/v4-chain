3fbcc520ae45deb29a2567bee60cea0b4d396c8e
Matthew Weeks <matthew.h.weeks@gmail.com>
2025-04-30 16:16:56 -0400

    (perf) use luxon.DateTime.fromSQL vs moment()

--- indexer/packages/postgres/src/db/pg-config.ts
+++ indexer/packages/postgres/src/db/pg-config.ts
@@ -1,12 +1,17 @@
-import moment from 'moment';
 import pg from 'pg';
+import { DateTime } from 'luxon';
 
 /**
  * we need to add this line, because the default type parser
  * changes all datetime objects to javascript dates, when we
  * need all dates returned with iso strings
  */
+
+const utcZone = {
+  zone: "utc",
+}
+
 pg.types.setTypeParser(
   pg.types.builtins.TIMESTAMPTZ,
-  (val) => (val === null ? null : moment(val).toISOString()),
+  (val) => (val === null ? null : DateTime.fromSQL(val, utcZone).toISO()),
 );
