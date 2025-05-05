a38f9713ce4933f53199a37c73173dd137b29320
Anmol Agrawal <anmolagrawal345@gmail.com>
2025-05-05 19:54:30 -0400

    resolve merge conflict

--- protocol/x/listing/keeper/listing_test.go
+++ protocol/x/listing/keeper/listing_test.go
@@ -189,21 +189,14 @@ func TestCreatePerpetual(t *testing.T) {
 					// Expected resolution = -6 - (Floor(log10(1000000000))-10) = -5
 					require.Equal(t, int32(-5), perpetual.Params.AtomicResolution)
 					require.Equal(t, int32(types.DefaultFundingPpm), perpetual.Params.DefaultFundingPpm)
-<<<<<<< HEAD
-					require.Equal(t, uint32(types.LiquidityTier_Isolated), perpetual.Params.LiquidityTier)
+
+					require.Equal(t, uint32(types.LiquidityTier_IML_5x), perpetual.Params.LiquidityTier)
 
 					expectedMarketType := perpetualtypes.PerpetualMarketType_PERPETUAL_MARKET_TYPE_ISOLATED
 					if tc.crossLaunch {
 						expectedMarketType = perpetualtypes.PerpetualMarketType_PERPETUAL_MARKET_TYPE_CROSS
 					}
 					require.Equal(t, expectedMarketType, perpetual.Params.MarketType)
-=======
-					require.Equal(t, uint32(types.LiquidityTier_IML_5x), perpetual.Params.LiquidityTier)
-					require.Equal(
-						t, perpetualtypes.PerpetualMarketType_PERPETUAL_MARKET_TYPE_ISOLATED,
-						perpetual.Params.MarketType,
-					)
->>>>>>> 705b9a5d ([ENG-29] Use New IML 5X Liquidity Tier for Newly Launched Markets)
 				}
 			},
 		)
