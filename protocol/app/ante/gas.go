13f52542b20baaac6880694d758c5da328eefb2d
Shrenuj Bansal <shrenuj@dydx.exchange>
2025-06-09 14:35:43 -0400

    Add tx batching gas decorator fix
    Signed-off-by: Shrenuj Bansal <shrenuj@dydx.exchange>

--- protocol/app/ante/gas.go
+++ protocol/app/ante/gas.go
@@ -27,14 +27,14 @@ func (dec FreeInfiniteGasDecorator) AnteHandle(
 	simulate bool,
 	next sdk.AnteHandler,
 ) (newCtx sdk.Context, err error) {
-	isSingleClobMsgTx, err := clobante.IsSingleClobMsgTx(tx)
+	hasClobMsg := clobante.HasClobMsg(tx)
 	if err != nil {
 		return ctx, err
 	}
 
-	// If this is a single clob msg tx, or a single app-injected msg tx, then set the gas meter to
+	// If this is a clob msg tx, or a single app-injected msg tx, then set the gas meter to
 	// FreeInfiniteGasMeter.
-	if isSingleClobMsgTx || libante.IsSingleAppInjectedMsg(tx.GetMsgs()) {
+	if hasClobMsg || libante.IsSingleAppInjectedMsg(tx.GetMsgs()) {
 		newCtx = ctx.WithGasMeter(types.NewFreeInfiniteGasMeter())
 		return next(newCtx, tx, simulate)
 	}
