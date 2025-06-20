883afc159542562f0107aa07cb5d6e76ae101324
anmolagrawal345 <anmolagrawal345@gmail.com>
2025-06-20 14:59:23 -0400

    Don't default to null in builder codes migration (#2935)

--- indexer/packages/postgres/src/db/migrations/migration_files/20250514151100_add_builder_fees_to_orders_and_fills.ts
+++ indexer/packages/postgres/src/db/migrations/migration_files/20250514151100_add_builder_fees_to_orders_and_fills.ts
@@ -2,13 +2,13 @@ import * as Knex from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.alterTable('orders', (table) => {
-    table.string('builderAddress').nullable().defaultTo(null);
-    table.string('feePpm').nullable().defaultTo(null);
+    table.string('builderAddress').nullable();
+    table.string('feePpm').nullable();
   });
 
   await knex.schema.alterTable('fills', (table) => {
-    table.string('builderAddress').nullable().defaultTo(null);
-    table.string('builderFee').nullable().defaultTo(null);
+    table.string('builderAddress').nullable();
+    table.string('builderFee').nullable();
   });
 }
 
