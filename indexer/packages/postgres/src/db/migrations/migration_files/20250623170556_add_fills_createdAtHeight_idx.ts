9491b14773f3bc58ed0a106cd99e8d6654dc0ee3
Kefan Cao <kefan@dydx.exchange>
2025-06-23 17:09:09 -0400

    changes

---
+++ indexer/packages/postgres/src/db/migrations/migration_files/20250623170556_add_fills_createdAtHeight_idx.ts
@@ -0,0 +1,14 @@
+import * as Knex from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.raw(`
+    CREATE INDEX CONCURRENTLY IF NOT EXISTS fills_createdatheight_index
+      ON fills("createdAtHeight");
+  `);
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.raw(`
+    DROP INDEX CONCURRENTLY IF EXISTS fills_createdatheight_index;
+  `);
+}
