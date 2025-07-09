8cea2eed958fbe72c0641a9a1dd0faa5ac85ff22
anmolagrawal345 <anmolagrawal345@gmail.com>
2025-07-09 16:42:06 -0400

    Upgrade Handler 8.2 (#2966)

--- protocol/app/upgrades.go
+++ protocol/app/upgrades.go
@@ -3,7 +3,7 @@ package app
 import (
 	"fmt"
 
-	v_8_1 "github.com/dydxprotocol/v4-chain/protocol/app/upgrades/v8.1"
+	v_8_2 "github.com/dydxprotocol/v4-chain/protocol/app/upgrades/v8.2"
 
 	upgradetypes "cosmossdk.io/x/upgrade/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -14,7 +14,7 @@ var (
 	// `Upgrades` defines the upgrade handlers and store loaders for the application.
 	// New upgrades should be added to this slice after they are implemented.
 	Upgrades = []upgrades.Upgrade{
-		v_8_1.Upgrade,
+		v_8_2.Upgrade,
 	}
 	Forks = []upgrades.Fork{}
 )
@@ -22,12 +22,12 @@ var (
 // setupUpgradeHandlers registers the upgrade handlers to perform custom upgrade
 // logic and state migrations for software upgrades.
 func (app *App) setupUpgradeHandlers() {
-	if app.UpgradeKeeper.HasHandler(v_8_1.UpgradeName) {
-		panic(fmt.Sprintf("Cannot register duplicate upgrade handler '%s'", v_8_1.UpgradeName))
+	if app.UpgradeKeeper.HasHandler(v_8_2.UpgradeName) {
+		panic(fmt.Sprintf("Cannot register duplicate upgrade handler '%s'", v_8_2.UpgradeName))
 	}
 	app.UpgradeKeeper.SetUpgradeHandler(
-		v_8_1.UpgradeName,
-		v_8_1.CreateUpgradeHandler(
+		v_8_2.UpgradeName,
+		v_8_2.CreateUpgradeHandler(
 			app.ModuleManager,
 			app.configurator,
 		),
