e60a278f392d01f7d097f5f3104ddb3b1f50a1e8
Justin Barnett <me@justin.cv>
2025-07-28 10:17:24 -0400

    Add governance messages

---
+++ protocol/x/revshare/keeper/msg_set_order_router_rev_shares.go
@@ -0,0 +1,41 @@
+package keeper
+
+import (
+	"context"
+
+	errorsmod "cosmossdk.io/errors"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
+	"github.com/dydxprotocol/v4-chain/protocol/lib"
+	"github.com/dydxprotocol/v4-chain/protocol/x/revshare/types"
+)
+
+func (k msgServer) SetOrderRouterRevShares(
+	goCtx context.Context,
+	msg *types.MsgSetOrderRouterRevShares,
+) (*types.MsgSetOrderRouterRevSharesResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	// Check if sender is authorized to set revenue share
+	if !k.HasAuthority(msg.Authority) {
+		return nil, errorsmod.Wrapf(
+			govtypes.ErrInvalidSigner,
+			"invalid authority %s",
+			msg.Authority,
+		)
+	}
+
+	for _, orderRouterRevShare := range msg.OrderRouterRevShares {
+		if orderRouterRevShare.SharePpm > lib.OneHundredThousand*5 {
+			return nil, errorsmod.Wrapf(
+				types.ErrInvalidRevenueSharePpm,
+				"rev share safety violation: rev shares greater than or equal to allowed amount",
+			)
+		}
+		if err := k.Keeper.SetOrderRouterRevShares(ctx, orderRouterRevShare.Address, orderRouterRevShare.SharePpm); err != nil {
+			return nil, err
+		}
+	}
+
+	return &types.MsgSetOrderRouterRevSharesResponse{}, nil
+}
---
+++ protocol/x/revshare/keeper/msg_set_order_router_rev_shares_test.go
@@ -0,0 +1,109 @@
+package keeper_test
+
+import (
+	"testing"
+
+	testapp "github.com/dydxprotocol/v4-chain/protocol/testutil/app"
+	"github.com/dydxprotocol/v4-chain/protocol/x/revshare/keeper"
+	"github.com/stretchr/testify/require"
+
+	"github.com/dydxprotocol/v4-chain/protocol/lib"
+	"github.com/dydxprotocol/v4-chain/protocol/testutil/constants"
+	"github.com/dydxprotocol/v4-chain/protocol/x/revshare/types"
+)
+
+func TestSetOrderRouterRevShares(t *testing.T) {
+	tests := map[string]struct {
+		// Msg
+		msg *types.MsgSetOrderRouterRevShares
+		// Expected error
+		expectedErr string
+	}{
+		"Success - Set revenue share": {
+			msg: &types.MsgSetOrderRouterRevShares{
+				Authority: lib.GovModuleAddress.String(),
+				OrderRouterRevShares: []types.OrderRouterRevShares{
+					{
+						Address:  constants.AliceAccAddress.String(),
+						SharePpm: 100_000,
+					},
+					{
+						Address:  constants.BobAccAddress.String(),
+						SharePpm: 100_000,
+					},
+				},
+			},
+			expectedErr: "",
+		},
+		"Failure - Invalid Authority": {
+			msg: &types.MsgSetOrderRouterRevShares{
+				Authority: constants.AliceAccAddress.String(),
+				OrderRouterRevShares: []types.OrderRouterRevShares{
+					{
+						Address:  constants.AliceAccAddress.String(),
+						SharePpm: 100_000,
+					},
+				},
+			},
+			expectedErr: "invalid authority",
+		},
+		"Failure - Empty Authority": {
+			msg: &types.MsgSetOrderRouterRevShares{
+				OrderRouterRevShares: []types.OrderRouterRevShares{
+					{
+						Address:  constants.AliceAccAddress.String(),
+						SharePpm: 100_000,
+					},
+				},
+			},
+			expectedErr: "invalid authority",
+		},
+		"Failure - Invalid revenue share address": {
+			msg: &types.MsgSetOrderRouterRevShares{
+				Authority: lib.GovModuleAddress.String(),
+				OrderRouterRevShares: []types.OrderRouterRevShares{
+					{
+						Address:  "invalid_address",
+						SharePpm: 100_000,
+					},
+				},
+			},
+			expectedErr: "invalid address",
+		},
+		"Failure - Invalid revenue share ppm": {
+			msg: &types.MsgSetOrderRouterRevShares{
+				Authority: lib.GovModuleAddress.String(),
+				OrderRouterRevShares: []types.OrderRouterRevShares{
+					{
+						Address:  constants.AliceAccAddress.String(),
+						SharePpm: 1_000_000,
+					},
+				},
+			},
+			expectedErr: "rev share safety violation: rev shares greater than or equal to allowed amount:",
+		},
+	}
+
+	for name, tc := range tests {
+		t.Run(
+			name, func(t *testing.T) {
+				tApp := testapp.NewTestAppBuilder(t).Build()
+				ctx := tApp.InitChain()
+				k := tApp.App.RevShareKeeper
+				ms := keeper.NewMsgServerImpl(k)
+				_, err := ms.Keeper.SetOrderRouterRevShares(ctx, tc.msg)
+				if tc.expectedErr != "" {
+					require.Error(t, err)
+					require.Contains(t, err.Error(), tc.expectedErr)
+				} else {
+					require.NoError(t, err)
+					for _, revShare := range tc.msg.OrderRouterRevShares {
+						params, err := k.GetOrderRouterRevShares(ctx, revShare.Address)
+						require.NoError(t, err)
+						require.Equal(t, revShare.SharePpm, params)
+					}
+				}
+			},
+		)
+	}
+}
