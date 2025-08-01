f231c305bcad1d2f3583d4d548719fe2e651aa77
Justin Barnett <me@justin.cv>
2025-08-01 11:45:59 -0400

    Add missing order router

--- indexer/packages/postgres/src/lib/order-translations.ts
+++ indexer/packages/postgres/src/lib/order-translations.ts
@@ -80,7 +80,7 @@ export function convertToIndexerOrderWithSubaccount(
     conditionType: orderTypeToProtocolConditionType(order.type),
     conditionalOrderTriggerSubticks: triggerSubticks,
     builderCodeParams: builderCodeParameters,
-    orderRouterAddress: '', // TODO(justin): Change this
+    orderRouterAddress: order.orderRouterAddress ?? '',
   };
 
   return indexerOrder;
