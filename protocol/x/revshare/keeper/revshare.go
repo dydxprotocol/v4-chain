57a3d2f001ce17cac92ef5fc8ecae72d2fa219ea
Justin Barnett <61020572+jusbar23@users.noreply.github.com>
2025-08-11 15:49:51 -0400

    Change to warn instead of error for invalid ORRS (#3036)

--- protocol/x/revshare/keeper/revshare.go
+++ protocol/x/revshare/keeper/revshare.go
@@ -327,7 +327,11 @@ func (k Keeper) getOrderRouterRevShares(
 	orderRouterRevShares := []types.RevShare{}
 	takerOrderRouterRevSharePpm, err := k.GetOrderRouterRevShare(ctx, fill.TakerOrderRouterAddr)
 	if err != nil {
-		k.Logger(ctx).Error("order router rev share invalid for taker: " + fill.TakerOrderRouterAddr)
+		k.Logger(ctx).Warn("order router rev share invalid for taker, ignoring ",
+			"taker_order_router_addr: ", fill.TakerOrderRouterAddr,
+			"taker_addr: ", fill.TakerAddr,
+			"error: ", err,
+		)
 	} else {
 		if fill.TakerOrderRouterAddr != "" {
 			// Orders can have 2 rev share ids, we need to calculate each side separately
@@ -346,7 +350,11 @@ func (k Keeper) getOrderRouterRevShares(
 
 	makerOrderRouterRevSharePpm, err := k.GetOrderRouterRevShare(ctx, fill.MakerOrderRouterAddr)
 	if err != nil {
-		k.Logger(ctx).Error("order router rev share invalid for maker: " + fill.MakerOrderRouterAddr)
+		k.Logger(ctx).Warn("order router rev share invalid for maker, ignoring ",
+			"maker_order_router_addr: ", fill.MakerOrderRouterAddr,
+			"maker_addr: ", fill.MakerAddr,
+			"error: ", err,
+		)
 	} else {
 		if fill.MakerOrderRouterAddr != "" {
 			// maker ppm * max(0, maker)
