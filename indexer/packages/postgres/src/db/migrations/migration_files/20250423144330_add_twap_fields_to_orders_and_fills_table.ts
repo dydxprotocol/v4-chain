9baac9543e56debfd522cc1d222fdf3cd8e55b84
Anmol Agrawal <anmolagrawal345@gmail.com>
2025-08-12 16:22:47 -0400

    combine migration files into one

--- indexer/packages/postgres/src/db/migrations/migration_files/20250423144330_add_twap_fields_to_orders_and_fills_table.ts
+++ indexer/packages/postgres/src/db/migrations/migration_files/20250423144330_add_twap_fields_to_orders_and_fills_table.ts
@@ -1,9 +1,14 @@
 import * as Knex from 'knex';
-
 import { formatAlterTableEnumSql } from '../helpers';
 
 export async function up(knex: Knex): Promise<void> {
-  return knex.raw(formatAlterTableEnumSql(
+  await knex.schema.alterTable('orders', (table) => {
+    table.string('duration').nullable();
+    table.string('interval').nullable();
+    table.string('priceTolerance').nullable();
+  });
+
+  await knex.raw(formatAlterTableEnumSql(
     'orders',
     'type',
     [
@@ -23,10 +28,22 @@ export async function up(knex: Knex): Promise<void> {
       'TWAP_SUBORDER',
     ],
   ));
+
+  await knex.raw(formatAlterTableEnumSql(
+    'fills',
+    'type',
+    ['LIMIT', 'LIQUIDATED', 'LIQUIDATION', 'DELEVERAGED', 'OFFSETTING', 'TWAP_SUBORDER'],
+  ));
 }
 
 export async function down(knex: Knex): Promise<void> {
-  return knex.raw(formatAlterTableEnumSql(
+  await knex.schema.alterTable('orders', (table) => {
+    table.dropColumn('duration');
+    table.dropColumn('interval');
+    table.dropColumn('priceTolerance');
+  });
+
+  await knex.raw(formatAlterTableEnumSql(
     'orders',
     'type',
     [
@@ -43,5 +60,11 @@ export async function down(knex: Knex): Promise<void> {
       'FAILED_HARD_TRADE',
       'TRANSFER_PLACEHOLDER',
     ],
+  ));
+
+  await knex.raw(formatAlterTableEnumSql(
+    'fills',
+    'type',
+    ['LIMIT', 'LIQUIDATED', 'LIQUIDATION', 'DELEVERAGED', 'OFFSETTING'],
   ));
 }
