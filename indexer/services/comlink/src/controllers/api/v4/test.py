fa8c140698f886f2609c347f97eaa793585f0dd9
Kefan Cao <kefan@dydx.exchange>
2025-09-06 15:39:01 -0400

    remove

--- indexer/services/comlink/src/controllers/api/v4/test.py
+++
@@ -1,9 +0,0 @@
-import os
-import binascii
-
-# Generate a 32-byte (256-bit) private key
-private_key = binascii.hexlify(os.urandom(32)).decode()
-
-
-
-print(f"Generated private key: {private_key}")
