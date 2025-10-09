a0496a02fbb44fdc432918bbda648e6d0c811bf8
Justin Barnett <me@justin.cv>
2025-10-09 14:46:11 -0400

    Update

--- protocol/x/affiliates/keeper/grpc_query.go
+++ protocol/x/affiliates/keeper/grpc_query.go
@@ -7,6 +7,7 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	"github.com/dydxprotocol/v4-chain/protocol/dtypes"
+	"github.com/dydxprotocol/v4-chain/protocol/lib"
 	"github.com/dydxprotocol/v4-chain/protocol/x/affiliates/types"
 )
 
@@ -37,18 +38,15 @@ func (k Keeper) AffiliateInfo(c context.Context,
 		return nil, err
 	}
 
-	referredVolume, err := k.GetReferredVolume(ctx, req.GetAddress())
-	if err != nil {
-		return nil, err
-	}
-
+	userStats := k.statsKeeper.GetUserStats(ctx, addr.String())
+	referredVolume := userStats.AffiliateReferredVolumeQuoteQuantums
 	stakedAmount := k.statsKeeper.GetStakedAmount(ctx, req.GetAddress())
 
 	return &types.AffiliateInfoResponse{
 		IsWhitelisted:  isWhitelisted,
 		Tier:           tierLevel,
 		FeeSharePpm:    feeSharePpm,
-		ReferredVolume: dtypes.NewIntFromBigInt(referredVolume),
+		ReferredVolume: dtypes.NewIntFromBigInt(lib.BigU(referredVolume)),
 		StakedAmount:   dtypes.NewIntFromBigInt(stakedAmount),
 	}, nil
 }
