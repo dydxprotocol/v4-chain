6c8545217ebbe43e0f73da3c4f221745bbf9252e
Justin Barnett <me@justin.cv>
2025-10-20 16:46:57 -0400

    9.4 update

--- protocol/app/upgrades/v9.4/upgrade_container_test.go
+++ protocol/app/upgrades/v9.4/upgrade_container_test.go
@@ -1,11 +1,11 @@
 //go:build all || container_test
 
-package v_9_3_test
+package v_9_4_test
 
 import (
 	"testing"
 
-	v_9_3 "github.com/dydxprotocol/v4-chain/protocol/app/upgrades/v9.3"
+	v_9_4 "github.com/dydxprotocol/v4-chain/protocol/app/upgrades/v9.4"
 
 	"github.com/dydxprotocol/v4-chain/protocol/testing/containertest"
 	"github.com/dydxprotocol/v4-chain/protocol/testutil/constants"
@@ -24,7 +24,7 @@ func TestStateUpgrade(t *testing.T) {
 	preUpgradeSetups(node, t)
 	preUpgradeChecks(node, t)
 
-	err = containertest.UpgradeTestnet(nodeAddress, t, node, v_9_3.UpgradeName)
+	err = containertest.UpgradeTestnet(nodeAddress, t, node, v_9_4.UpgradeName)
 	require.NoError(t, err)
 
 	postUpgradeChecks(node, t)
