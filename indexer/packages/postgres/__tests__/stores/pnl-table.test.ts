b38917dc9bf47dfcdc7a381def04612e34c28f6b
David Li <h396lidavid@gmail.com>
2025-10-22 12:31:01 -0400

    Remove test deployment

--- indexer/packages/postgres/__tests__/stores/pnl-table.test.ts
+++ indexer/packages/postgres/__tests__/stores/pnl-table.test.ts
@@ -9,7 +9,10 @@ import {
   defaultSubaccountId,
   defaultSubaccountId2,
   defaultPnl,
+  defaultPnl2,
+  defaultBlock,
 } from '../helpers/constants';
+import { BlockTable } from 'packages/postgres/src';
 
 describe('Pnl store', () => {
   beforeEach(async () => {
@@ -32,6 +35,106 @@ describe('Pnl store', () => {
     await PnlTable.create(defaultPnl);
     const fetched = await PnlTable.findById(defaultPnl.subaccountId, defaultPnl.createdAt);
     expect(fetched).toEqual(expect.objectContaining(defaultPnl));
+  });
+
+  it('Successfully creates multiple Pnl records', async () => {
+    await BlockTable.create({
+      ...defaultBlock,
+      blockHeight: '5',
+    });
+    await Promise.all([
+      PnlTable.create(defaultPnl),
+      PnlTable.create(defaultPnl2),
+    ]);
+
+    const { results: pnls } = await PnlTable.findAll({}, [], {
+      orderBy: [[PnlColumns.createdAtHeight, Ordering.ASC]],
+    });
+
+    expect(pnls.length).toEqual(2);
+    expect(pnls[0]).toEqual(expect.objectContaining(defaultPnl));
+    expect(pnls[1]).toEqual(expect.objectContaining(defaultPnl2));
+  });
+
+  it('Successfully finds Pnl records with subaccountId', async () => {
+    await Promise.all([
+      PnlTable.create(defaultPnl),
+      PnlTable.create({
+        ...defaultPnl,
+        createdAt: '2022-06-01T01:00:00.000Z',
+      }),
+      PnlTable.create({
+        ...defaultPnl,
+        subaccountId: defaultSubaccountId2,
+        createdAt: '2022-06-01T00:00:00.000Z',
+      }),
+    ]);
+
+    const { results: pnls } = await PnlTable.findAll(
+      {
+        subaccountId: [defaultSubaccountId],
+      },
+      [],
+      {},
+    );
+
+    expect(pnls.length).toEqual(2);
+  });
+
+  it('Successfully finds Pnl records using pagination', async () => {
+    await Promise.all([
+      PnlTable.create(defaultPnl),
+      PnlTable.create({
+        ...defaultPnl,
+        createdAt: '2020-01-01T00:00:00.000Z',
+        createdAtHeight: '1000',
+      }),
+    ]);
+
+    const responsePageOne = await PnlTable.findAll({
+      page: 1,
+      limit: 1,
+    }, [], {
+      orderBy: [[PnlColumns.createdAtHeight, Ordering.DESC]],
+    });
+
+    expect(responsePageOne.results.length).toEqual(1);
+    expect(responsePageOne.results[0]).toEqual(expect.objectContaining({
+      ...defaultPnl,
+      createdAt: '2020-01-01T00:00:00.000Z',
+      createdAtHeight: '1000',
+    }));
+    expect(responsePageOne.offset).toEqual(0);
+    expect(responsePageOne.total).toEqual(2);
+
+    const responsePageTwo = await PnlTable.findAll({
+      page: 2,
+      limit: 1,
+    }, [], {
+      orderBy: [[PnlColumns.createdAtHeight, Ordering.DESC]],
+    });
+
+    expect(responsePageTwo.results.length).toEqual(1);
+    expect(responsePageTwo.results[0]).toEqual(expect.objectContaining(defaultPnl));
+    expect(responsePageTwo.offset).toEqual(1);
+    expect(responsePageTwo.total).toEqual(2);
+
+    const responsePageAllPages = await PnlTable.findAll({
+      page: 1,
+      limit: 2,
+    }, [], {
+      orderBy: [[PnlColumns.createdAtHeight, Ordering.DESC]],
+    });
+
+    expect(responsePageAllPages.results.length).toEqual(2);
+    expect(responsePageAllPages.results[0]).toEqual(expect.objectContaining({
+      ...defaultPnl,
+      createdAt: '2020-01-01T00:00:00.000Z',
+      createdAtHeight: '1000',
+    }));
+    expect(responsePageAllPages.results[1]).toEqual(expect.objectContaining(defaultPnl));
+    expect(responsePageAllPages.offset).toEqual(0);
+    expect(responsePageAllPages.total).toEqual(2);
   });
 
   it('Successfully retrieves hourly aggregated PNL records for single subaccount', async () => {
