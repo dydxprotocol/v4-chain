8b129d31334ff93436bb1f7009b6aab55f59821b
unbornaztecking <21340974+UnbornAztecKing@users.noreply.github.com>
2025-10-23 12:46:41 -0400

    Revert "[Eng-962] Apple login redirect comlink endpoint (#3162)"
    This reverts commit 779b3ff73f2fd63b604b39c17125b01610f5e59e.

--- indexer/services/comlink/src/lib/validation/schemas.ts
+++ indexer/services/comlink/src/lib/validation/schemas.ts
@@ -386,21 +386,6 @@ const uploadDydxAddressSchema: Record<string, ParamSchema> = {
   },
 };
 
-const appleLoginRedirectSchema: Record<string, ParamSchema> = {
-  state: {
-    in: ['query'],
-    isString: true,
-    notEmpty: true,
-    errorMessage: 'state (public key) is required and must be a non-empty string',
-  },
-  code: {
-    in: ['query'],
-    isString: true,
-    notEmpty: true,
-    errorMessage: 'code (authorization code) is required and must be a non-empty string',
-  },
-};
-
 const getDepositAddressSchema: Record<string, ParamSchema> = {
   dydxAddress: {
     in: ['params'],
@@ -411,8 +396,6 @@ const getDepositAddressSchema: Record<string, ParamSchema> = {
 export const CheckSignInSchema = checkSchema(signInSchema);
 
 export const CheckUploadDydxAddressSchema = checkSchema(uploadDydxAddressSchema);
-
-export const CheckAppleLoginRedirectSchema = checkSchema(appleLoginRedirectSchema);
 
 export const CheckGetDepositAddressSchema = checkSchema(getDepositAddressSchema);
 
