2385db1c9778914ebb6c9ce83978edd27e156577
Justin Barnett <61020572+jusbar23@users.noreply.github.com>
2025-10-23 12:02:27 -0400

    Integrate commission and overrides to fee tier calculation (Backport) (#3117) (#3191)
    (cherry picked from commit 64ca4508458dd0d69e086dda1634a02c5ebb6df8)

--- protocol/x/affiliates/types/expected_keepers.go
+++ protocol/x/affiliates/types/expected_keepers.go
@@ -11,6 +11,7 @@ type StatsKeeper interface {
 	GetStakedAmount(ctx sdk.Context, delegatorAddr string) *big.Int
 	GetBlockStats(ctx sdk.Context) *stattypes.BlockStats
 	GetUserStats(ctx sdk.Context, address string) *stattypes.UserStats
+	SetUserStats(ctx sdk.Context, address string, userStats *stattypes.UserStats)
 }
 
 type FeetiersKeeper interface {
