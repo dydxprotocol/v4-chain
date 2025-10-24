fea4a31a043bb9ce884ccf71c7b82c67bef787d3
anmolagrawal345 <anmolagrawal345@gmail.com>
2025-10-24 14:44:39 -0400

    Check Leverage on Withdrawals and Updates (#3153)
    (cherry picked from commit 70ac841215f9ad79cb65d83d8fb5a4d925f6c172)
    
    # Conflicts:
    #	protocol/x/clob/keeper/leverage_e2e_test.go
    #	protocol/x/subaccounts/keeper/leverage.go
    #	protocol/x/subaccounts/types/errors.go

--- protocol/x/subaccounts/keeper/subaccount.go
+++ protocol/x/subaccounts/keeper/subaccount.go
@@ -723,6 +723,22 @@ func (k Keeper) internalCanUpdateSubaccounts(
 	return success, successPerUpdate, nil
 }
 
+func (k Keeper) GetNetCollateralAndMarginRequirements(
+	ctx sdk.Context,
+	update types.Update,
+) (
+	risk margin.Risk,
+	err error,
+) {
+	// Get leverage configuration for this subaccount
+	var leverageMap map[uint32]uint32
+	if leverage, found := k.GetLeverage(ctx, &update.SubaccountId); found {
+		leverageMap = leverage
+	}
+
+	return k.GetNetCollateralAndMarginRequirementsWithLeverage(ctx, update, leverageMap)
+}
+
 // GetNetCollateralAndMarginRequirements returns the total net collateral, total initial margin requirement,
 // and total maintenance margin requirement for the subaccount as if the `update` was applied.
 // It is used to get information about speculative changes to the subaccount.
@@ -733,9 +749,10 @@ func (k Keeper) internalCanUpdateSubaccounts(
 // If two position updates reference the same position, an error is returned.
 //
 // All return values are denoted in quote quantums.
-func (k Keeper) GetNetCollateralAndMarginRequirements(
+func (k Keeper) GetNetCollateralAndMarginRequirementsWithLeverage(
 	ctx sdk.Context,
 	update types.Update,
+	leverageMap map[uint32]uint32,
 ) (
 	risk margin.Risk,
 	err error,
