4e32ffc3851f77194e65cd82182d1a2176c7ca6e
jaredvu <jaredvu@gmail.com>
2025-12-12 13:56:24 -0800

    join exposed headers

--- indexer/services/comlink/src/request-helpers/geo-headers-middleware.ts
+++ indexer/services/comlink/src/request-helpers/geo-headers-middleware.ts
@@ -6,21 +6,27 @@ export default function geoHeadersMiddleware(
   next: express.NextFunction,
 ): void {
   const country = req.headers['geo-origin-country'];
+  const exposedHeaders = [];
+
   if (country !== undefined) {
-    res.header('Access-Control-Expose-Headers', 'geo-origin-country');
+    exposedHeaders.push('geo-origin-country');
     res.set('Geo-Origin-Country', country);
   }
 
   const region = req.headers['geo-origin-region'];
   if (region !== undefined) {
-    res.header('Access-Control-Expose-Headers', 'geo-origin-region');
+    exposedHeaders.push('geo-origin-region');
     res.set('Geo-Origin-Region', region);
   }
 
   const status = req.headers['geo-origin-status'];
   if (status !== undefined) {
-    res.header('Access-Control-Expose-Headers', 'geo-origin-status');
+    exposedHeaders.push('geo-origin-status');
     res.set('Geo-Origin-Status', status);
+  }
+
+  if (exposedHeaders.length > 0) {
+    res.header('Access-Control-Expose-Headers', exposedHeaders.join(', '));
   }
 
   next();
