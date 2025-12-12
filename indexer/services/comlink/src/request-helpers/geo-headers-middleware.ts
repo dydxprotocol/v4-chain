19e5d04bec77df41decae2b3675336b73f6dcbd9
jaredvu <jaredvu@gmail.com>
2025-12-12 12:33:22 -0800

    Expose geo headers

--- indexer/services/comlink/src/request-helpers/geo-headers-middleware.ts
+++ indexer/services/comlink/src/request-helpers/geo-headers-middleware.ts
@@ -7,16 +7,19 @@ export default function geoHeadersMiddleware(
 ): void {
   const country = req.headers['geo-origin-country'];
   if (country !== undefined) {
+    res.header('Access-Control-Expose-Headers', 'geo-origin-country');
     res.set('Geo-Origin-Country', country);
   }
 
   const region = req.headers['geo-origin-region'];
   if (region !== undefined) {
+    res.header('Access-Control-Expose-Headers', 'geo-origin-region');
     res.set('Geo-Origin-Region', region);
   }
 
   const status = req.headers['geo-origin-status'];
   if (status !== undefined) {
+    res.header('Access-Control-Expose-Headers', 'geo-origin-status');
     res.set('Geo-Origin-Status', status);
   }
 
