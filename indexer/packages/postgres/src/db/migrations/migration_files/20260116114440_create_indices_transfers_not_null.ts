02600f0a6d399f5d78dc071a3f2a20df0c5afb69
Unborn Aztec King <21340974+UnbornAztecKing@users.noreply.github.com>
2026-01-16 17:31:08 -0500

    fix knex multi-statement migrations

--- indexer/packages/postgres/src/db/migrations/migration_files/20260116114440_create_indices_transfers_not_null.ts
+++ indexer/packages/postgres/src/db/migrations/migration_files/20260116114440_create_indices_transfers_not_null.ts
@@ -1,33 +1,45 @@
 import * as Knex from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
-  await knex.raw(`
-    DROP INDEX CONCURRENTLY IF EXISTS "transfers_sendersubaccountid_createdatheight_index";
+  await knex.raw(
+    `DROP INDEX CONCURRENTLY IF EXISTS "transfers_sendersubaccountid_createdatheight_index";`
+  );
 
-    CREATE INDEX CONCURRENTLY IF NOT EXISTS transfers_sender_id_height_nn
-    ON transfers ("senderSubaccountId", "createdAtHeight")
-    WHERE "senderSubaccountId" IS NOT NULL;
+  await knex.raw(
+    `CREATE INDEX CONCURRENTLY IF NOT EXISTS transfers_sender_id_height_nn
+    ON transfers("senderSubaccountId", "createdAtHeight")
+    WHERE "senderSubaccountId" IS NOT NULL;`
+  );
 
-    DROP INDEX CONCURRENTLY IF EXISTS "transfers_recipientsubaccountid_createdatheight_index";
+  await knex.raw(
+    `DROP INDEX CONCURRENTLY IF EXISTS "transfers_recipientsubaccountid_createdatheight_index";`
+  );
 
-    CREATE INDEX CONCURRENTLY IF NOT EXISTS transfers_recipient_id_height_nn
-    ON transfers ("recipientSubaccountId", "createdAtHeight")
-    WHERE "recipientSubaccountId" IS NOT NULL;
-  `);
+  await knex.raw(
+    `CREATE INDEX CONCURRENTLY IF NOT EXISTS transfers_recipient_id_height_nn
+    ON transfers("recipientSubaccountId", "createdAtHeight")
+    WHERE "recipientSubaccountId" IS NOT NULL;`
+  );
 }
 
 export async function down(knex: Knex): Promise<void> {
-  await knex.raw(`
-    DROP INDEX CONCURRENTLY IF EXISTS "transfers_sender_id_height_nn";
+  await knex.raw(
+    `DROP INDEX CONCURRENTLY IF EXISTS "transfers_sender_id_height_nn";`
+  );
 
-    CREATE INDEX CONCURRENTLY IF NOT EXISTS transfers_sendersubaccountid_createdatheight_index
-    ON transfers ("senderSubaccountId", "createdAtHeight");
+  await knex.raw(
+    `CREATE INDEX CONCURRENTLY IF NOT EXISTS transfers_sendersubaccountid_createdatheight_index
+    ON transfers("senderSubaccountId", "createdAtHeight");`
+  );
 
-    DROP INDEX CONCURRENTLY IF EXISTS "transfers_recipient_id_height_nn";
+  await knex.raw(
+    `DROP INDEX CONCURRENTLY IF EXISTS "transfers_recipient_id_height_nn";`
+  );
 
-    CREATE INDEX CONCURRENTLY IF NOT EXISTS transfers_recipientsubaccountid_createdatheight_index
-    ON transfers ("recipientSubaccountId", "createdAtHeight");
-  `);
+  await knex.raw(
+    `CREATE INDEX CONCURRENTLY IF NOT EXISTS transfers_recipientsubaccountid_createdatheight_index
+    ON transfers("recipientSubaccountId", "createdAtHeight");`
+  );
 }
 
 export const config = {
