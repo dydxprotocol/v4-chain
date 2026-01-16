56b63abfd92987f44762f27ae73be4cb14ca1974
Unborn Aztec King <21340974+UnbornAztecKing@users.noreply.github.com>
2026-01-16 12:20:43 -0500

    perf(postgres:subaccounts): add partial indices and optimize query for subaccounts w/transfers

---
+++ indexer/packages/postgres/src/db/migrations/migration_files/20260116114440_create_indices_transfers_not_null.ts
@@ -0,0 +1,35 @@
+import * as Knex from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.raw(`
+    DROP INDEX IF EXISTS "transfers_sendersubaccountid_createdatheight_index";
+
+    CREATE INDEX IF NOT EXISTS transfers_sender_id_height_nn
+    ON transfers ("senderSubaccountId", "createdAtHeight")
+    WHERE "senderSubaccountId" IS NOT NULL;
+
+    DROP INDEX IF EXISTS "transfers_recipientsubaccountid_createdatheight_index";
+
+    CREATE INDEX IF NOT EXISTS transfers_recipient_id_height_nn
+    ON transfers ("recipientSubaccountId", "createdAtHeight")
+    WHERE "recipientSubaccountId" IS NOT NULL;
+  `);
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.raw(`
+    DROP INDEX IF EXISTS "transfers_sender_id_height_nn";
+
+    CREATE INDEX IF NOT EXISTS transfers_sendersubaccountid_createdatheight_index
+    ON transfers ("senderSubaccountId", "createdAtHeight");
+
+    DROP INDEX IF EXISTS "transfers_recipient_id_height_nn";
+
+    CREATE INDEX IF NOT EXISTS transfers_recipientsubaccountid_createdatheight_index
+    ON transfers ("recipientSubaccountId", "createdAtHeight");
+  `);
+}
+
+export const config = {
+  transaction: false,
+};
